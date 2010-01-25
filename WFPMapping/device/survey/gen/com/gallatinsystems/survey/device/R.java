8908d609737b41b4c16681204d9cb996b5df5afc
Christopher Fagiani <christopher.fagiani@gmail.com>
2010-01-25 20:35:55 +0000

    changed icon to WFP logo

--- WFPMapping/device/survey/gen/com/gallatinsystems/survey/device/R.java
+++ WFPMapping/device/survey/gen/com/gallatinsystems/survey/device/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int wfplogo=0x7f020001;
     }
     public static final class id {
         public static final int QuestionLayout=0x7f060000;
