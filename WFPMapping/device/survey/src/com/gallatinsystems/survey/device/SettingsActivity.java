6b6e942084c77f4c991b74ac34626d84b4a7319f
Christopher Fagiani <christopher.fagiani@gmail.com>
2010-02-03 01:05:26 +0000

    changed settings menu to use listView so options show highlighting properly

--- WFPMapping/device/survey/src/com/gallatinsystems/survey/device/SettingsActivity.java
+++ WFPMapping/device/survey/src/com/gallatinsystems/survey/device/SettingsActivity.java
@@ -1,53 +1,84 @@
 package com.gallatinsystems.survey.device;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.ListActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnFocusChangeListener;
-import android.widget.TableRow;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 /**
  * Displays the settings menu and handles the user choices
  * 
- *TODO: display highlighting when a item is clicked?
  * 
  * @author Christopher Fagiani
  * 
  */
-public class SettingsActivity extends Activity implements OnClickListener {
+public class SettingsActivity extends ListActivity {
+
+	private static final String LABEL = "label";
+	private static final String DESC = "desc";
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settingsmenu);
-		TableRow row = (TableRow) findViewById(R.id.sendOption);
-		row.setOnClickListener(this);
-		row = (TableRow) findViewById(R.id.exportDataOption);
-		row.setOnClickListener(this);
-		row = (TableRow) findViewById(R.id.powerMgmtOption);
-		row.setOnClickListener(this);		
+
+		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+		Resources resources = getResources();
+		list.add(createMap(resources.getString(R.string.sendoptlabel),
+				resources.getString(R.string.sendoptdesc)));
+		list.add(createMap(resources.getString(R.string.exportoptlabel),
+				resources.getString(R.string.exportoptdesc)));
+		list.add(createMap(resources.getString(R.string.poweroptlabel),
+				resources.getString(R.string.poweroptdesc)));
+		String[] fromKeys = { LABEL, DESC };
+		int[] toIds = { R.id.optionLabel, R.id.optionDesc };
+
+		setListAdapter(new SimpleAdapter(this.getApplicationContext(), list,
+				R.layout.settingsdetail, fromKeys, toIds));
 	}
 
-	public void onClick(View v) {		
-		if (v.getId() == R.id.powerMgmtOption) {
-			WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
-			if (!wm.isWifiEnabled()) {
-				wm.setWifiEnabled(true);
+	private HashMap<String, String> createMap(String label, String desc) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put(LABEL, label);
+		map.put(DESC, desc);
+		return map;
+	}
+
+	@Override
+	protected void onListItemClick(ListView list, View view, int position,
+			long id) {
+		TextView label = (TextView) view.findViewById(R.id.optionLabel);
+		if (label != null) {
+			String val = label.getText().toString();
+			Resources resources = getResources();
+			if (resources.getString(R.string.poweroptlabel).equals(val)) {
+				WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+				if (!wm.isWifiEnabled()) {
+					wm.setWifiEnabled(true);
+				} else {
+					wm.setWifiEnabled(false);
+				}
 			} else {
-				wm.setWifiEnabled(false);
+				Intent i = new Intent(view.getContext(), DataSyncActivity.class);
+				if (resources.getString(R.string.sendoptlabel).equals(val)) {
+					i
+							.putExtra(DataSyncActivity.TYPE_KEY,
+									DataSyncActivity.SEND);
+				} else {
+					i.putExtra(DataSyncActivity.TYPE_KEY,
+							DataSyncActivity.EXPORT);
+				}
+				startActivity(i);
+				// terminate this activity
+				finish();
 			}
-		} else {
-			Intent i = new Intent(v.getContext(), DataSyncActivity.class);
-			if (v.getId() == R.id.sendOption) {
-				i.putExtra(DataSyncActivity.TYPE_KEY, DataSyncActivity.SEND);
-			} else {
-				i.putExtra(DataSyncActivity.TYPE_KEY, DataSyncActivity.EXPORT);
-			}
-			startActivity(i);
-			// terminate this activity
-			finish();
 		}
-	}	
+	}
 }
