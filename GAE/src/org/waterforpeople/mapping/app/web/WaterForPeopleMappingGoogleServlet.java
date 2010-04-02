6e75ece8af5901ba7eac6c86e85526dd3cde7caf
Dru Borden <dru.borden@gmail.com>
2010-04-02 22:24:26 +0000

    removed persistence manager commented out blocks of else that used it as I do not think they are used

--- GAE/src/org/waterforpeople/mapping/app/web/WaterForPeopleMappingGoogleServlet.java
+++ GAE/src/org/waterforpeople/mapping/app/web/WaterForPeopleMappingGoogleServlet.java
@@ -24,8 +24,7 @@ public class WaterForPeopleMappingGoogleServlet extends HttpServlet {
 	@SuppressWarnings("unchecked")
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-
+	
 		String showKML = req.getParameter("showKML");
 		@SuppressWarnings("unused")
 		String processFile = req.getParameter("processFile");
@@ -56,40 +55,40 @@ public class WaterForPeopleMappingGoogleServlet extends HttpServlet {
 					.generateRegionDocumentString("Regions.vm");
 			resp.setContentType("application/vnd.google-earth.kml+xml");
 			resp.getWriter().println(placemarksDocument);
-		} else if (listFiles != null) {
-			//TODO move this to a DAO
-			// List processed files
-			javax.jdo.Query query = pm.newQuery("select from  "
-					+ DeviceFiles.class.getName());
-			List<DeviceFiles> entries = (List<DeviceFiles>) query.execute();
-			StringBuilder sb = new StringBuilder();
-			for (DeviceFiles df : entries) {
-				sb.append(df.toString());
-			}
-			pm.close();
-
-			resp.setContentType("text/plain");
-			resp.getWriter().println(sb.toString());
-
-		} else if (testVelocity != null) {
-			KMLGenerator kmlGen = new KMLGenerator();
-			String placemarksDocument = kmlGen
-					.generateDocument("PlacemarkTabs.vm");
-			resp.setContentType("text/plain");
-			resp.getWriter().println(placemarksDocument);
-		} else {
-			//TODO move to DAO
-			javax.jdo.Query query = pm.newQuery("select from  "
-					+ AccessPoint.class.getName());
-			List<AccessPoint> entries = (List<AccessPoint>) query.execute();
-			StringBuilder sb = new StringBuilder();
-			for (AccessPoint ap : entries) {
-				sb.append(ap.toString());
-			}
-			pm.close();
-
-			resp.setContentType("text/plain");
-			resp.getWriter().println(sb.toString());
+//		} else if (listFiles != null) {
+//			//TODO move this to a DAO
+//			// List processed files
+//			javax.jdo.Query query = pm.newQuery("select from  "
+//					+ DeviceFiles.class.getName());
+//			List<DeviceFiles> entries = (List<DeviceFiles>) query.execute();
+//			StringBuilder sb = new StringBuilder();
+//			for (DeviceFiles df : entries) {
+//				sb.append(df.toString());
+//			}
+//			pm.close();
+//
+//			resp.setContentType("text/plain");
+//			resp.getWriter().println(sb.toString());
+//
+//		} else if (testVelocity != null) {
+//			KMLGenerator kmlGen = new KMLGenerator();
+//			String placemarksDocument = kmlGen
+//					.generateDocument("PlacemarkTabs.vm");
+//			resp.setContentType("text/plain");
+//			resp.getWriter().println(placemarksDocument);
+//		} else {
+//			//TODO move to DAO
+//			javax.jdo.Query query = pm.newQuery("select from  "
+//					+ AccessPoint.class.getName());
+//			List<AccessPoint> entries = (List<AccessPoint>) query.execute();
+//			StringBuilder sb = new StringBuilder();
+//			for (AccessPoint ap : entries) {
+//				sb.append(ap.toString());
+//			}
+//			pm.close();
+//
+//			resp.setContentType("text/plain");
+//			resp.getWriter().println(sb.toString());
 		}
 	}
 
