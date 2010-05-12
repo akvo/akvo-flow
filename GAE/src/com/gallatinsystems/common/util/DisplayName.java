648bd7fa6b23a680e8839b19a04fc7dc0e59f5a3
Dru Borden <dru.borden@gmail.com>
2010-05-12 20:25:04 +0000

    Fixed Imports

--- GAE/src/com/gallatinsystems/common/util/DisplayName.java
+++ GAE/src/com/gallatinsystems/common/util/DisplayName.java
@@ -1,9 +1,7 @@
 package com.gallatinsystems.common.util;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 
 @Retention(value=RetentionPolicy.RUNTIME)
