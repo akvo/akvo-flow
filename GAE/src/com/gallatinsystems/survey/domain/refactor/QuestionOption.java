cef71de617c2af228bb0af057b5c1bbcf90119af
Dru Borden <dru.borden@gmail.com>
2010-07-20 22:32:30 +0000

    Put in the right place this time

---
+++ GAE/src/com/gallatinsystems/survey/domain/refactor/QuestionOption.java
@@ -0,0 +1,27 @@
+package com.gallatinsystems.survey.domain.refactor;
+
+import java.util.HashMap;
+
+import com.gallatinsystems.framework.domain.BaseDomain;
+
+public class QuestionOption extends BaseDomain{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2794521663923141747L;
+	private String code =null;
+	private HashMap<String,String> optionMap=null;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public HashMap<String, String> getOptionMap() {
+		return optionMap;
+	}
+	public void setOptionMap(HashMap<String, String> optionMap) {
+		this.optionMap = optionMap;
+	}
+}
---
+++ GAE/src/com/gallatinsystems/survey/domain/refactor/QuestionOptionSummary.java
@@ -0,0 +1,48 @@
+package com.gallatinsystems.survey.domain.refactor;
+
+import com.gallatinsystems.framework.domain.BaseDomain;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Text;
+
+public class QuestionOptionSummary extends BaseDomain {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4569158260741601484L;
+	private Key surveyKey = null;
+	private String surveyCode = null;
+	private Key quesitonGroupKey = null;
+	private String questionGroupCode = null;
+	private Text questionOptionText = null;
+	public Key getSurveyKey() {
+		return surveyKey;
+	}
+	public void setSurveyKey(Key surveyKey) {
+		this.surveyKey = surveyKey;
+	}
+	public String getSurveyCode() {
+		return surveyCode;
+	}
+	public void setSurveyCode(String surveyCode) {
+		this.surveyCode = surveyCode;
+	}
+	public Key getQuesitonGroupKey() {
+		return quesitonGroupKey;
+	}
+	public void setQuesitonGroupKey(Key quesitonGroupKey) {
+		this.quesitonGroupKey = quesitonGroupKey;
+	}
+	public String getQuestionGroupCode() {
+		return questionGroupCode;
+	}
+	public void setQuestionGroupCode(String questionGroupCode) {
+		this.questionGroupCode = questionGroupCode;
+	}
+	public Text getQuestionOptionText() {
+		return questionOptionText;
+	}
+	public void setQuestionOptionText(Text questionOptionText) {
+		this.questionOptionText = questionOptionText;
+	}
+}
