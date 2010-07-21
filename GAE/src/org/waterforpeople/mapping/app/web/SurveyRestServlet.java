20859cec3b0a5b849f0dc528f14a2ba75b66e866
Dru Borden <dru.borden@gmail.com>
2010-07-21 23:33:46 +0000

    New Servlet for saving surveys via rest request

---
+++ GAE/src/org/waterforpeople/mapping/app/web/SurveyRestServlet.java
@@ -0,0 +1,32 @@
+package org.waterforpeople.mapping.app.web;
+
+import com.gallatinsystems.framework.rest.AbstractRestApiServlet;
+import com.gallatinsystems.framework.rest.RestRequest;
+import com.gallatinsystems.framework.rest.RestResponse;
+
+public class SurveyRestServlet extends AbstractRestApiServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1165507917062204859L;
+
+	@Override
+	protected RestRequest convertRequest() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected RestResponse handleRequest(RestRequest req) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void writeOkResponse(RestResponse resp) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
