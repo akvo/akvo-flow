edc9c9b1f4edfc04480d2b601429ccbcabefb50e
Dru Borden <dru.borden@gmail.com>
2010-08-05 03:05:15 +0000

    Fixed for new survey graph

--- GAE/src/org/waterforpeople/mapping/app/web/SurveyRestServlet.java
+++ GAE/src/org/waterforpeople/mapping/app/web/SurveyRestServlet.java
@@ -1,15 +1,16 @@
 package org.waterforpeople.mapping.app.web;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.waterforpeople.mapping.app.gwt.client.survey.QuestionDto;
 import org.waterforpeople.mapping.app.web.dto.SurveyRestRequest;
 
+import com.gallatinsystems.framework.dao.BaseDAO;
 import com.gallatinsystems.framework.rest.AbstractRestApiServlet;
 import com.gallatinsystems.framework.rest.RestRequest;
 import com.gallatinsystems.framework.rest.RestResponse;
@@ -22,6 +23,9 @@ import com.gallatinsystems.survey.domain.QuestionGroup;
 import com.gallatinsystems.survey.domain.QuestionOption;
 import com.gallatinsystems.survey.domain.Survey;
 import com.gallatinsystems.survey.domain.SurveyGroup;
+import com.gallatinsystems.survey.domain.Translation;
+import com.gallatinsystems.survey.domain.Question.Type;
+import com.gallatinsystems.survey.domain.Translation.ParentType;
 
 public class SurveyRestServlet extends AbstractRestApiServlet {
 	private static final Logger log = Logger.getLogger(TaskServlet.class
@@ -74,11 +78,15 @@ public class SurveyRestServlet extends AbstractRestApiServlet {
 			Integer questionOrder, Integer questionGroupOrder)
 			throws UnsupportedEncodingException {
 
+		// TODO:Change Impl Later if we support multiple langs
+		surveyName = parseLangMap(surveyName).get("en");
+		questionGroupName = parseLangMap(questionGroupName).get("en");
+
 		SurveyGroupDAO sgDao = new SurveyGroupDAO();
 		SurveyDAO surveyDao = new SurveyDAO();
 		QuestionGroupDao qgDao = new QuestionGroupDao();
 		QuestionDao qDao = new QuestionDao();
-
+		BaseDAO<Translation> tDao = new BaseDAO<Translation>(Translation.class);
 		SurveyGroup sg = null;
 		if (surveyGroupName != null) {
 			sg = sgDao.findBySurveyGroupName(surveyGroupName);
@@ -91,65 +99,68 @@ public class SurveyRestServlet extends AbstractRestApiServlet {
 		}
 
 		Survey survey = null;
-		if (sg.getSurveyList() != null)
-			for (Survey item : sg.getSurveyList()) {
-				if (survey.getCode().equals(surveyName)) {
-					survey = item;
-				}
-			}
+		String surveyPath = surveyGroupName;
+		survey = surveyDao.getByPath(surveyName, surveyPath);
 
 		if (survey == null) {
 			survey = new Survey();
-			survey.setNameMap(parseLangMap(surveyName));
-			survey.setPath(surveyGroupName);
+			survey.setName(surveyName);
+			survey.setPath(surveyPath);
 			survey.setCode(surveyName);
 		}
 
 		QuestionGroup qg = null;
-		if (survey.getQuestionGroupMap() != null)
-			for (Map.Entry<Integer, QuestionGroup> qgEntry : survey
-					.getQuestionGroupMap().entrySet()) {
-				if (qgEntry.getValue().getCode().equals(questionGroupName))
-					qg = qgEntry.getValue();
-			}
+		String qgPath = surveyGroupName + "/" + surveyName;
+		qg = qgDao.getByPath(questionGroupName, qgPath);
 
 		if (qg == null) {
-			
-			String path = surveyGroupName + "/" + surveyName;
 			qg = new QuestionGroup();
+			qg.setName(questionGroupName);
 			qg.setCode(questionGroupName);
-			qg.setNameMap(parseLangMap(questionGroupName));
-			qg.setPath(path);
-			qg.setDescMap(parseLangMap(questionGroupName));
-			qg.setNameMap(parseLangMap(questionGroupName));
+			qg.setPath(qgPath);
+			survey.addQuestionGroup(questionGroupOrder, qg);
 		}
 
-		survey.addQuestionGroup(questionGroupOrder, qg);
-
 		Question q = new Question();
-		q.setTextMap(parseLangMap(questionText));
+		String questionPath = qgPath + "/" + questionGroupName;
+		q.setText(parseLangMap(questionText).get("en"));
+		q.setPath(questionPath);
 		q.setOrder(questionOrder);
+		q.setReferenceId(questionOrder.toString());
+
+		for (Map.Entry<String, String> qTextItem : parseLangMap(questionText)
+				.entrySet()) {
+			if (!qTextItem.getKey().equals("en")) {
+				Translation t = new Translation();
+				t.setLanguageCode(qTextItem.getKey());
+				t.setText(qTextItem.getValue());
+				t.setParentType(ParentType.QUESTION_TEXT);
+				q.addTranslation(t);
+			}
+		}
 
 		if (questionType.equals("GEO"))
 			q.setType(Question.Type.GEO);
-		else if (questionType.equals("FREE"))
-			q.setType(Question.Type.TEXT);
+		else if (questionType.equals("FREE_TEXT"))
+			q.setType(Question.Type.FREE_TEXT);
 		else if (questionType.equals("OPTION")) {
-			// oc.setAllowMultipleFlag(allowMultipleFlag);
-			// oc.setAllowOtherFlag(allowOtherFlag);
-			// q.setType(QuestionDto.QuestionType.OPTION);
-			// String[] optionsParts = options.split(";");
-			// for (String option : optionsParts) {
-			// String[] parts = option.split("\\|");
-			// String value = parts[0];
-			// String text = parts[1];
-			// QuestionOption qo = new QuestionOption();
-			// qo.setCode(value);
-			// qo.setText(text);
-			// oc.addQuestionOption(qo);
-			// }
-			// ocDao.save(oc);
-			// q.setOptionContainer(oc);
+			q.setAllowMultipleFlag(allowMultipleFlag);
+			q.setAllowOtherFlag(allowOtherFlag);
+			q.setType(Type.OPTION);
+			QuestionOption qo = new QuestionOption();
+			for(QuestionOptionContainer qoc:parseQuestionOption(options)){
+				if(qoc.getLangCode().equals("en")){
+					qo.setCode(qoc.getOption());
+					qo.setText(qoc.getOption());
+				}else{
+					Translation t = new Translation();
+					t.setLanguageCode(qoc.langCode);
+					t.setText(qoc.getOption());
+					t.setParentType(ParentType.QUESTION_TEXT);
+					qo.addTranslation(t);
+				}
+			}
+			q.addQuestionOption(qo);
 		} else if (questionType.equals("PHOTO"))
 			q.setType(Question.Type.PHOTO);
 		else if (questionType.equals("NUMBER"))
@@ -159,24 +170,26 @@ public class SurveyRestServlet extends AbstractRestApiServlet {
 			q.setMandatoryFlag(mandatoryFlag);
 
 		// deal with options and dependencies
-		String questionPath = surveyGroupName + "/" + surveyName;
-		q.setPath(questionPath);
 
-		if (dependentQuestion != null && dependentQuestion.trim().length() > 1) {
+		 if (dependentQuestion != null && dependentQuestion.trim().length() > 1) {
 			String[] parts = dependentQuestion.split("\\|");
 			Integer quesitonOrderId = new Integer(parts[0]);
 			String answer = parts[1];
 			Question dependsOnQuestion = qDao.getByPath(quesitonOrderId,
 					questionPath);
 			if (dependsOnQuestion != null) {
-				// QuestionDependency qd = new QuestionDependency();
-				// qd.setAnswerValue(answer);
-				// qd.setQuestionId(dependsOnQuestion.getKey().getId());
-				// q.setDependQuestion(qd);
+				q.setDependentFlag(true);
+				q.setDependentQuestionId(dependsOnQuestion.getKey().getId());
+				q.setDependentQuestionAnswer(answer);
 			}
 
+		}else{
+			q.setDependentFlag(false);
 		}
+		qDao.save(q);
 		qg.addQuestion(questionOrder, q);
+		qgDao.save(qg);
+		surveyDao.save(survey);
 		sgDao.save(sg);
 		log.info("Just saved " + surveyGroupName + ":" + surveyName + ":"
 				+ questionGroupName + ":" + questionOrder);
@@ -192,5 +205,48 @@ public class SurveyRestServlet extends AbstractRestApiServlet {
 			langMap.put(langParts[0], langParts[1]);
 		}
 		return langMap;
+	}
+
+	private ArrayList<QuestionOptionContainer> parseQuestionOption(String questionOption) {
+		ArrayList<QuestionOptionContainer> qoList = new ArrayList<QuestionOptionContainer>();
+
+		String[] parts = questionOption.split("#");
+		for (String item : parts) {
+			for (Map.Entry<String, String> entry : parseLangMap(item)
+					.entrySet()) {
+				qoList
+						.add(new QuestionOptionContainer(entry.getKey(), entry
+								.getValue()));
+			}
+		}
+		return qoList;
+	}
+
+	private class QuestionOptionContainer {
+
+		public QuestionOptionContainer(String langCode, String optionText) {
+			this.setLangCode(langCode);
+			this.setOption(optionText);
+		}
+
+		private String langCode = null;
+		private String option = null;
+
+		public void setLangCode(String langCode) {
+			this.langCode = langCode;
+		}
+
+		public String getLangCode() {
+			return langCode;
+		}
+
+		public void setOption(String option) {
+			this.option = option;
+		}
+
+		public String getOption() {
+			return option;
+		}
+
 	}
 }
