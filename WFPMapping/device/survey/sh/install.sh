3d88de819efa0f0ff0b98d77a2a2eea437d69773
Christopher Fagiani <christopher.fagiani@gmail.com>
2010-08-07 20:09:53 +0000

    added variables for ANDROID_HOME and APK_HOME. you'll need to update them to point the the paths on your system

--- WFPMapping/device/survey/sh/install.sh
+++ WFPMapping/device/survey/sh/install.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 #
 
+
+#set this to match the paths on your system
+ANDROID_HOME=c:/android-sdk-windows
+APK_HOME=C:/Users/Chris/development/gallatin/wfp/trunk/WFPMapping/device/survey/bin
+
+
 rm /tmp/adbinstalllog.txt
 
 for arg
 do
-	/Users/dru/devenv/tools/android-sdk-mac_86-1/tools/adb -s "$arg" install ../bin/WFP_Map_Monitor.apk  >> /tmp/adbinstalllog.txt
-	/Users/dru/devenv/tools/android-sdk-mac_86-1/tools/adb -s "$arg" install ../bin/GPSStatus.apk  >> /tmp/adbinstalllog.txt
-	/Users/dru/devenv/tools/android-sdk-mac_86-1/tools/adb -s "$arg" install ../bin/Logcat.apk >> /tmp/adbinstalllog.txt
-	/Users/dru/devenv/tools/android-sdk-mac_86-1/tools/adb -s "$arg" install ../bin/FileManager-1.1.3.apk >> /tmp/adbinstalllog.txt 
+	$ANDROID_HOME/tools/adb -s "$arg" install $APK_HOME/WFP_Map_Monitor.apk  >> /tmp/adbinstalllog.txt
+	$ANDROID_HOME/tools/adb -s "$arg" install $APK_HOME/GPSStatus.apk  >> /tmp/adbinstalllog.txt
+	$ANDROID_HOME/tools/adb -s "$arg" install $APK_HOME/Logcat.apk >> /tmp/adbinstalllog.txt
+	$ANDROID_HOME/tools/adb -s "$arg" install $APK_HOME/FileManager-1.1.3.apk >> /tmp/adbinstalllog.txt 
 done
 
 loggg=$(cat /tmp/adbinstalllog.txt)
