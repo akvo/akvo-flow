30b9006d8fddab274b94eede328aab967dc3d355
Dru Borden <dru.borden@gmail.com>
2010-08-23 17:07:17 +0000

    Added a new action to build the map grouped by tech type
    action=buildMap

--- GAE/src/org/waterforpeople/mapping/app/web/WaterForPeopleMappingGoogleServlet.java
+++ GAE/src/org/waterforpeople/mapping/app/web/WaterForPeopleMappingGoogleServlet.java
@@ -2,6 +2,7 @@ package org.waterforpeople.mapping.app.web;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletOutputStream;
@@ -10,8 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.waterforpeople.mapping.dao.KMLDAO;
+import org.waterforpeople.mapping.helper.KMLHelper;
 
 import com.gallatinsystems.common.util.ZipUtil;
+import com.gallatinsystems.gis.map.dao.MapFragmentDao;
+import com.gallatinsystems.gis.map.domain.MapFragment;
+import com.gallatinsystems.gis.map.domain.MapFragment.FRAGMENTTYPE;
+import com.google.appengine.api.datastore.Blob;
 
 @SuppressWarnings("serial")
 public class WaterForPeopleMappingGoogleServlet extends HttpServlet {
@@ -26,7 +32,7 @@ public class WaterForPeopleMappingGoogleServlet extends HttpServlet {
 		@SuppressWarnings("unused")
 		String processFile = req.getParameter("processFile");
 		String showRegion = req.getParameter("showRegion");
-
+		String action = req.getParameter("action");
 		if (showKML != null) {
 			Long kmlKey = null;
 			if (req.getParameter("kmlID") != null) {
@@ -59,40 +65,18 @@ public class WaterForPeopleMappingGoogleServlet extends HttpServlet {
 					.generateRegionDocumentString("Regions.vm");
 			resp.setContentType("application/vnd.google-earth.kml+xml");
 			resp.getWriter().println(placemarksDocument);
-			// } else if (listFiles != null) {
-			// //TODO move this to a DAO
-			// // List processed files
-			// javax.jdo.Query query = pm.newQuery("select from  "
-			// + DeviceFiles.class.getName());
-			// List<DeviceFiles> entries = (List<DeviceFiles>) query.execute();
-			// StringBuilder sb = new StringBuilder();
-			// for (DeviceFiles df : entries) {
-			// sb.append(df.toString());
-			// }
-			// pm.close();
-			//
-			// resp.setContentType("text/plain");
-			// resp.getWriter().println(sb.toString());
-			//
-			// } else if (testVelocity != null) {
-			// KMLGenerator kmlGen = new KMLGenerator();
-			// String placemarksDocument = kmlGen
-			// .generateDocument("PlacemarkTabs.vm");
-			// resp.setContentType("text/plain");
-			// resp.getWriter().println(placemarksDocument);
-			// } else {
-			// //TODO move to DAO
-			// javax.jdo.Query query = pm.newQuery("select from  "
-			// + AccessPoint.class.getName());
-			// List<AccessPoint> entries = (List<AccessPoint>) query.execute();
-			// StringBuilder sb = new StringBuilder();
-			// for (AccessPoint ap : entries) {
-			// sb.append(ap.toString());
-			// }
-			// pm.close();
-			//
-			// resp.setContentType("text/plain");
-			// resp.getWriter().println(sb.toString());
+		}else if("buildMap".equals(action)){
+			KMLHelper kmlHelper = new KMLHelper();
+			MapFragmentDao mfDao = new MapFragmentDao();
+			kmlHelper.buildMap();
+			List<MapFragment> mfList = mfDao.searchMapFragments(null,null,null,null, FRAGMENTTYPE.GLOBAL_ALL_PLACEMARKS, "all");
+			Blob map = mfList.get(0).getBlob();
+			resp.setContentType("application/vnd.google-earth.kmz+xml");
+			ServletOutputStream out = resp.getOutputStream();
+			resp.setHeader("Content-Disposition",
+					"inline; filename=waterforpeoplemapping.kmz;");
+			out.write(map.getBytes());
+			out.flush();
 		}
 	}
 
