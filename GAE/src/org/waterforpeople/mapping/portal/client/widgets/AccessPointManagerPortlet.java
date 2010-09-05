66944616a4c4d24a08a860606b986925b77eeb22
Christopher Fagiani <christopher.fagiani@gmail.com>
2010-09-05 15:31:32 +0000

    added country code to AccessPointManager edit screen

--- GAE/src/org/waterforpeople/mapping/portal/client/widgets/AccessPointManagerPortlet.java
+++ GAE/src/org/waterforpeople/mapping/portal/client/widgets/AccessPointManagerPortlet.java
@@ -353,7 +353,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 					if (communityCode.length() > 10)
 						communityCode = communityCode.substring(0, 10);
 					accessPointFT.setWidget(i, 1, new Label(communityCode));
-				}
+				}				
 
 				if (apDto.getLatitude() != null && apDto.getLongitude() != null) {
 					accessPointFT.setWidget(i, 2, new Label(apDto.getLatitude()
@@ -553,22 +553,32 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 
 		accessPointDetail.setWidget(0, 0, new Label("Community Code: "));
 		TextBox communityCodeTB = new TextBox();
-		if (accessPointDto != null)
+		if (accessPointDto != null){
 			communityCodeTB.setText(accessPointDto.getCommunityCode());
+		}
 
 		accessPointDetail.setWidget(0, 1, communityCodeTB);
+		
+		accessPointDetail.setWidget(1, 0, new Label("Country Code: "));
+		TextBox countryCodeTB = new TextBox();
+		if (accessPointDto != null){
+			countryCodeTB.setText(accessPointDto.getCountryCode());
+		}
 
-		accessPointDetail.setWidget(1, 0, new Label("Latititude: "));
+		accessPointDetail.setWidget(1, 1, countryCodeTB);
+
+
+		accessPointDetail.setWidget(2, 0, new Label("Latititude: "));
 		TextBox latitudeTB = new TextBox();
 		if (accessPointDto != null)
 			latitudeTB.setText(accessPointDto.getLatitude().toString());
-		accessPointDetail.setWidget(1, 1, latitudeTB);
+		accessPointDetail.setWidget(2, 1, latitudeTB);
 
-		accessPointDetail.setWidget(2, 0, new Label("Longitude: "));
+		accessPointDetail.setWidget(3, 0, new Label("Longitude: "));
 		TextBox longitudeTB = new TextBox();
 		if (accessPointDto != null)
 			longitudeTB.setText(accessPointDto.getLongitude().toString());
-		accessPointDetail.setWidget(2, 1, longitudeTB);
+		accessPointDetail.setWidget(3, 1, longitudeTB);
 
 		if (accessPointDto.getLatitude() != null
 				&& accessPointDto.getLongitude() != null) {
@@ -584,56 +594,56 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 			accessPointDetail.getFlexCellFormatter().setRowSpan(0, 2, 5);
 		}
 
-		accessPointDetail.setWidget(3, 0, new Label("Collection Date: "));
+		accessPointDetail.setWidget(4, 0, new Label("Collection Date: "));
 		DateBox pickerCollectionDate = new DateBox();
 		if (accessPointDto != null)
 			pickerCollectionDate.setValue(accessPointDto.getCollectionDate());
-		accessPointDetail.setWidget(3, 1, pickerCollectionDate);
+		accessPointDetail.setWidget(4, 1, pickerCollectionDate);
 
-		accessPointDetail.setWidget(4, 0, new Label("Point Construction Date"));
+		accessPointDetail.setWidget(5, 0, new Label("Point Construction Date"));
 		DateBox pickerConstructionDate = new DateBox();
 		if (accessPointDto != null)
 			pickerConstructionDate.setValue(accessPointDto
 					.getConstructionDate());
-		accessPointDetail.setWidget(4, 1, pickerConstructionDate);
+		accessPointDetail.setWidget(5, 1, pickerConstructionDate);
 
-		accessPointDetail.setWidget(5, 0, new Label("Cost Per: "));
+		accessPointDetail.setWidget(6, 0, new Label("Cost Per: "));
 		TextBox costPerTB = new TextBox();
 		if (accessPointDto != null && accessPointDto.getCostPer() != null)
 			costPerTB.setText(accessPointDto.getCostPer().toString());
-		accessPointDetail.setWidget(5, 1, costPerTB);
+		accessPointDetail.setWidget(6, 1, costPerTB);
 
 		ListBox unitOfMeasureLB = new ListBox();
 		unitOfMeasureLB.addItem("ml");
 		unitOfMeasureLB.addItem("liters");
 		unitOfMeasureLB.addItem("ounces");
 		unitOfMeasureLB.addItem("gallons");
-		accessPointDetail.setWidget(5, 2, unitOfMeasureLB);
+		accessPointDetail.setWidget(6, 2, unitOfMeasureLB);
 
-		accessPointDetail.setWidget(6, 0, new Label(
+		accessPointDetail.setWidget(7, 0, new Label(
 				"Current Management Structure: "));
 		TextBox currentMgmtStructureTB = new TextBox();
 		if (accessPointDto != null)
 			currentMgmtStructureTB.setText(accessPointDto
 					.getCurrentManagementStructurePoint());
-		accessPointDetail.setWidget(6, 1, currentMgmtStructureTB);
+		accessPointDetail.setWidget(7, 1, currentMgmtStructureTB);
 
-		accessPointDetail.setWidget(7, 0, new Label("Description: "));
+		accessPointDetail.setWidget(8, 0, new Label("Description: "));
 		TextBox descTB = new TextBox();
 		if (accessPointDto != null)
 			descTB.setText(accessPointDto.getDescription());
-		accessPointDetail.setWidget(7, 1, descTB);
+		accessPointDetail.setWidget(8, 1, descTB);
 
-		accessPointDetail.setWidget(8, 0, new Label(
+		accessPointDetail.setWidget(9, 0, new Label(
 				"Number of Households using Point: "));
 		TextBox numHouseholdsTB = new TextBox();
 		if (accessPointDto != null
 				&& accessPointDto.getNumberOfHouseholdsUsingPoint() != null)
 			numHouseholdsTB.setText(accessPointDto
 					.getNumberOfHouseholdsUsingPoint().toString());
-		accessPointDetail.setWidget(8, 1, numHouseholdsTB);
+		accessPointDetail.setWidget(9, 1, numHouseholdsTB);
 
-		accessPointDetail.setWidget(9, 0, new Label("Photo Url: "));
+		accessPointDetail.setWidget(10, 0, new Label("Photo Url: "));
 		TextBox photoURLTB = new TextBox();
 		FormPanel form = new FormPanel();
 		form.setMethod(FormPanel.METHOD_POST);
@@ -641,11 +651,10 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 		form.setAction("/webapp/photoupload");
 		FileUpload upload = new FileUpload();
 		form.setWidget(upload);
-		accessPointDetail.setWidget(9, 3, form);
+		accessPointDetail.setWidget(10, 3, form);
 		Button submitUpload = new Button("Upload");
-		// accessPointDetail.setWidget(9,4, upload);
 		upload.setName("uploadFormElement");
-		accessPointDetail.setWidget(9, 4, submitUpload);
+		accessPointDetail.setWidget(10, 4, submitUpload);
 
 		form.addSubmitHandler(new SubmitHandler() {
 			@Override
@@ -659,7 +668,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 			public void onSubmitComplete(SubmitCompleteEvent event) {
 				Window.alert("File uploaded");
 				String fileName = ((FileUpload) ((FormPanel) accessPointDetail
-						.getWidget(9, 3)).getWidget()).getFilename();
+						.getWidget(10, 3)).getWidget()).getFilename();
 			
 				if(fileName.contains("/")){
 					fileName = fileName.substring(fileName.lastIndexOf("/")+1);
@@ -668,11 +677,11 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 					fileName = fileName.substring(fileName.lastIndexOf("\\")+1);
 				}
 				
-				((TextBox) accessPointDetail.getWidget(9, 1))
+				((TextBox) accessPointDetail.getWidget(10, 1))
 						.setText("http://waterforpeople.s3.amazonaws.com/images/"
 								+ fileName);
 
-				Image i = ((Image) accessPointDetail.getWidget(9, 2));
+				Image i = ((Image) accessPointDetail.getWidget(10, 2));
 				i.setHeight("200px");
 
 				if (i == null) {
@@ -681,7 +690,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 							.setUrl("http://waterforpeople.s3.amazonaws.com/images/"
 									+ fileName);
 					photo.setHeight("200px");
-					accessPointDetail.setWidget(9, 2, photo);
+					accessPointDetail.setWidget(10, 2, photo);
 				} else {
 					i.setUrl("http://waterforpeople.s3.amazonaws.com/images/"
 							+ fileName);
@@ -693,7 +702,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				((FormPanel) accessPointDetail.getWidget(9, 3)).submit();
+				((FormPanel) accessPointDetail.getWidget(10, 3)).submit();
 
 			}
 
@@ -702,17 +711,17 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 		if (accessPointDto != null) {
 			photoURLTB.setText(accessPointDto.getPhotoURL());
 			Image photo = new Image(accessPointDto.getPhotoURL());
-			accessPointDetail.setWidget(9, 2, photo);
+			accessPointDetail.setWidget(10, 2, photo);
 			photo.addClickHandler(new ClickHandler() {
 
 				@Override
 				public void onClick(ClickEvent event) {
-					((Image) accessPointDetail.getWidget(9, 2))
+					((Image) accessPointDetail.getWidget(10, 2))
 							.setVisible(false);
-					accessPointDetail.setWidget(9, 4, new Label(
+					accessPointDetail.setWidget(10, 4, new Label(
 							"Please wait while image is rotated 90 Degrees"));
 					svc.rotateImage(((TextBox) accessPointDetail
-							.getWidget(9, 1)).getText(),
+							.getWidget(10, 1)).getText(),
 							new AsyncCallback<Boolean>() {
 								@Override
 								public void onFailure(Throwable caught) {
@@ -724,11 +733,11 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 								public void onSuccess(Boolean result) {
 									Integer random = Random.nextInt();
 									Image photo = ((Image) accessPointDetail
-											.getWidget(9, 2));
-									accessPointDetail.getWidget(9, 4)
+											.getWidget(10, 2));
+									accessPointDetail.getWidget(10, 4)
 											.setVisible(false);
 									photo.setUrl(((TextBox) accessPointDetail
-											.getWidget(9, 1)).getText()
+											.getWidget(10, 1)).getText()
 											+ "?random=" + random);
 									photo.setVisible(true);
 								}
@@ -736,15 +745,15 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 				}
 			});
 		}
-		accessPointDetail.setWidget(9, 1, photoURLTB);
+		accessPointDetail.setWidget(10, 1, photoURLTB);
 
-		accessPointDetail.setWidget(10, 0, new Label("Photo Caption: "));
+		accessPointDetail.setWidget(11, 0, new Label("Photo Caption: "));
 		TextBox captionTB = new TextBox();
 		if (accessPointDto != null)
 			captionTB.setText(accessPointDto.getPointPhotoCaption());
-		accessPointDetail.setWidget(10, 1, captionTB);
+		accessPointDetail.setWidget(11, 1, captionTB);
 
-		accessPointDetail.setWidget(11, 0, new Label("Point Status: "));
+		accessPointDetail.setWidget(12, 0, new Label("Point Status: "));
 		statusLB = new ListBox();
 		statusLB.addItem("Functioning High");
 		statusLB.addItem("Functioning Ok");
@@ -768,9 +777,9 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 				statusLB.setSelectedIndex(4);
 			}
 		}
-		accessPointDetail.setWidget(11, 1, statusLB);
+		accessPointDetail.setWidget(12, 1, statusLB);
 
-		accessPointDetail.setWidget(12, 0, new Label("Point Type: "));
+		accessPointDetail.setWidget(13, 0, new Label("Point Type: "));
 
 		ListBox pointType = new ListBox();
 		pointType
@@ -795,14 +804,14 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 		} else {
 			pointType.setSelectedIndex(0);
 		}
-		accessPointDetail.setWidget(12, 1, pointType);
+		accessPointDetail.setWidget(13, 1, pointType);
 
-		accessPointDetail.setWidget(13, 0, new Label("Farthest Point From"));
+		accessPointDetail.setWidget(14, 0, new Label("Farthest Point From"));
 		TextBox farthestPointFromTB = new TextBox();
 		if (accessPointDto != null)
 			farthestPointFromTB.setText(accessPointDto
 					.getFarthestHouseholdfromPoint());
-		accessPointDetail.setWidget(13, 1, farthestPointFromTB);
+		accessPointDetail.setWidget(14, 1, farthestPointFromTB);
 		Label apId;
 		if (accessPointDto != null) {
 			apId = new Label(accessPointDto.getKeyId().toString());
@@ -810,7 +819,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 			apId = new Label("-1");
 		}
 		apId.setVisible(false);
-		accessPointDetail.setWidget(14, 1, apId);
+		accessPointDetail.setWidget(15, 1, apId);
 
 		// if(accessPointDto.getPointType().toString()!=null){
 		// type = accessPointDto.getPointType().toString();
@@ -873,8 +882,8 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 			}
 
 		});
-		accessPointDetail.setWidget(15, 0, saveButton);
-		accessPointDetail.setWidget(15, 1, cancelButton);
+		accessPointDetail.setWidget(16, 0, saveButton);
+		accessPointDetail.setWidget(16, 1, cancelButton);
 		accessPointDetail.setVisible(true);
 		mainVPanel.add(accessPointDetail);
 	}
@@ -882,7 +891,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 	private AccessPointDto buildAccessPointDto() {
 		AccessPointDto apDto = new AccessPointDto();
 
-		Label apId = (Label) accessPointDetail.getWidget(14, 1);
+		Label apId = (Label) accessPointDetail.getWidget(15, 1);
 		Long id = new Long(apId.getText());
 		if (id > -1) {
 			apDto.setKeyId(id);
@@ -893,30 +902,34 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 		String communityCode = communityCodeTB.getText();
 		apDto.setCommunityCode(communityCode);
 
-		TextBox latitudeTB = (TextBox) accessPointDetail.getWidget(1, 1);
+		TextBox countryCodeTB = (TextBox) accessPointDetail.getWidget(1, 1);
+		String countryCode = countryCodeTB.getText();
+		apDto.setCountryCode(countryCode);
+		
+		TextBox latitudeTB = (TextBox) accessPointDetail.getWidget(2, 1);
 		Double latitude = new Double(latitudeTB.getText());
 		apDto.setLatitude(latitude);
 
-		TextBox longitudeTB = (TextBox) accessPointDetail.getWidget(2, 1);
+		TextBox longitudeTB = (TextBox) accessPointDetail.getWidget(3, 1);
 		Double longitude = new Double(longitudeTB.getText());
 		apDto.setLongitude(longitude);
 
-		DateBox collectionDateTB = (DateBox) accessPointDetail.getWidget(3, 1);
+		DateBox collectionDateTB = (DateBox) accessPointDetail.getWidget(4, 1);
 
 		apDto.setCollectionDate(collectionDateTB.getValue());
 
 		DateBox constructionDateTB = (DateBox) accessPointDetail
-				.getWidget(4, 1);
+				.getWidget(5, 1);
 		apDto.setConstructionDate(constructionDateTB.getValue());
 
-		TextBox costPerTB = (TextBox) accessPointDetail.getWidget(5, 1);
+		TextBox costPerTB = (TextBox) accessPointDetail.getWidget(6, 1);
 		String costPerTemp = costPerTB.getText();
 
 		if (costPerTemp != null && costPerTemp.length() > 0) {
 			Double costPer = new Double(costPerTB.getText());
 			apDto.setCostPer(costPer);
 		}
-		ListBox unitOfMeasureLB = (ListBox) accessPointDetail.getWidget(5, 2);
+		ListBox unitOfMeasureLB = (ListBox) accessPointDetail.getWidget(6, 2);
 		if (unitOfMeasureLB.getSelectedIndex() == 0) {
 			// ml
 			UnitOfMeasureDto uom = new UnitOfMeasureDto();
@@ -946,15 +959,15 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 		}
 
 		TextBox currentMgmtStructureTB = (TextBox) accessPointDetail.getWidget(
-				6, 1);
+				7, 1);
 		String currentMgmtStructure = currentMgmtStructureTB.getText();
 		apDto.setCurrentManagementStructurePoint(currentMgmtStructure);
 
-		TextBox descTB = (TextBox) accessPointDetail.getWidget(7, 1);
+		TextBox descTB = (TextBox) accessPointDetail.getWidget(8, 1);
 		String desc = descTB.getText();
 		apDto.setDescription(desc);
 
-		TextBox numHouseholdsTB = (TextBox) accessPointDetail.getWidget(8, 1);
+		TextBox numHouseholdsTB = (TextBox) accessPointDetail.getWidget(9, 1);
 		String numHouseholds = numHouseholdsTB.getText();
 		if (numHouseholds != null && numHouseholds.trim().length() > 0) {
 			try {
@@ -965,15 +978,15 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 			}
 		}
 
-		TextBox photoURLTB = (TextBox) accessPointDetail.getWidget(9, 1);
+		TextBox photoURLTB = (TextBox) accessPointDetail.getWidget(10, 1);
 		String photoUrl = photoURLTB.getText();
 		apDto.setPhotoURL(photoUrl);
 
-		TextBox captionTB = (TextBox) accessPointDetail.getWidget(10, 1);
+		TextBox captionTB = (TextBox) accessPointDetail.getWidget(11, 1);
 		String caption = captionTB.getText();
 		apDto.setPointPhotoCaption(caption);
 
-		ListBox statusLB = (ListBox) accessPointDetail.getWidget(11, 1);
+		ListBox statusLB = (ListBox) accessPointDetail.getWidget(12, 1);
 		if (statusLB.getSelectedIndex() == 0) {
 			apDto.setPointStatus(AccessPointDto.Status.FUNCTIONING_HIGH);
 		} else if (statusLB.getSelectedIndex() == 1) {
@@ -986,7 +999,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 			apDto.setPointStatus(Status.OTHER);
 		}
 
-		ListBox pointTypeLB = (ListBox) accessPointDetail.getWidget(12, 1);
+		ListBox pointTypeLB = (ListBox) accessPointDetail.getWidget(13, 1);
 		Integer selectedIndex = pointTypeLB.getSelectedIndex();
 		String type = pointTypeLB.getItemText(selectedIndex);
 		if (type.equals("Water Point")) {
@@ -999,7 +1012,7 @@ public class AccessPointManagerPortlet extends LocationDrivenPortlet {
 			apDto.setPointType(AccessPointType.SCHOOL);
 		}
 
-		TextBox farthestPointFromTB = (TextBox) accessPointDetail.getWidget(13,
+		TextBox farthestPointFromTB = (TextBox) accessPointDetail.getWidget(14,
 				1);
 		String farthestPointFrom = farthestPointFromTB.getText();
 		apDto.setFarthestHouseholdfromPoint(farthestPointFrom);
