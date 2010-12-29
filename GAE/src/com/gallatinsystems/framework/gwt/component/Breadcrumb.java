7338ec50453b3580d772d735bd489a524ac8a93a
Christopher Fagiani <christopher.fagiani@gmail.com>
2010-12-29 22:36:46 +0000

    more styling. added survey group list widget implementation.

--- GAE/src/com/gallatinsystems/framework/gwt/component/Breadcrumb.java
+++ GAE/src/com/gallatinsystems/framework/gwt/component/Breadcrumb.java
@@ -1,15 +1,22 @@
 package com.gallatinsystems.framework.gwt.component;
 
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.user.client.ui.Label;
 
-public class Breadcrumb extends Label {
+public class Breadcrumb extends Label implements MouseOverHandler, MouseOutHandler {
 	private static final String BREADCRUMB_STYLE = "breadcrumb";
+	private static final String HOVER_STYLE = "red-hover";
 	private String targetNode;
 
 	public Breadcrumb(String text, String targetNode) {
 		super();
 		setText(text);
 		setStylePrimaryName(BREADCRUMB_STYLE);
+		addMouseOutHandler(this);
+		addMouseOverHandler(this);
 		this.targetNode = targetNode;
 	}
 
@@ -30,5 +37,15 @@ public class Breadcrumb extends Label {
 	
 	public int hashCode(){
 		return getText().hashCode();
+	}
+
+	@Override
+	public void onMouseOut(MouseOutEvent event) {
+		removeStyleName(HOVER_STYLE);
+	}
+
+	@Override
+	public void onMouseOver(MouseOverEvent event) {
+		addStyleName(HOVER_STYLE);
 	}
 }
--- GAE/src/org/waterforpeople/mapping/portal/client/widgets/component/SurveyGroupListWidget.java
+++ GAE/src/org/waterforpeople/mapping/portal/client/widgets/component/SurveyGroupListWidget.java
@@ -1,35 +1,50 @@
 package org.waterforpeople.mapping.portal.client.widgets.component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.waterforpeople.mapping.app.gwt.client.survey.SurveyGroupDto;
 import org.waterforpeople.mapping.app.gwt.client.survey.SurveyService;
 import org.waterforpeople.mapping.app.gwt.client.survey.SurveyServiceAsync;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-public class SurveyGroupListWidget extends Composite {
+public class SurveyGroupListWidget extends Composite implements ClickHandler, MouseOverHandler, MouseOutHandler{
 
 	private static final String LOADING_CSS = "loading-label";
 	private static final String LOADING_TEXT = "Loading. Please wait...";
+	private static final String LIST_ITEM_CSS = "clickable-list-item";
+	private static final String LIST_ITEM_HOVER_CSS = "red-hover";
+	
 
 	private VerticalPanel panel;
 	private SurveyServiceAsync surveyService;
 	private Label loadingLabel;
+	private Map<Widget,SurveyGroupDto> groupMap;
 
 	public SurveyGroupListWidget() {
 		panel = new VerticalPanel();
 		surveyService = GWT.create(SurveyService.class);
+		groupMap = new HashMap<Widget,SurveyGroupDto>();
 		loadingLabel = new Label();
 		loadingLabel.setText(LOADING_TEXT);
 		loadingLabel.setStylePrimaryName(LOADING_CSS);
 		panel.add(loadingLabel);
 		initWidget(panel);
+		loadData();
 	}
 
 	public void loadData() {
@@ -47,10 +62,34 @@ public class SurveyGroupListWidget extends Composite {
 						loadingLabel.setVisible(false);
 						if(result != null && result.size()>0){
 							Grid dataGrid = new Grid(result.size(),2);
-							
+							for(int i=0; i < result.size(); i++){
+								Label l = new Label();									
+								l.setStylePrimaryName(LIST_ITEM_CSS);
+								l.setText(result.get(i).getDisplayName());
+								l.addMouseOutHandler(SurveyGroupListWidget.this);
+								l.addMouseOverHandler(SurveyGroupListWidget.this);
+								l.addClickHandler(SurveyGroupListWidget.this);
+								dataGrid.setWidget(i, 0,l);
+								groupMap.put(l,result.get(i));
+							}
+							panel.add(dataGrid);
 						}
 					}
 				});
 	}
 
+	@Override
+	public void onMouseOut(MouseOutEvent event) {
+		((Label)event.getSource()).removeStyleName(LIST_ITEM_HOVER_CSS);
+	}
+
+	@Override
+	public void onMouseOver(MouseOverEvent event) {
+		((Label)event.getSource()).addStyleName(LIST_ITEM_HOVER_CSS);
+	}
+
+	@Override
+	public void onClick(ClickEvent event) {
+	//TODO: load next page	
+	}
 }
