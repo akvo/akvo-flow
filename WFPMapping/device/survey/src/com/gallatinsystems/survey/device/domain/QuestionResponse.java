cd1fce43230a4b8dc56018336f05c0bc8f0ed94a
Christopher Fagiani <christopher.fagiani@gmail.com>
2011-01-26 14:25:34 +0000

    fixed isValid logic on device for geo questions

--- WFPMapping/device/survey/src/com/gallatinsystems/survey/device/domain/QuestionResponse.java
+++ WFPMapping/device/survey/src/com/gallatinsystems/survey/device/domain/QuestionResponse.java
@@ -60,14 +60,36 @@ public class QuestionResponse {
 	}
 
 	public boolean isValid() {
-		if (!ConstantUtil.OTHER_RESPONSE_TYPE.equals(type)) {
-			// if the response isn't "OTHER" then we have to check that it has a
-			// value that isn't just a blank
-			if (value == null || value.trim().length() == 0) {
-				return false;
+			if (!ConstantUtil.OTHER_RESPONSE_TYPE.equals(type)) {
+				// if the response isn't "OTHER" then we have to check that it has a
+				// value that isn't just a blank
+				if (value == null || value.trim().length() == 0) {
+					return false;
+				}
+				//now check that, if it's a geo question, we have something specified
+				if(ConstantUtil.GEO_RESPONSE_TYPE.equals(type)){
+				  //at this point, we know value isn't null
+				  StringTokenizer strTok = new StringTokenizer(value,"|");
+				  if(strTok.countTokens() >=2){
+					  //at least the first 2 tokens must be numeric
+					  for(int i=0; i < 2; i++){
+						String token = strTok.nextToken();
+						try{
+							if(token.trim().length()>0){
+								Double d = Double.parseDouble(token);
+							}else{
+								return false;
+							}
+						}catch(NumberFormatException e){
+							return false;
+						}
+					  }
+				  }else{
+					  return false;
+				  }
+				}
 			}
-		}
-		return true;
+			return true;
 	}
 
 	public String getQuestionId() {
