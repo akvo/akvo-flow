64ab9ab98f7a0bdcfe6d0d4c4d2e8801b4606684
Christopher Fagiani <christopher.fagiani@gmail.com>
2011-02-06 14:22:26 +0000

    added logging of checksum of last report and logic to supress email if no changes exist

--- GAE/src/com/gallatinsystems/notification/dao/NotificationSubscriptionDao.java
+++ GAE/src/com/gallatinsystems/notification/dao/NotificationSubscriptionDao.java
@@ -1,9 +1,15 @@
 package com.gallatinsystems.notification.dao;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.jdo.PersistenceManager;
 
 import com.gallatinsystems.framework.dao.BaseDAO;
+import com.gallatinsystems.framework.servlet.PersistenceFilter;
+import com.gallatinsystems.notification.domain.NotificationHistory;
 import com.gallatinsystems.notification.domain.NotificationSubscription;
 
 /**
@@ -27,6 +33,64 @@ public class NotificationSubscriptionDao extends
 	public List<NotificationSubscription> listUnexpiredNotifications() {
 
 		return listByProperty("expiryDate", new Date(), "Date", LTE_OP);
+	}
+
+	/**
+	 * finds the notificaiton history record (if it exists) for this type/entity
+	 * combo
+	 * 
+	 * @param type
+	 * @param entityId
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public NotificationHistory findNotificationHistory(String type,
+			Long entityId) {
+		PersistenceManager pm = PersistenceFilter.getManager();
+		javax.jdo.Query query = pm.newQuery(NotificationHistory.class);
+		Map<String, Object> paramMap = null;
+
+		StringBuilder filterString = new StringBuilder();
+		StringBuilder paramString = new StringBuilder();
+		paramMap = new HashMap<String, Object>();
+
+		appendNonNullParam("type", filterString, paramString, "String", type,
+				paramMap);
+		appendNonNullParam("entityId", filterString, paramString, "Long",
+				entityId, paramMap);
+
+		query.setFilter(filterString.toString());
+		query.declareParameters(paramString.toString());
+
+		List<NotificationHistory> results = (List<NotificationHistory>) query
+				.executeWithMap(paramMap);
+		if (results != null && results.size() > 0) {
+			return results.get(0);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * saves the notification history object passed in, incrementing the count
+	 * and updating the date before saving.
+	 * 
+	 * @param h
+	 * @return
+	 */
+	public static synchronized NotificationHistory saveNotificationHistory(
+			NotificationHistory h) {
+		if (h != null) {
+			if (h.getCount() == null) {
+				h.setCount(1L);
+			} else {
+				h.setCount(h.getCount() + 1);
+			}
+			h.setLastNotification(new Date());
+			NotificationSubscriptionDao dao = new NotificationSubscriptionDao();
+			h = dao.save(h);
+		}
+		return h;
 	}
 
 }
--- GAE/src/org/waterforpeople/mapping/notification/RawDataReportNotificationHandler.java
+++ GAE/src/org/waterforpeople/mapping/notification/RawDataReportNotificationHandler.java
@@ -2,13 +2,17 @@ package org.waterforpeople.mapping.notification;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 import org.waterforpeople.mapping.dataexport.RawDataExporter;
 
+import com.gallatinsystems.common.util.MD5Util;
 import com.gallatinsystems.common.util.MailUtil;
 import com.gallatinsystems.common.util.PropertyUtil;
 import com.gallatinsystems.notification.NotificationHandler;
 import com.gallatinsystems.notification.NotificationRequest;
+import com.gallatinsystems.notification.dao.NotificationSubscriptionDao;
+import com.gallatinsystems.notification.domain.NotificationHistory;
 
 /**
  * notifier that is capable of generating the raw data report for a survey and
@@ -22,9 +26,14 @@ public class RawDataReportNotificationHandler implements NotificationHandler {
 	public static final String TYPE = "rawDataReport";
 	private final static String EMAIL_FROM_ADDRESS_KEY = "emailFromAddress";
 	private static String FROM_ADDRESS;
+	private static final Logger log = Logger
+			.getLogger(RawDataReportNotificationHandler.class.getName());
+
+	private NotificationSubscriptionDao dao;
 
 	public RawDataReportNotificationHandler() {
 		super();
+		dao = new NotificationSubscriptionDao();
 		FROM_ADDRESS = PropertyUtil.getProperty(EMAIL_FROM_ADDRESS_KEY);
 	}
 
@@ -39,11 +48,29 @@ public class RawDataReportNotificationHandler implements NotificationHandler {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		PrintWriter pw = new PrintWriter(bos);
 		exporter.export(serverBase, entityId, pw);
-		MailUtil.sendMail(FROM_ADDRESS, destinations,
-				NotificationRequest.DELIMITER, "FLOW Raw Data Report",
-				"Please see the latest raw data report", bos.toByteArray(),
-				"rawDataReport.csv", "text/plain");
+		NotificationHistory hist = getHistory(TYPE, entityId);
+		String newChecksum = MD5Util.generateChecksum(bos.toByteArray());
+		if (hist.getChecksum() == null
+				|| !hist.getChecksum().equals(newChecksum)) {
+			hist.setChecksum(newChecksum);
+			MailUtil.sendMail(FROM_ADDRESS, destinations,
+					NotificationRequest.DELIMITER, "FLOW Raw Data Report",
+					"Please see the latest raw data report", bos.toByteArray(),
+					"rawDataReport.csv", "text/plain");
+			NotificationSubscriptionDao.saveNotificationHistory(hist);
+		}
 		pw.close();
+	}
+
+	protected NotificationHistory getHistory(String type, Long id) {
+
+		NotificationHistory hist = dao.findNotificationHistory(type, id);
+		if (hist == null) {
+			hist = new NotificationHistory();
+			hist.setType(type);
+			hist.setEntityId(id);
+		}
+		return hist;
 	}
 
 }
