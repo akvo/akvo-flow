13efb7d20bb154a558ed297ceec3be268487354d
Christopher Fagiani <christopher.fagiani@gmail.com>
2011-05-22 18:07:46 +0000

    changed question response text to text area and put listener to prevent > 500 chars.

--- GAE/src/org/waterforpeople/mapping/surveyentry/client/component/TextQuestionWidget.java
+++ GAE/src/org/waterforpeople/mapping/surveyentry/client/component/TextQuestionWidget.java
@@ -3,10 +3,12 @@ package org.waterforpeople.mapping.surveyentry.client.component;
 import org.waterforpeople.mapping.app.gwt.client.survey.QuestionDto;
 import org.waterforpeople.mapping.app.gwt.client.surveyinstance.QuestionAnswerStoreDto;
 
+import com.gallatinsystems.framework.gwt.util.client.MessageDialog;
 import com.gallatinsystems.framework.gwt.util.client.ViewUtil;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 
 /**
@@ -15,10 +17,14 @@ import com.google.gwt.user.client.ui.TextBox;
  * @author Christopher Fagiani
  * 
  */
-public class TextQuestionWidget extends QuestionWidget {
-	private TextBox textBox;
+public class TextQuestionWidget extends QuestionWidget implements
+		KeyPressHandler {
+	private static final String MAX_WIDTH = "300px";
+	private static final String TEXT_HEIGHT = "300px";
+	private static final int MAX_TXT_LENGTH = 500;
+
+	private TextArea textBox;
 	private boolean isNumeric;
-	static final String MAX_WIDTH="300px";
 
 	public TextQuestionWidget(QuestionDto q, QuestionAnswerStoreDto a,
 			boolean numericOnly) {
@@ -51,8 +57,10 @@ public class TextQuestionWidget extends QuestionWidget {
 
 	@Override
 	protected void constructResponseUi() {
-		textBox = new TextBox();
+		textBox = new TextArea();
+		textBox.setHeight(TEXT_HEIGHT);
 		textBox.setWidth(MAX_WIDTH);
+		textBox.addKeyPressHandler(this);
 		getPanel().add(textBox);
 		if (getAnswer().getKeyId() != null) {
 			textBox.setText(getAnswer().getValue());
@@ -63,8 +71,8 @@ public class TextQuestionWidget extends QuestionWidget {
 	protected void captureAnswer() {
 		if (ViewUtil.isTextPopulated(textBox)) {
 			String answerTextValue = textBox.getText().trim();
-			if(answerTextValue.contains(",")){
-				
+			if (answerTextValue.contains(",")) {
+
 			}
 			getAnswer().setValue(answerTextValue);
 		}
@@ -75,4 +83,14 @@ public class TextQuestionWidget extends QuestionWidget {
 		textBox.setText("");
 	}
 
+	@Override
+	public void onKeyPress(KeyPressEvent event) {
+		if (textBox != null && textBox.getText() != null
+				&& textBox.getText().length() > MAX_TXT_LENGTH) {
+			MessageDialog dia = new MessageDialog(TEXT_CONSTANTS.inputError(),
+					TEXT_CONSTANTS.textMustBeLessThan500Chars());
+			dia.showCentered();
+			textBox.setText(textBox.getText().substring(0, MAX_TXT_LENGTH - 1));
+		}
+	}
 }
