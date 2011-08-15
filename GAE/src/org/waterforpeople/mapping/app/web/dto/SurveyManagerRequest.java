f4dadeec2c50620c81f509cb4a41d7b4225db15f
Christopher Fagiani <christopher.fagiani@gmail.com>
2011-08-15 13:40:48 +0000

    added population of deviceid and phone number

--- GAE/src/org/waterforpeople/mapping/app/web/dto/SurveyManagerRequest.java
+++ GAE/src/org/waterforpeople/mapping/app/web/dto/SurveyManagerRequest.java
@@ -18,21 +18,32 @@ public class SurveyManagerRequest extends RestRequest {
 	private static final long serialVersionUID = -1914332708852551948L;
 
 	public static final String GET_AVAIL_DEVICE_SURVEY_ACTION = "getAvailableSurveysDevice";
-	public static final String GET_SURVEY_HEADER_ACTION  = "getSurveyHeader";
+	public static final String GET_SURVEY_HEADER_ACTION = "getSurveyHeader";
 	public static final String GET_ZIP_FILE_URL_ACTION = "getZipFileUrls";
-	
 
 	private static final String SURVEY_INSTANCE_PARAM = "surveyInstanceId";
 	private static final String SURVEY_ID_PARAM = "surveyId";
 	private static final String SURVEY_DOC_PARAM = "surveyDocument";
 	private static final String PHONE_NUM_PARAM = "devicePhoneNumber";
+	@Deprecated
+	private static final String PHONE_NUM_ALT_PARAM = "phoneNumber";
 	private static final String FILE_START_DATE_PARAM = "startDate";
-	
+	private static final String DEVICE_ID_PARAM = "devId";
+
 	private Long surveyId;
 	private Long surveyInstanceId;
 	private String surveyDoc;
 	private String phoneNumber;
 	private Date startDate;
+	private String deviceId;
+
+	public String getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
 
 	public Long getSurveyId() {
 		return surveyId;
@@ -65,12 +76,13 @@ public class SurveyManagerRequest extends RestRequest {
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	public void setStartDate(String dateString) throws Exception{
-		try{
+
+	public void setStartDate(String dateString) throws Exception {
+		try {
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd");
 			Date convertedDate = dateFormat.parse(dateString);
 			setStartDate(convertedDate);
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			throw new Exception("Could not parse date param");
 		}
 	}
@@ -80,9 +92,13 @@ public class SurveyManagerRequest extends RestRequest {
 		surveyInstanceId = parseLong(req.getParameter(SURVEY_INSTANCE_PARAM),
 				SURVEY_INSTANCE_PARAM);
 		phoneNumber = req.getParameter(PHONE_NUM_PARAM);
+		if(phoneNumber == null || phoneNumber.trim().length()==0){
+			phoneNumber = req.getParameter(PHONE_NUM_ALT_PARAM);
+		}
+		deviceId= req.getParameter(DEVICE_ID_PARAM);
 		surveyId = parseLong(req.getParameter(SURVEY_ID_PARAM), SURVEY_ID_PARAM);
 		surveyDoc = req.getParameter(SURVEY_DOC_PARAM);
-		if(req.getParameter(FILE_START_DATE_PARAM)!=null)
+		if (req.getParameter(FILE_START_DATE_PARAM) != null)
 			setStartDate(req.getParameter(FILE_START_DATE_PARAM));
 	}
 
