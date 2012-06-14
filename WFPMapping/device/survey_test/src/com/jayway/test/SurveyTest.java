8208d13d1a3758343576e65499f0b7f71cafee3a
Mark Tiele Westra <m.t.westra@akvo.org>
2012-06-14 11:56:47 +0200

    added many tests

--- WFPMapping/device/survey_test/src/com/jayway/test/SurveyTest.java
+++ WFPMapping/device/survey_test/src/com/jayway/test/SurveyTest.java
@@ -12,6 +12,9 @@ package com.jayway.test;
 
 import com.gallatinsystems.survey.device.activity.SurveyHomeActivity;
 import com.jayway.android.robotium.solo.Solo;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.Smoke;
 import android.util.Log;
@@ -26,13 +29,22 @@ import java.util.TreeMap;
 public class SurveyTest extends ActivityInstrumentationTestCase2<SurveyHomeActivity>{
 
 	private Solo solo;
+	private Solo solo1;
+	
+	public void launchActivity(String packageName, String className) { 
+	    Intent intent = new Intent(); 
+	    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+	    intent.setClassName(packageName, className); 
+	    getInstrumentation().getContext().startActivity(intent); 
+	} 
 	
 	public SurveyTest() {
 		super("com.gallatinsystems.survey.device", SurveyHomeActivity.class);
 	}
 
 	public void setUp() throws Exception {
-		solo = new Solo(getInstrumentation(), getActivity());		
+		Activity activity=getActivity();		
+		solo = new Solo(getInstrumentation(), activity);		
 	}
 	
 	
@@ -67,9 +79,26 @@ public class SurveyTest extends ActivityInstrumentationTestCase2<SurveyHomeActiv
 		solo.goBack();
 		
 	}
-
+	
+	private void deleteAllData() {
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Delete All Data");
+		solo.enterText(0,"12345");
+		solo.clickOnButton("OK");
+		solo.clickOnButton("OK");
+		solo.sleep(2000);
+		solo.goBack();
+	}
+	
 	@Smoke
-	public void t1estCreateUser() throws Exception {
+	public void testAppOpen() throws Exception {
+		assertEquals("App not opening correctly", true, solo.searchText("^Manage Users$")); 
+		assertEquals("App not opening correctly", true, solo.searchText("^Review Saved Surveys$"));
+		assertEquals("App not opening correctly", true, solo.searchText("^Settings$")); 
+	}
+	
+	@Smoke
+	public void testCreateUser() throws Exception {
 		////////////////  SCENARIO - CREATE A USER /////////////////
 		// go to manage users 
 		deleteTestUsers();
@@ -98,7 +127,7 @@ public class SurveyTest extends ActivityInstrumentationTestCase2<SurveyHomeActiv
 	}
 	
 	@Smoke
-	public void t1estEditUser() throws Exception {
+	public void testEditUser() throws Exception {
 		//////////////// SCENARIO -  EDIT A USER /////////////////////
 		deleteTestUsers();
 		createUser("Test User 2","name-2.lastname@akvo.org");
@@ -123,7 +152,7 @@ public class SurveyTest extends ActivityInstrumentationTestCase2<SurveyHomeActiv
 	}
 	
 	@Smoke
-	public void t1estDeleteUser() throws Exception {
+	public void testDeleteUser() throws Exception {
 		////////////////SCENARIO -  Delete A USER /////////////////////	
 		deleteTestUsers();
 		createUser("Test User 3","name-3.lastname@akvo.org");
@@ -134,7 +163,7 @@ public class SurveyTest extends ActivityInstrumentationTestCase2<SurveyHomeActiv
 	}
 	
 	@Smoke
-	public void t1estSelectUser() throws Exception {
+	public void testSelectUser() throws Exception {
 		////////////////SCENARIO -  Delete A USER /////////////////////	
 		deleteTestUsers();
 		createUser("Test User 4","name-4.lastname@akvo.org");
@@ -144,12 +173,251 @@ public class SurveyTest extends ActivityInstrumentationTestCase2<SurveyHomeActiv
 	}
 	
 	@Smoke
-	public void testFillAllQuestions() throws Exception {
+	public void testDownloadSurveys() throws Exception {
+		deleteAllData();
+		assertEquals("Survey should not be here", false, solo.searchText("FLOW test survey")); 	
+		
+		//leave app
+		solo.goBack();
+		solo.goBack();
+		
+		//start app
+		launchActivity("com.gallatinsystems.survey.device","com.gallatinsystems.survey.device.activity.SurveyHomeActivity");
+		solo=new Solo(getInstrumentation());
+		
+		//start app again
+		solo.goBack();
+		solo.goBack();
+		launchActivity("com.gallatinsystems.survey.device","com.gallatinsystems.survey.device.activity.SurveyHomeActivity");
+		solo=new Solo(getInstrumentation());
+		
+		// check if expected surveys are there now
+		assertEquals("Survey 1 should be here", true, solo.searchText("FLOW test survey 1")); 	
+		assertEquals("Survey 2 should be here", true, solo.searchText("FLOW test survey 2")); 
+	}
+	
+	public void testDownloadExtraSurvey() throws Exception {
+		deleteAllData();
+		assertEquals("Survey should not be here", false, solo.searchText("FLOW test survey")); 	
+		
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Download Survey");
+		
+		solo.enterText(0,"12345");
+		solo.clickOnButton("OK");
+		
+		solo.enterText(0,"686193");
+		solo.clickOnButton("OK");
+		solo.goBack();
+		solo.sleep(500);
+		assertEquals("Survey FLOW test survey 4 should be here", true, solo.searchText("FLOW test survey 4")); 	
+		
+	}
+	
+	public void testDeleteAllData() throws Exception {
+		//create user
+		createUser("Test User 2","name-2.lastname@akvo.org");
+		
+		//download survey
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Download Survey");
+		
+		solo.enterText(0,"12345");
+		solo.clickOnButton("OK");
+		
+		solo.enterText(0,"686193");
+		solo.clickOnButton("OK");
+		solo.goBack();
+		solo.sleep(500);
+		
+		// delete all data
+		deleteAllData();
+		assertEquals("Survey should not be here", false, solo.searchText("FLOW test survey")); 	
+		solo.clickOnText("User Management");
+		assertEquals("User should not be here", false, solo.searchText("Test User")); 	
+		
+	}
+	
+	@Smoke
+	public void testOpenSurvey() throws Exception {
+		deleteTestUsers();
+		
+		solo.clickOnText("FLOW test survey 1");
+		assertEquals("Survey should not be selectable without user", true, solo.searchText("Please click the Manage Users icon and choose a user before continuing.")); 	
+		
+		solo.clickOnButton("OK");
+		createAndSelectUser("Test User 1","name-1.lastname@akvo.org");
+		
+		// now I should be able to select the survey
+		solo.clickOnText("FLOW test survey 1");
+		assertEquals("Survey not selected", true, solo.searchText("Group 1.*")); 	
+	}	
+	
+	@Smoke
+	public void testReloadSurveys() throws Exception {
+		deleteAllData();
+		assertEquals("Survey should not be here", false, solo.searchText("FLOW test survey")); 	
+		
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Reload all surveys");
+		solo.enterText(0,"12345");
+		solo.clickOnButton("OK");
+		solo.clickOnButton("OK");
+		solo.sleep(3000);
+		solo.goBack();
+		
+		assertEquals("Survey 1 should be here", true, solo.searchText("FLOW test survey 1")); 	
+		assertEquals("Survey 2 should be here", true, solo.searchText("FLOW test survey 2")); 	
+		
+	}
+	
+	@Smoke
+	public void testDependencies() throws Exception {
+		deleteTestUsers();
+		createAndSelectUser("Test User 1","name-1.lastname@akvo.org");
+		
+		// make sure we can select FLOW test survey 2
+		solo.clickOnText("FLOW test survey 2.*");
+		assertEquals("Survey not selected", true, solo.searchText("Group 1.*")); 	
+	
+		// click on first option of button 1 - nothing should be visible now
+		solo.clickOnRadioButton(0);
+		solo.sleep(1000);
+		// make sure questions are not visible
+		assertEquals("question 2a should not be here", false, solo.searchText("question 2a",true)); 
+		assertEquals("question 3a should not be here", false, solo.searchText("question 3a",true)); 
+		
+		// make sure group 2 is still empty
+		solo.clickOnText("Group 2");
+		assertEquals("question 1b should not be here", false, solo.searchText("question 1b",true)); 
+		assertEquals("question 2b should not be here", false, solo.searchText("question 2b",true)); 
+		solo.clickOnText("Group 1");
+		
+		// option field
+		solo.clickOnRadioButton(2);
+		assertEquals("button 3 of question 2 not selected", true, solo.searchText("question 2a",true)); 
+		
+		solo.clickOnRadioButton(4);
+		assertEquals("button 2 of question 3 not selected", true, solo.searchText("question 3a",true)); 
+		
+		solo.clickOnRadioButton(8);
+		
+		// check if group 2 now shows right questions
+		solo.clickOnText("Group 2");
+		assertEquals("question 1b should be here", true, solo.searchText("question 1b",true)); 
+		assertEquals("question 2b should be here", true, solo.searchText("question 2b",true)); 
+		
+		solo.clickOnRadioButton(2);
+		solo.clickOnRadioButton(5);
+			
+	}
+	
+	@Smoke
+	public void testKeepLastSelectedUserLoggedIn() throws Exception {
+		deleteTestUsers();
+		createAndSelectUser("Test User 1","name-1.lastname@akvo.org");
+		assertEquals("Test user 1 not selected", true, solo.searchText("Test User 1",true)); 
+		
+		//leave app
+		solo.goBack();
+		solo.goBack();
+		
+		//start app
+		launchActivity("com.gallatinsystems.survey.device","com.gallatinsystems.survey.device.activity.SurveyHomeActivity");
+		solo=new Solo(getInstrumentation());
+		
+		// User should be gone now.
+		assertEquals("Test user 1 still selected", false, solo.searchText("Test User 1",true)); 
+		
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Preferences");
+		solo.clickOnCheckBox(0);
+		solo.goBack();
+		solo.goBack();
+		
+		// select user again
+		solo.clickOnMenuItem("Manage Users");
+		solo.clickOnText("Test User 1");
+	
+		//leave app
+		solo.goBack();
+		solo.goBack();
+				
+		//start app
+		launchActivity("com.gallatinsystems.survey.device","com.gallatinsystems.survey.device.activity.SurveyHomeActivity");
+		solo=new Solo(getInstrumentation());
+				
+		// User should be selected now.
+		assertEquals("Test user 1 not selected", true, solo.searchText("Test User 1",true)); 
+		
+		
+	}
+	
+	public void testChangeSurveyLanguage() throws Exception {
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Preferences");
+		solo.clickOnImageButton(2);
+		
+		solo.sleep(500);
+		// if Spanish is selected, uncheck
+		if (solo.isTextChecked("Spanish")) {
+			solo.clickOnText("Spanish");
+			solo.clickOnButton("OK");
+			solo.goBack();
+			solo.goBack();
+			solo.clickOnMenuItem("Settings");
+			solo.clickOnText("Preferences");
+			solo.clickOnImageButton(2);
+		}
+
+		solo.clickOnText("Spanish");
+		solo.clickOnButton("OK");
+		solo.goBack();
+		solo.goBack();
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Preferences");
+		solo.clickOnImageButton(2);
+		solo.sleep(500);
+		// check if spanish is still selected
+		assertEquals("Language spanish not selected.", true, solo.isTextChecked("Spanish")); 
+	}
+	
+	public void testSetDeviceIdentifier() throws Exception {
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Preferences");
+		solo.clickOnImageButton(8);
+	
+		solo.enterText(0,"12345");
+		solo.clickOnButton("OK");
+		solo.clearEditText(0);
+		solo.clickOnButton("OK");
+		solo.sleep(500);
+		
+		assertEquals("device identifier not cleared", false, solo.searchText("FLOW test ID")); 	
+		solo.goBack();
+		solo.goBack();
+		
+		// fill in device id
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Preferences");
+		solo.clickOnImageButton(8);
+		solo.enterText(0,"12345");
+		solo.clickOnButton("OK");
+		solo.clearEditText(0);
+		solo.enterText(0,"FLOW test ID");
+		solo.clickOnButton("OK");
+		
+		
+		assertEquals("device identifier not set", true, solo.searchText("FLOW test ID")); 	
+	}
+	
+	@Smoke
+	public void testFillQuestions() throws Exception {
 		////////////////SCENARIO -  Delete A USER /////////////////////	
-		//deleteTestUsers();
-		//createAndSelectUser("Test User 1","name-1.lastname@akvo.org");
-		selectUser("Test User 1");
-		solo.clickOnText("Robotium test survey 1.*");
+		deleteTestUsers();
+		createAndSelectUser("Test User 1","name-1.lastname@akvo.org");
+		
+		solo.clickOnText("FLOW test survey 1.*");
 		assertEquals("Survey not selected", true, solo.searchText("Group 1.*")); 	
 		
 		// free text field
@@ -161,24 +429,17 @@ public class SurveyTest extends ActivityInstrumentationTestCase2<SurveyHomeActiv
 		
 		// number field
 		solo.clearEditText(1);
-		solo.enterText(1,"42.42");
-
-		// geo field
-		solo.clickOnText("Check Geo Location");
-		
+		solo.enterText(1,"42");
+	
 		// date field
-		solo.clickOnText("Select Date");
-		solo.clickOnText("Set");
+		solo.clickOnButton("Select Date");
+		solo.setDatePicker(0, 2012, 2, 16);
+		solo.clickOnButton("Set");
+		assertEquals("date not set", true, solo.searchText("Mar 16, 2012")); 
 		
-		// barcode field
-		solo.clearEditText(2);
-		solo.enterText(2,"Barcode code");
-
-		solo.clickOnText("Next");
-		
-		solo.sendKey(solo.MENU);
-		solo.clickOnText("Save and Start New") ;
-		
+		solo.goBack();
+		//solo.sendKey(solo.MENU);
+		//solo.clickOnText("Save and Start New") ;
 		
 	}
 
