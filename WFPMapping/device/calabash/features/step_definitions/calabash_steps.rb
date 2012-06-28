f744d7be8b230433ac7e77a7c759a8e98259d173
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-06-28 09:08:22 +0100

    [#30] Added AGPL open source license headers for test case files.

--- WFPMapping/device/calabash/features/step_definitions/calabash_steps.rb
+++ WFPMapping/device/calabash/features/step_definitions/calabash_steps.rb
@@ -1,3 +1,17 @@
+# Copyright (C) 2010-2012 Stichting Akvo (Akvo Foundation)
+#
+# This file is part of Akvo FLOW.
+#
+# Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+# the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+# either version 3 of the License or any later version.
+#
+# Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License included below for more details.
+#
+# The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>
+
 require 'calabash-android/calabash_steps'
 
 When /^user "([^\"]*)" with email "([^\"]*)" exists$/ do |user_name,email|
@@ -18,4 +32,3 @@ When /^user "([^\"]*)" with email "([^\"]*)" is selected$/ do |user_name,email|
     	step %{I press "Save"}
     	step %{I press "#{user_name}"}
 end
-
