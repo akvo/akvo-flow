da1cac9253e63c0423a0ae20eaa7ddb9c41abae3
Mark Tiele Westra <m.t.westra@akvo.org>
2012-08-22 12:49:16 +0200

    moving frontend stuff to separate feature branch

--- GAE/war/frontend-assets/js/not_needed/general.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('#topnav li', 'menuTopbar a').click(function(e){
-		e.preventDefault();
-		$('#pageWrap').load( e.target.href + ' #main');
-		$('#topnav li').removeClass("current");
-		$("this").addClass("current");
-	}); 
-});
