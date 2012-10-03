e53b5be3bc702d33f6414a171c2cedd445879e89
Oliver Galloway-Lunn <olivergl@akvo.org>
2012-10-03 11:03:00 +0000

    Added a helper script to rebuild the dashboard UI and restart Apache.

---
+++ Dashboard/update_ui.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+THIS_SCRIPT=$0
+
+DASHBOARD_HOME="$(cd `dirname "$THIS_SCRIPT"` && pwd)"
+echo "Updating dashboard at: $DASHBOARD_HOME"
+
+cd $DASHBOARD_HOME
+git status
+git pull origin feature/frontend
+git remote update
+bundle
+rake build
+sudo apache2ctl restart
+
