b7e82bc033cecc239a37d39d0a538e26e4fdf7e3
Christopher Fagiani <christopher.fagiani@gmail.com>
2012-10-13 15:01:51 -0400

    added rest api for listing latest version

---
+++ GAE/src/org/waterforpeople/mapping/app/web/DeviceApplicationRestService.java
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (C) 2010-2012 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo FLOW.
+ *
+ *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+package org.waterforpeople.mapping.app.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONObject;
+import org.waterforpeople.mapping.app.web.dto.DeviceApplicationRestRequest;
+import org.waterforpeople.mapping.dao.DeviceApplicationDao;
+import org.waterforpeople.mapping.domain.DeviceApplication;
+
+import com.gallatinsystems.framework.rest.AbstractRestApiServlet;
+import com.gallatinsystems.framework.rest.RestRequest;
+import com.gallatinsystems.framework.rest.RestResponse;
+
+/**
+ * restful service for DeviceApplications
+ * 
+ * @author Christopher Fagiani
+ * 
+ */
+public class DeviceApplicationRestService extends AbstractRestApiServlet {
+
+	private static final long serialVersionUID = -830140106880504436L;
+	private DeviceApplicationDao devAppDao;
+
+	public DeviceApplicationRestService() {
+		setMode(JSON_MODE);
+		devAppDao = new DeviceApplicationDao();
+	}
+
+	@Override
+	protected RestRequest convertRequest() throws Exception {
+		HttpServletRequest req = getRequest();
+		RestRequest restRequest = new DeviceApplicationRestRequest();
+		restRequest.populateFromHttpRequest(req);
+		return restRequest;
+	}
+
+	@Override
+	protected RestResponse handleRequest(RestRequest req) throws Exception {
+		RestResponse resp = new RestResponse();
+		if (DeviceApplicationRestRequest.GET_LATEST_VERSION_ACTION
+				.equalsIgnoreCase(req.getAction())) {
+
+			DeviceApplicationRestRequest daReq = (DeviceApplicationRestRequest) req;
+			List<DeviceApplication> devAppList = devAppDao
+					.listByDeviceTypeAndAppCode(daReq.getDeviceType(),
+							daReq.getAppCode(), 1);
+			if (devAppList != null && devAppList.size() > 0) {
+				resp.setMessage(devAppList.get(0).getVersion());
+			}
+		}
+		return resp;
+	}
+
+	@Override
+	protected void writeOkResponse(RestResponse resp) throws Exception {
+		getResponse().setStatus(200);
+		JSONObject obj = new JSONObject(resp, true);
+		getResponse().getWriter().println(obj.toString());
+	}
+
+}
--- GAE/src/org/waterforpeople/mapping/app/web/dto/DeleteTaskRequest.java
+++ GAE/src/org/waterforpeople/mapping/app/web/dto/DeleteTaskRequest.java
@@ -18,21 +18,20 @@ package org.waterforpeople.mapping.app.web.dto;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.gallatinsystems.framework.rest.RestError;
 import com.gallatinsystems.framework.rest.RestRequest;
 
 public class DeleteTaskRequest extends RestRequest {
 
-	
 	private static final long serialVersionUID = 8342489912346343508L;
-	public static final String OBJECT_PARAM ="object";
-	public static final String KEY_PARAM = "key";	
+	public static final String OBJECT_PARAM = "object";
+	public static final String KEY_PARAM = "key";
 	public static final String TASK_COUNT_PARAM = "taskCount";
 	private String objectName = null;
 	private String key = null;
 	private String cursor = null;
 	private String taskCount = null;
-	
-	
+
 	public String getTaskCount() {
 		return taskCount;
 	}
@@ -65,30 +64,29 @@ public class DeleteTaskRequest extends RestRequest {
 		this.cursor = cursor;
 	}
 
-	
-
 	@Override
 	protected void populateFields(HttpServletRequest req) throws Exception {
-		if(req.getParameter(OBJECT_PARAM)!=null){
+		if (req.getParameter(OBJECT_PARAM) != null) {
 			setObjectName(req.getParameter(OBJECT_PARAM));
-			if(req.getParameter(KEY_PARAM)!=null){
+			if (req.getParameter(KEY_PARAM) != null) {
 				setKey(req.getParameter(KEY_PARAM));
-			}else{
+			} else {
 				throw new Exception("Parameter " + KEY_PARAM + " is mandatory");
 			}
-			if(req.getParameter(CURSOR_PARAM)!=null){
+			if (req.getParameter(CURSOR_PARAM) != null) {
 				setCursor(req.getParameter(CURSOR_PARAM));
 			}
-			if(req.getParameter(TASK_COUNT_PARAM)!=null){
+			if (req.getParameter(TASK_COUNT_PARAM) != null) {
 				setTaskCount(req.getParameter(TASK_COUNT_PARAM));
 			}
-		}else{
+		} else {
 			throw new Exception("Parameter " + OBJECT_PARAM + " is mandatory");
 		}
 	}
 
 	@Override
 	protected void populateErrors() {
+
 	}
 
 }
