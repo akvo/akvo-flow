7a5ac06e5dc376e9592b12b8d5887de91eab195c
Mark Tiele Westra <m.t.westra@akvo.org>
2012-11-21 13:15:55 +0100

    implement Question group rest service

--- GAE/src/org/waterforpeople/mapping/app/web/controller/QuestionGroupRestService.java
+++ GAE/src/org/waterforpeople/mapping/app/web/controller/QuestionGroupRestService.java
@@ -14,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.waterforpeople.mapping.app.gwt.client.survey.QuestionGroupDto;
+import org.waterforpeople.mapping.app.gwt.client.survey.SurveyDto;
 import org.waterforpeople.mapping.app.util.DtoMarshaller;
 
 import com.gallatinsystems.common.Constants;
+import com.gallatinsystems.framework.exceptions.IllegalDeletionException;
 import com.gallatinsystems.survey.dao.QuestionGroupDao;
 import com.gallatinsystems.survey.domain.QuestionGroup;
+import com.gallatinsystems.survey.domain.Survey;
 
 @Controller
 @RequestMapping("/question-group")
@@ -69,8 +72,25 @@ public class QuestionGroupRestService {
 			dto = new QuestionGroupDto();
 			DtoMarshaller.copyToDto(sg, dto);
 		}
+		return dto;	
+	}
+	
+	// delete question group by id
+	@RequestMapping(method = RequestMethod.DELETE, value = "/del/{id}")
+	@ResponseBody
+	public RestStatusDto deleteQuestionGroupById(@PathVariable("id") Long id){
+		QuestionGroup qg = questionGroupDao.getByKey(id);		
+		RestStatusDto dto = null;
+		dto = new RestStatusDto();
+		dto.setStatus("failed");
+				  
+		// check if questionGroup exists in the datastore
+		if (qg != null){
+			// delete question group
+			questionGroupDao.delete(qg);
+			dto.setStatus("ok");	
+		}
 		return dto;
-		
 	}
 	
 	// save a question group
@@ -79,7 +99,29 @@ public class QuestionGroupRestService {
 	public QuestionGroupDto saveQuestionGroup(@RequestBody QuestionGroupDto questionGroupDto){
 		QuestionGroupDto dto = null;
 		
-		// TODO
+		// if the POST data contains a valid QuestionGroupDto, continue. Otherwise, server will respond with 400 Bad Request 
+		if (questionGroupDto != null){
+			Long keyId = questionGroupDto.getKeyId();
+			QuestionGroup qg;
+					
+			// if the questionGroupDto has a key, try to get the surveyGroup.
+			if (keyId != null) {
+				qg = questionGroupDao.getByKey(keyId);
+				// if the questionGroup doesn't exist, create a new questionGroup
+				if (qg == null) {
+					qg = new QuestionGroup();
+				}
+			} else {
+				qg = new QuestionGroup();
+			}
+			
+			// copy the properties, except the createdDateTime property, because it is set in the Dao.
+			BeanUtils.copyProperties(questionGroupDto, qg, new String[] {"createdDateTime"});
+			qg = questionGroupDao.save(qg);
+					
+			dto = new QuestionGroupDto();
+			DtoMarshaller.copyToDto(qg, dto);
+		}
 		return dto;
 	}
 
