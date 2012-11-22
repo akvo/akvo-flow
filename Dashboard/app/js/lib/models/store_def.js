9079c2bfe93733c6dbd916ce8c24174c2d2f29ee
Daniel Karlsson <daniel@kardans.com>
2012-11-22 06:02:06 +0100

    Added the FLOWRESTAdapter to gitignore

--- Dashboard/app/js/lib/models/store_def.js
+++ Dashboard/app/js/lib/models/store_def.js
@@ -1,5 +1,5 @@
 FLOW.store = DS.Store.create({
 	revision: 8,
-	adapter:DS.FLOWRESTAdapter.create({bulkCommit:false, namespace:"rest", url:"http://33.33.33.6"})
+	adapter:DS.FLOWRESTAdapter.create({bulkCommit:false, namespace:"rest", url:"http://akvo-flow.dev"})
 	// adapter: DS.fixtureAdapter
 });
