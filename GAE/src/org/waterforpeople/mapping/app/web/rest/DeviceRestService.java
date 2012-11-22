eb9c68cd7bbac12f5422ed13acdae32576eb91fb
Iván Perdomo <katratxo@gmail.com>
2012-11-22 19:42:31 +0100

    Fixes Devices service with new approach

--- GAE/src/org/waterforpeople/mapping/app/web/rest/DeviceRestService.java
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/DeviceRestService.java
@@ -1,7 +1,9 @@
 package org.waterforpeople.mapping.app.web.rest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -17,19 +19,19 @@ import com.gallatinsystems.common.Constants;
 import com.gallatinsystems.device.dao.DeviceDAO;
 import com.gallatinsystems.device.domain.Device;
 
-
 @Controller
-@RequestMapping("/device")
+@RequestMapping("/devices")
 public class DeviceRestService {
 
 	@Inject
 	private DeviceDAO deviceDao;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/")
+	@RequestMapping(method = RequestMethod.GET, value = "")
 	@ResponseBody
-	public List<DeviceDto> listDevices() {
-		List<DeviceDto> deviceList = new ArrayList<DeviceDto>();
-		List<Device> devices = deviceDao.list(Constants.ALL_RESULTS);
+	public Map<String, List<DeviceDto>> listDevices() {
+		final Map<String, List<DeviceDto>> response = new HashMap<String, List<DeviceDto>>();
+		final List<DeviceDto> deviceList = new ArrayList<DeviceDto>();
+		final List<Device> devices = deviceDao.list(Constants.ALL_RESULTS);
 
 		if (devices != null) {
 			for (Device d : devices) {
@@ -38,18 +40,21 @@ public class DeviceRestService {
 				deviceList.add(deviceDto);
 			}
 		}
-		return deviceList;
+		response.put("devices", deviceList);
+		return response;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	@ResponseBody
-	public DeviceDto findDevice(@PathVariable("id") Long id) {
-		Device d = deviceDao.getByKey(id);
+	public Map<String, DeviceDto> findDevice(@PathVariable("id") Long id) {
+		final Map<String, DeviceDto> response = new HashMap<String, DeviceDto>();
+		final Device d = deviceDao.getByKey(id);
 		DeviceDto dto = null;
 		if (d != null) {
 			dto = new DeviceDto();
 			DtoMarshaller.copyToDto(d, dto);
 		}
-		return dto;
+		response.put("device", dto);
+		return response;
 	}
 }
