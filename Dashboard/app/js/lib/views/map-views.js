44d2ba6b4762cb3e9c482170f7d3800c1193701c
Daniel Karlsson <daniel@kardans.com>
2012-11-28 16:07:16 +0100

    Removed bubbles from map - Map now show selected bubble as red - Removed the bubble to open - There is a timeing bug that needs fixing,
    . #94

--- Dashboard/app/js/lib/views/map-views.js
+++ Dashboard/app/js/lib/views/map-views.js
@@ -1,19 +1,19 @@
 FLOW.NavMapsView = Ember.View.extend({
   templateName: "navMaps/nav-maps",
-  showDetailsBool:false,
+  showDetailsBool: false,
 
   PlacemarkDetailsView:Ember.View.extend({
 
   }),
 
-  init:function(){
+  init: function () {
     this._super();
     FLOW.placemarkControl.populate();
   },
 
 
-  didInsertElement: function() {
-    var map = new mxn.Mapstraction("flowMap", "google"),
+  didInsertElement: function () {
+    var map = new mxn.Mapstraction("flowMap", "google", true),
         latlon = new mxn.LatLonPoint(-0.703107, 36.765747);
 
     map.addControls({
@@ -24,42 +24,70 @@ FLOW.NavMapsView = Ember.View.extend({
 
     map.setCenterAndZoom(latlon, 8);
     map.enableScrollWheelZoom();
-    FLOW.placemarkControl.set('map',map);
+    FLOW.placemarkControl.set('map', map);
   },
 
-   putPlacemarksOnMap: function(){
+  putPlacemarksOnMap: function () {
     console.log('ready to put placemarks on map');
-    var mapHandler=FLOW.placemarkControl.get('map');
+    var mapHandler = FLOW.placemarkControl.get('map');
     var htmlContent, marker, pm, point, mark;
-    FLOW.placemarkControl.get('content').forEach(function(item){
+
+    FLOW.placemarkControl.get('content').forEach(function (item) {
+        var mapHandler = FLOW.placemarkControl.get('map');
         point = new mxn.LatLonPoint(item.get('latitude'), item.get('longitude'));
         marker = new mxn.Marker(point);
         item.set('marker', marker);
         marker.setLabel('label');
-        marker.setInfoBubble("content bubble");
+        // marker.setInfoBubble('content bubble');
+        marker.setIcon('images/maps/blueMarker.png');
 
-        marker.click.addHandler(function(event_name, event_source, event_args) {
+        marker.click.addHandler(function (event_name, event_source, event_args) {
+          // Deselect marker
+          
+          var toggleMarker = function (placeMark, map) {
+            // var coordinate = new mxn.LatLonPoint(placeMark.get('latitude'), placeMark.get('longitude'));
+            // var newMark = new mxn.Marker(coordinate);
+            var newMark = new mxn.Marker(placeMark.marker.location);
+            newMark.setLabel('label');
+            // newMark.setInfoBubble('bubble');
+            if (placeMark.marker.iconUrl === 'images/maps/blueMarker.png') {
+              newMark.setIcon('images/maps/redMarker.png');
+            } else {
+              newMark.setIcon('images/maps/blueMarker.png');
+            }
+            map.removeMarker(placeMark.marker);
+            map.addMarker(newMark);
+            placeMark.set('marker', newMark);
+          };
+
+          var oldSelected = FLOW.placemarkControl.get('selected');
+          
+          if (typeof oldSelected != 'undefined') {
+            toggleMarker(oldSelected, mapHandler);
+          }
+
+          toggleMarker(item, mapHandler);
           FLOW.placemarkControl.set('selected', item);
         });
         
-        mapHandler.addMarker(marker, true);
+        mapHandler.addMarker(marker, false);
     });
   },
 
-  triggerPlacemarks:function(){
-    if (FLOW.placemarkControl.content.get('isUpdating')===false) {this.putPlacemarksOnMap();}
+  triggerPlacemarks: function () {
+    if (FLOW.placemarkControl.content.get('isUpdating') === false) {this.putPlacemarksOnMap();}
   }.observes('FLOW.placemarkControl.content.isUpdating'),
 
-  getDetails:function(){
-    var pmId=FLOW.placemarkControl.selected.get('keyId');
+  getDetails: function () {
+    var pmId = FLOW.placemarkControl.selected.get('keyId');
     FLOW.placemarkDetailControl.populate(pmId);
     console.log('getting details');
   }.observes('FLOW.placemarkControl.selected'),
 
-  showDetails:function(){
-    if (FLOW.placemarkDetailControl.content.get('isLoaded')===true) {
+  showDetails: function () {
+    if (FLOW.placemarkDetailControl.content.get('isLoaded') === true) {
       console.log('details loaded');
-      this.set('showDetailsBool',true);
+      this.set('showDetailsBool', true);
     }
   }.observes('FLOW.placemarkDetailControl.content.isLoaded')
 
