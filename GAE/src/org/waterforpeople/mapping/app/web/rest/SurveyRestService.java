bde9e45d804fac35208f77093b4848323aca6338
Iván Perdomo <katratxo@gmail.com>
2012-11-28 11:54:09 +0100

    Cleanup imports - Removed unused imports - Sort imports using Eclipse default settings

--- GAE/src/org/waterforpeople/mapping/app/web/rest/SurveyRestService.java
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/SurveyRestService.java
@@ -12,11 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.waterforpeople.mapping.app.gwt.client.survey.SurveyDto;
-import org.waterforpeople.mapping.app.gwt.client.survey.SurveyGroupDto;
 import org.waterforpeople.mapping.app.util.DtoMarshaller;
 import org.waterforpeople.mapping.app.web.rest.dto.SurveyPayload;
 
