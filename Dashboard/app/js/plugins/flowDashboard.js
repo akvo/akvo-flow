f2c39630ea1827db8e1b7830e04d46e21c5049f1
Loic <loic.sans@gmail.com>
2012-11-29 13:08:13 +0200

    Fixed tooltip javascript function

--- Dashboard/app/js/plugins/flowDashboard.js
+++ Dashboard/app/js/plugins/flowDashboard.js
@@ -8,18 +8,16 @@ this.tooltip = function(){
 		// these 2 variable determine popup's distance from the cursor
 		// you might want to adjust to get the right result
 	/* END CONFIG */
+	var titleAttr = $("a.tooltip").attr("title");
 	$("a.tooltip").hover(function(e){
 		this.t = this.title;
-		//this.title = "";
 		$("body").append("<p id='tooltip'>"+ this.t +"</p>");
-		$("#tooltip")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px")
-			.fadeIn("fast");
+		$("#tooltip").css("top",(e.pageY - xOffset) + "px").css("left",(e.pageX + yOffset) + "px").fadeIn("fast");
+		this.title = "";
     },
 
 	function(){
-		this.title = this.t;
+		$("a.tooltip").attr( "title" , titleAttr );
 		$("#tooltip").remove();
     });
 	$("a.tooltip").mousemove(function(e){
