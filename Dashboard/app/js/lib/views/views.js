0f6cb00472172e7c9aec81a3e800d818c8f92073
Mark Tiele Westra <m.t.westra@akvo.org>
2012-11-29 14:00:05 +0100

    comment on where to put extra javascript

--- Dashboard/app/js/lib/views/views.js
+++ Dashboard/app/js/lib/views/views.js
@@ -90,6 +90,10 @@ FLOW.NavigationView = Em.View.extend({
 //                      standard views
 // ********************************************************//
 
+// TODO check if doing this in View is not impacting performance, 
+// as some pages have a lot of views (all navigation elements, for example)
+// one way could be use an extended copy of view, with the didInsertElement,
+// for some of the elements, and not for others.
 Ember.View.reopen({
     didInsertElement: function() {
         this._super();
