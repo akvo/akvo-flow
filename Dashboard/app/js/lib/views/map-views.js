559fdf60a4e1088d2810273a7535c3a845cb7be0
Daniel Karlsson <daniel@kardans.com>
2012-12-09 11:24:33 +0100

    Changes to the map-view to make static code analysis tools happy
    - Added JSHint options
    - Added a "global" function that enables JS strict mode.
    - Changed all strings to use '...' notation. Just so we use the
      same in the file.
    
    . #94

--- Dashboard/app/js/lib/views/map-views.js
+++ Dashboard/app/js/lib/views/map-views.js
@@ -1,4 +1,5 @@
-/*global mxn:false*/
+/*global Ember:false, mxn:false */
+/*jshint strict:true, indent:2, browser:true */
 
 /**
   View that handles map page.
@@ -7,151 +8,156 @@
     "marker" is a map object that is rendered as a pin. Each marker have
       a placemark counterpart.
 **/
-FLOW.NavMapsView = Ember.View.extend({
-  templateName: "navMaps/nav-maps",
-  showDetailsBool: false,
-  imageURL: 'flow15/images/invisible.png',
 
-  init: function () {
-    this._super();
-    FLOW.placemarkControl.populate();
-  },
+(function (FLOW) {
+  'use strict';
+  FLOW.NavMapsView = Ember.View.extend({
+    templateName: 'navMaps/nav-maps',
+    showDetailsBool: false,
+    imageURL: 'flow15/images/invisible.png',
 
-  /**
-    ...
-  **/
-  didInsertElement: function () {
-    var map = new mxn.Mapstraction("flowMap", "google", true),
-        latLon = new mxn.LatLonPoint(-0.703107, 36.765747);
-
-    map.addControls({
-        pan: true,
-        zoom: 'small',
-        map_type: true
-    });
-
-    map.setCenterAndZoom(latLon, 8);
-    map.enableScrollWheelZoom();
-    FLOW.placemarkControl.set('map', map);
-  },
-
-  /**
-    Populate the map with markers
-  **/
-  populateMap: function () {
-    var map;
-    if (FLOW.placemarkControl.content.get('isUpdating') === false) {
-      map = FLOW.placemarkControl.get('map');
-      FLOW.placemarkControl.get('content').forEach(function (placemark) {
-        map.addMarker(this.createMarker(placemark));
-      }, this);
-    }
-  }.observes('FLOW.placemarkControl.content.isUpdating'),
-
-  /**
-    ...
-  **/
-  handlePlacemarkSelection: function () {
-    var selected = FLOW.placemarkControl.get('selected');
-    if (typeof selected !== 'undefined') {
-      FLOW.placemarkDetailControl.populate(selected.id);
-    } else {
-      FLOW.placemarkDetailControl.populate(selected);
-    }
-    this.set('imageURL', 'flow15/images/invisible.png');
-  }.observes('FLOW.placemarkControl.selected'),
-
-  /**
-    ...
-  **/
-  handlePlacemarkDetails: function () {
-    var details, imageURL, selectedPlacemarkDetail, stringVal;
-
-    details = FLOW.placemarkDetailControl.get('content');
-
-    if ((typeof details !== 'undefined') && (details !== null) && (details.get('isLoaded') === true)) {
-      this.set('showDetailsBool', true);
-      details.forEach(function (item) {
-        stringVal = item.get('stringValue');
-        if (stringVal.indexOf('wfpPhoto') != -1) {
-          imageURL = 'http://flowdemo.s3.amazonaws.com/images/' + stringVal.slice(stringVal.indexOf('wfpPhoto'));
-          this.set('imageURL', imageURL);
-        }
-      }, this);
-    }
-  }.observes('FLOW.placemarkDetailControl.content.isLoaded'),
-
-  /**
-     Returns a marker(pin on the map) to represent the placemarker
-  **/
-  createMarker: function (placemark) {
-    // Create a marker
-    var point = new mxn.LatLonPoint(placemark.get('latitude'), placemark.get('longitude')),
-      marker = new mxn.Marker(point);
-
-    marker.setIcon('flow15/images/maps/blueMarker.png');
-    marker.placemark = placemark;
-    // Add a click handler that handles what happens when marker is clicked
-    
-    /**
-      Adds a custom clickHandler to the markers
-    **/
-    placemark.addMarkerClickHandler = function (marker, placemark) {
-      var clickHandler = function (event_name, event_source, event_args) {
-        event_source.placemark.handleClick(event_source.placemark.marker);
-      };
-      marker.click.addHandler(clickHandler);
-    };
-
-    /**
-      When a marker is clicked we do different thing depending on
-      the state of the map. E.g. if the same marker is clicked we deselect
-      that marker and no marker is selected.
-    **/
-    placemark.handleClick = function (marker) {
-      var oldSelected;
-      marker.placemark.toggleMarker(marker.placemark);
-
-      oldSelected = FLOW.placemarkControl.get('selected');
-      if (typeof oldSelected === 'undefined') {
-        FLOW.placemarkControl.set('selected', placemark);
-      } else {
-        if (this.marker === oldSelected.marker) {
-          FLOW.placemarkControl.set('selected', undefined);
-        } else {
-          oldSelected.toggleMarker(oldSelected);
-          FLOW.placemarkControl.set('selected', placemark);
-        }
-      }
-    };
-
-    /**
-      Toggle between selected and deselected marker.
-      In reality there is no toggle but delete and create
-    **/
-    placemark.toggleMarker = function (placemark) {
-      var map = FLOW.placemarkControl.get('map');
-      var point = new mxn.LatLonPoint(placemark.get('latitude'), placemark.get('longitude')),
-        newMarker = new mxn.Marker(point);
-
-      if (placemark.marker.iconUrl === 'flow15/images/maps/blueMarker.png') {
-        newMarker.iconUrl = 'flow15/images/maps/redMarker.png';
-      } else {
-        newMarker.iconUrl = 'flow15/images/maps/blueMarker.png';
-      }
-    
-      placemark.addMarkerClickHandler(newMarker, placemark);
-      map.addMarker(newMarker);
-      map.removeMarker(placemark.marker);
-      newMarker.placemark = placemark;
-      
-      placemark.set('marker', newMarker);
+    init: function () {
+      this._super();
+      FLOW.placemarkControl.populate();
     },
 
-    placemark.addMarkerClickHandler(marker, placemark);
-    // Attach the new marker to the placemarker object
-    placemark.set('marker', marker);
-    return marker;
-  }
-  
-});
+    /**
+      ...
+    **/
+    didInsertElement: function () {
+      var map = new mxn.Mapstraction('flowMap', 'google', true),
+          latLon = new mxn.LatLonPoint(-0.703107, 36.765747);
+
+      map.addControls({
+          pan: true,
+          zoom: 'small',
+          map_type: true
+        });
+
+      map.setCenterAndZoom(latLon, 8);
+      map.enableScrollWheelZoom();
+      FLOW.placemarkControl.set('map', map);
+    },
+
+    /**
+      Populate the map with markers
+    **/
+    populateMap: function () {
+      var map;
+      if (FLOW.placemarkControl.content.get('isUpdating') === false) {
+        map = FLOW.placemarkControl.get('map');
+        FLOW.placemarkControl.get('content').forEach(function (placemark) {
+          map.addMarker(this.createMarker(placemark));
+        }, this);
+      }
+    }.observes('FLOW.placemarkControl.content.isUpdating'),
+
+    /**
+      ...
+    **/
+    handlePlacemarkSelection: function () {
+      var selected = FLOW.placemarkControl.get('selected');
+      if (typeof selected !== 'undefined') {
+        FLOW.placemarkDetailControl.populate(selected.id);
+      } else {
+        FLOW.placemarkDetailControl.populate(selected);
+      }
+      this.set('imageURL', 'flow15/images/invisible.png');
+    }.observes('FLOW.placemarkControl.selected'),
+
+    /**
+      ...
+    **/
+    handlePlacemarkDetails: function () {
+      var details, imageURL, stringVal;
+
+      details = FLOW.placemarkDetailControl.get('content');
+
+      if ((typeof details !== 'undefined') && (details !== null) && (details.get('isLoaded') === true)) {
+        this.set('showDetailsBool', true);
+        details.forEach(function (item) {
+          stringVal = item.get('stringValue');
+          if (stringVal.indexOf('wfpPhoto') != -1) {
+            imageURL = 'http://flowdemo.s3.amazonaws.com/images/' + stringVal.slice(stringVal.indexOf('wfpPhoto'));
+            this.set('imageURL', imageURL);
+          }
+        }, this);
+      }
+    }.observes('FLOW.placemarkDetailControl.content.isLoaded'),
+
+    /**
+       Returns a marker(pin on the map) to represent the placemarker
+    **/
+    createMarker: function (placemark) {
+      // Create a marker
+      var point = new mxn.LatLonPoint(placemark.get('latitude'), placemark.get('longitude')),
+        marker = new mxn.Marker(point);
+
+      marker.setIcon('flow15/images/maps/blueMarker.png');
+      marker.placemark = placemark;
+      // Add a click handler that handles what happens when marker is clicked
+      
+      /**
+        Adds a custom clickHandler to the markers
+      **/
+      placemark.addMarkerClickHandler = function (marker) {
+        var clickHandler = function (event_name, event_source, event_args) {
+          event_source.placemark.handleClick(event_source.placemark.marker);
+        };
+        marker.click.addHandler(clickHandler);
+      };
+
+      /**
+        When a marker is clicked we do different thing depending on
+        the state of the map. E.g. if the same marker is clicked we deselect
+        that marker and no marker is selected.
+      **/
+      placemark.handleClick = function (marker) {
+        var oldSelected;
+        marker.placemark.toggleMarker(marker.placemark);
+
+        oldSelected = FLOW.placemarkControl.get('selected');
+        if (typeof oldSelected === 'undefined') {
+          FLOW.placemarkControl.set('selected', placemark);
+        } else {
+          if (this.marker === oldSelected.marker) {
+            FLOW.placemarkControl.set('selected', undefined);
+          } else {
+            oldSelected.toggleMarker(oldSelected);
+            FLOW.placemarkControl.set('selected', placemark);
+          }
+        }
+      };
+
+      /**
+        Toggle between selected and deselected marker.
+        In reality there is no toggle but delete and create
+      **/
+      placemark.toggleMarker = function (placemark) {
+        var map = FLOW.placemarkControl.get('map');
+        var point = new mxn.LatLonPoint(placemark.get('latitude'), placemark.get('longitude')),
+          newMarker = new mxn.Marker(point);
+
+        if (placemark.marker.iconUrl === 'flow15/images/maps/blueMarker.png') {
+          newMarker.iconUrl = 'flow15/images/maps/redMarker.png';
+        } else {
+          newMarker.iconUrl = 'flow15/images/maps/blueMarker.png';
+        }
+      
+        placemark.addMarkerClickHandler(newMarker);
+        map.addMarker(newMarker);
+        map.removeMarker(placemark.marker);
+        newMarker.placemark = placemark;
+        
+        placemark.set('marker', newMarker);
+      },
+
+      placemark.addMarkerClickHandler(marker, placemark);
+      // Attach the new marker to the placemarker object
+      placemark.set('marker', marker);
+      return marker;
+    }
+    
+  });
+
+})(window.FLOW);
