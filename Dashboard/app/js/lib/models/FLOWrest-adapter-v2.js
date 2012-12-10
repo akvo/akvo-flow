28849444165ea50d4766a989ebf3975faeaafe17
Iván Perdomo <ivan@akvo.org>
2012-12-10 11:18:11 +0100

    Redefines sideload with custom logic - Whenever we can we should use this._super() to get benefits from any   possible bugfix and main code

--- Dashboard/app/js/lib/models/FLOWrest-adapter-v2.js
+++ Dashboard/app/js/lib/models/FLOWrest-adapter-v2.js
@@ -16,41 +16,11 @@ DS.FLOWRESTAdapter = DS.RESTAdapter.extend({
     return url + (record === 'placemark' ? '?country=KE' : '');
   },
 
-  sideload: function(store, type, json, root) {
-    var sideloadedType, mappings, loaded = {};
-
-    loaded[root] = true;
-
-    for (var prop in json) {
-      if (!json.hasOwnProperty(prop)) { continue; }
-      if (prop === root) { continue; }
-      
-      if (prop === 'meta') {
-        metaContent=json['meta'];
-        FLOW.metaControl.set('since',metaContent['since']);
-        FLOW.metaControl.set('num',metaContent['num']);
-        FLOW.metaControl.set('message',metaContent['message']);
-        FLOW.metaControl.set('status',metaContent['status']);
-        continue;
-      }
-
-      sideloadedType = type.typeForAssociation(prop);
-
-      if (!sideloadedType) {
-        mappings = get(this, 'mappings');
-        Ember.assert("Your server returned a hash with the key " + prop + " but you have no mappings", !!mappings);
-
-        sideloadedType = get(mappings, prop);
-
-        if (typeof sideloadedType === 'string') {
-          sideloadedType = get(window, sideloadedType);
-        }
-
-        Ember.assert("Your server returned a hash with the key " + prop + " but you have no mapping for it", !!sideloadedType);
-      }
-
-      this.sideloadAssociations(store, sideloadedType, json, prop, loaded);
-    }
-  },
-
+  sideload: function (store, type, json, root) {
+    this._super(store, type, json, root);
+    FLOW.metaControl.set('since', this.extractMeta(json).since);
+    FLOW.metaControl.set('num', this.extractMeta(json).num);
+    FLOW.metaControl.set('message', this.extractMeta(json).message);
+    FLOW.metaControl.set('status', this.extractMeta(json).status);
+  }
 });
