cea7392cb8775d76ce330490ff3d8279802b63d1
Mark Tiele Westra <m.t.westra@akvo.org>
2012-12-14 16:05:39 +0100

    add ENUM Sector type in survey class

--- GAE/src/com/gallatinsystems/survey/domain/Survey.java
+++ GAE/src/com/gallatinsystems/survey/domain/Survey.java
@@ -41,6 +41,7 @@ public class Survey extends BaseDomain {
 	private String name = null;
 	private String desc = null;
 	private Status status = null;
+	private Sector sector = null;
 	@NotPersistent
 	private TreeMap<Integer, QuestionGroup> questionGroupMap = null;
 	private Double version = null;
@@ -52,6 +53,10 @@ public class Survey extends BaseDomain {
 
 	public enum Status {
 		PUBLISHED, NOT_PUBLISHED, IMPORTED, VERIFIED
+	};
+	
+	public enum Sector {
+		WASH, EDUC, ECONDEV, HEALTH, ICT, FOODSEC, OTHER
 	};
 
 	public Survey() {
@@ -105,6 +110,14 @@ public class Survey extends BaseDomain {
 
 	public void setStatus(Status status) {
 		this.status = status;
+	}
+	
+	public Sector getSector() {
+		return sector;
+	}
+
+	public void setSector(Sector sector) {
+		this.sector = sector;
 	}
 
 	public Double getVersion() {
