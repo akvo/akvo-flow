akvo/akvo-flow

commit aad96a0b5491ce135face548ba7773ace67e9f04

Issue #105 - Adds the raw data and bulk import applets
- This needs styling and better navigation
- Changes the way of using the credentials, instead of passing them as criteria
  we'll ship it in the as part of the export exporterapplet.jar

GAE/src/com/gallatinsystems/framework/dataexport/applet/AbstractDataImportExportApplet.java
@@ -16,8 +16,11 @@
 
 package com.gallatinsystems.framework.dataexport.applet;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.StringTokenizer;
 
 import javax.swing.JApplet;
@@ -105,6 +108,32 @@ public class AbstractDataImportExportApplet extends JApplet {
 	 * @return
 	 */
 	protected Map<String, String> getConfigCriteria() {
-		return parseCriteria(getParameter(CRITERIA_PARAM));
+		final String criteria = getParameter(CRITERIA_PARAM);
+
+		if (criteria != null && "".equals(criteria)) {
+			return parseCriteria(criteria);
+		}
+
+		System.out.println("Loading configuration from UploadConstants.properties");
+
+		final Properties uploadProperties = new Properties();
+		final InputStream is = AbstractDataImportExportApplet.class
+				.getResourceAsStream("/UploadConstants.properties");
+		try {
+			uploadProperties.load(is);
+		} catch (IOException ioe) {
+			System.err.println("Error loading upload constants");
+			return null;
+		} finally {
+			try {
+				is.close();
+			} catch (Exception e) {
+				// no-op
+			}
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		Map<String, String> config = new HashMap(System.getProperties());
+		return config;
 	}
 }

Dashboard/app/js/lib/views/views.js
@@ -356,6 +356,14 @@ FLOW.surveyFormApplet = Ember.View.extend({
   templateName: 'navReports/applets/survey-form-applet'
 });
 
+FLOW.bulkImportApplet = Ember.View.extend({
+  templateName: 'navData/applets/bulk-import-applet'
+});
+
+FLOW.rawDataImportApplet = Ember.View.extend({
+  templateName: 'navData/applets/raw-data-import-applet'
+});
+
 // users views
 FLOW.NavUsersView = Ember.View.extend({
   templateName: 'navUsers/nav-users'

Dashboard/app/js/templates/navData/applets/bulk-import-applet.handlebars
@@ -1,0 +1,9 @@
+<applet height="30" width="100"
+	code="com.gallatinsystems.framework.dataexport.applet.DataImportAppletImpl"
+	archive="exporterapplet.jar,gdata-core-1.0.jar">
+	<param name="importType" value="BULK_SURVEY">
+	<param name="selectionMode" value="dir">
+	<param name="java_arguments" value="-Xmx1024m">
+	<param name="factoryClass"
+		value="org.waterforpeople.mapping.dataexport.SurveyDataImportExportFactory">
+</applet>

Dashboard/app/js/templates/navData/applets/raw-data-import-applet.handlebars
@@ -1,0 +1,12 @@
+<applet height="30" width="100"
+	code="com.gallatinsystems.framework.dataexport.applet.DataImportAppletImpl"
+	archive="exporterapplet.jar,json.jar,jcommon-1.0.16.jar,jfreechart-1.0.13.jar,poi-3.8-20120326.jar,poi-ooxml-3.8-20120326.jar,poi-ooxml-schemas-3.8-20120326.jar,xbean.jar,dom4j-1.6.1.jar,gdata-core-1.0.jar">
+	<param name="cache-archive"
+		value="exporterapplet.jar, json.jar, poi-3.5-signed.jar">
+	<param name="cache-version" value="1.3, 1.0, 3.5">
+	<param name="factoryClass"
+		value="org.waterforpeople.mapping.dataexport.SurveyDataImportExportFactory">
+	<param name="java_arguments" value="-Xmx512m">
+	<param name="importType" value="RAW_DATA">
+	<param name="criteria" value="k:=test;surveyId:={{unbound FLOW.selectedControl.selectedSurvey.keyId}}">
+</applet>

Dashboard/app/js/templates/navData/excel-import.handlebars
@@ -1,3 +1,4 @@
  <section class="fullWidth" id="excelImport">
         <h1>This is the content of Spreadsheet import</h1>
+        {{view FLOW.rawDataImportApplet }}
     </section>

Dashboard/app/js/templates/navData/import-survey.handlebars
@@ -1,4 +1,5 @@
  <section class="fullWidth" id="importSurvey">
         <h1>Content of Import Survey Data tab
         </h1>
+        {{view FLOW.bulkImportApplet}}
     </section>

GAE/appletBuild.xml
@@ -65,8 +65,8 @@
                 <include name="*.vm" />
             </fileset>
         </copy>
-        <jar destfile="${dist}/exporterapplet.jar" basedir="${build}">
-        </jar>
+        <copy file="${src}/org/waterforpeople/mapping/app/gwt/client/util/UploadConstants.properties" tofile="${build}/UploadConstants.properties" overwrite="true" verbose="true" />
+        <jar destfile="${dist}/exporterapplet.jar" basedir="${build}" />
     </target>
 
     <target name="signjar" depends="package">

commit 7b10b2cf14a1ae9ffa4a2b58c1fc59a134a931b9

Issue #105 - The file choose has a default file name
* When a user exports data the file choose proposes a file name
* Fixes a bug introduced in aad96a0

Dashboard/app/js/templates/navReports/applets/raw-data-report-applet.handlebars
@@ -10,5 +10,5 @@
   <param name="criteria" value="surveyId:={{unbound FLOW.selectedControl.selectedSurvey.keyId}}">
   <!-- FIXME locale must be configurable -->
   <param name="options"
-    value="exportMode:=RAW_DATA;locale:=en;imgPrefix:=http://flowdemo.s3.amazonaws.com/images;generateTabFormat=false/">
+    value="exportMode:=RAW_DATA;locale:=en;imgPrefix:=http://flowdemo.s3.amazonaws.com/images;generateTabFormat=false"/>
 </applet>

GAE/src/com/gallatinsystems/framework/dataexport/applet/AbstractDataImportExportApplet.java
@@ -110,7 +110,7 @@ public class AbstractDataImportExportApplet extends JApplet {
 	protected Map<String, String> getConfigCriteria() {
 		final String criteria = getParameter(CRITERIA_PARAM);
 
-		if (criteria != null && "".equals(criteria)) {
+		if (criteria != null && !"".equals(criteria)) {
 			return parseCriteria(criteria);
 		}
 

GAE/src/com/gallatinsystems/framework/dataexport/applet/DataExportAppletImpl.java
@@ -16,6 +16,7 @@
 
 package com.gallatinsystems.framework.dataexport.applet;
 
+import java.io.File;
 import java.util.Map;
 
 import javax.swing.JFileChooser;
@@ -63,9 +64,15 @@ public class DataExportAppletImpl extends AbstractDataImportExportApplet {
 	 */
 	public void doExport(String type, Map<String, String> criteriaMap,
 			String serverBase, Map<String, String> options) {
-		JFileChooser chooser = new JFileChooser();
+		final JFileChooser chooser = new JFileChooser();
+		final String surveyId = criteriaMap.containsKey("surveyId") ? criteriaMap
+				.get("surveyId") : null;
+		final String fileName = type + (surveyId != null ? "-" + surveyId : "")
+				+ ".xlsx";
 
+		chooser.setSelectedFile(new File(fileName));
 		chooser.showSaveDialog(this);
+
 		if (chooser.getSelectedFile() != null) {
 			DataExporter exporter = dataExporterFactory.getExporter(type);
 			statusLabel.setText("Exporting...");