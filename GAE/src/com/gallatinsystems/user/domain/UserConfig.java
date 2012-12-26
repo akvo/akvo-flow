4420eb15a65a3c444c0a9372d23ff3ced636fdea
Mark Tiele Westra <m.t.westra@akvo.org>
2012-12-26 14:51:14 +0100

    add userId field to userConfig

--- GAE/src/com/gallatinsystems/user/domain/UserConfig.java
+++ GAE/src/com/gallatinsystems/user/domain/UserConfig.java
@@ -37,7 +37,17 @@ public class UserConfig extends BaseDomain {
 	private String name;
 	@Persistent
 	private String value;
+	@Persistent
+	private Long userId;
+	
+	public Long getUserId() {
+		return userId;
+	}
 
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	
 	public String getGroup() {
 		return group;
 	}
