a93b136a506320cf0c48f1f2a7e36ac71b9bdcf0
Iván Perdomo <ivan@akvo.org>
2012-12-27 19:11:31 +0100

    Implements a dynamically generated js code
    * The main idea is to get before the app initilization the current logged user
    * The code register in the loader a `akvo-flow/currentuser` that needs to be required somewhere
    * When this code gets required, a FLOW.CurrentUser object is created

---
+++ GAE/src/org/waterforpeople/mapping/app/web/CurrentUserServlet.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (C) 2012 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo FLOW.
+ *
+ *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+package org.waterforpeople.mapping.app.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class CurrentUserServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -430515593814261770L;
+	private static final Logger log = Logger.getLogger(CurrentUserServlet.class
+			.getName());
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		final VelocityEngine engine = new VelocityEngine();
+		engine.setProperty("runtime.log.logsystem.class",
+				"org.apache.velocity.runtime.log.NullLogChute");
+		try {
+			engine.init();
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Could not initialize velocity", e);
+		}
+
+		Template t = null;
+		try {
+			t = engine.getTemplate("CurrentUser.vm");
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Could not get the template `CurrentUser`", e);
+			return;
+		}
+
+		final VelocityContext context = new VelocityContext();
+		// TODO: Change the CurrentUser with a UserDao?
+		context.put("user", UserServiceFactory.getUserService()
+				.getCurrentUser());
+
+		final StringWriter writer = new StringWriter();
+		t.merge(context, writer);
+
+		resp.setContentType("application/javascript;charset=UTF-8");
+
+		final PrintWriter pw = resp.getWriter();
+		pw.println(writer.toString());
+		pw.close();
+	}
+
+}
