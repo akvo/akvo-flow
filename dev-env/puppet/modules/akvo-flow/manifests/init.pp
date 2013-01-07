db50571226e5ee7a32a58809e2e34532034894f1
Daniel Karlsson <daniel@kardans.com>
2013-01-07 09:50:09 +0100

    Added a check for existing directory for the puppet script

--- dev-env/puppet/modules/akvo-flow/manifests/init.pp
+++ dev-env/puppet/modules/akvo-flow/manifests/init.pp
@@ -9,9 +9,14 @@ class akvo-flow {
     content => template("akvo-flow/build.properties.erb")
   }
 
+  file { "/akvo-flow/GAE/war/WEB-INF/appengine-generated/":
+    ensure => "directory",
+  }
+
   file { '/akvo-flow/GAE/war/WEB-INF/appengine-generated/local_db.bin':
     ensure => 'link',
     target => '/akvo-flow-env/local_db.bin',
+    require => File["/akvo-flow/GAE/war/WEB-INF/appengine-generated/"]
   }
 
   file { '/akvo-flow/GAE/src/org/waterforpeople/mapping/app/gwt/client/util/UploadConstants.properties':
