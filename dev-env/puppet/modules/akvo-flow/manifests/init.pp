421d45990ff19bc30fe63a9f917023117c61719f
Daniel Karlsson <daniel@kardans.com>
2013-01-07 08:27:21 +0100

    Added puppet desciptions to create symlinks to the correct path in the VM for non public files.

--- dev-env/puppet/modules/akvo-flow/manifests/init.pp
+++ dev-env/puppet/modules/akvo-flow/manifests/init.pp
@@ -1,10 +1,21 @@
 class akvo-flow {
-  # file { "/akvo-flow/Dashboard/app/js/lib/models/store_def.js":
-  #   ensure  => present,
-  #   content => template("akvo-flow/store_def.js_rest.erb")
-  # }
+  
   file { "/akvo-flow/GAE/war/WEB-INF/appengine-web.xml":
     ensure  => present,
     content => template("akvo-flow/appengine-web.xml.erb")
+  }
+  file { "/akvo-flow/GAE/build.properties":
+    ensure  => present,
+    content => template("akvo-flow/build.properties.erb")
+  }
+
+  file { '/akvo-flow/GAE/war/WEB-INF/appengine-generated/local_db.bin':
+    ensure => 'link',
+    target => '/akvo-flow-env/local_db.bin',
+  }
+
+  file { '/akvo-flow/GAE/src/org/waterforpeople/mapping/app/gwt/client/util/UploadConstants.properties':
+    ensure => 'link',
+    target => '/akvo-flow-env/UploadConstants.properties',
   }
 }
