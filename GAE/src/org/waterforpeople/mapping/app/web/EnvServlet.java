bf84e016441e40dee9d94033815e3e08d3715b94
Iván Perdomo <ivan@akvo.org>
2013-01-16 13:04:58 +0100

    Issue #109 - Expose available countries in FLOW.Env object

--- GAE/src/org/waterforpeople/mapping/app/web/EnvServlet.java
+++ GAE/src/org/waterforpeople/mapping/app/web/EnvServlet.java
@@ -19,6 +19,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,8 +32,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import com.gallatinsystems.common.Constants;
 import com.gallatinsystems.common.util.PropertyUtil;
+import com.gallatinsystems.framework.dao.BaseDAO;
+import com.gallatinsystems.gis.geography.domain.Country;
 
 public class EnvServlet extends HttpServlet {
 
@@ -68,7 +75,17 @@ public class EnvServlet extends HttpServlet {
 		}
 
 		final VelocityContext context = new VelocityContext();
-		context.put("env", PropertyUtil.getPropertiesMap(properties));
+		final Map<String, String> props = PropertyUtil
+				.getPropertiesMap(properties);
+
+		final BaseDAO<Country> countryDAO = new BaseDAO<Country>(Country.class);
+		final JSONArray jsonArray = new JSONArray();
+		for (Country c : countryDAO.list(Constants.ALL_RESULTS)) {
+			jsonArray.put(new JSONObject(c));
+		}
+		props.put("countries", jsonArray.toString());
+
+		context.put("env", props);
 
 		final StringWriter writer = new StringWriter();
 		t.merge(context, writer);
