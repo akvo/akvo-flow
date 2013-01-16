121a1603932a1697972c0259d6d251dfb82f525c
Iván Perdomo <ivan@akvo.org>
2013-01-16 13:14:33 +0100

    Issue #109 - Cleanup, remove unused import

--- GAE/src/org/waterforpeople/mapping/app/web/EnvServlet.java
+++ GAE/src/org/waterforpeople/mapping/app/web/EnvServlet.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
