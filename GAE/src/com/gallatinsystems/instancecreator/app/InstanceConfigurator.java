01f73d58c870fcbfe6bb777e01b649dc5216a129
Mark Tiele Westra <m.t.westra@akvo.org>
2013-01-31 14:33:24 +0100

    simplify instanceConfigurator.java

--- GAE/src/com/gallatinsystems/instancecreator/app/InstanceConfigurator.java
+++ GAE/src/com/gallatinsystems/instancecreator/app/InstanceConfigurator.java
@@ -38,6 +38,12 @@ import com.gallatinsystems.instancecreator.util.S3PolicySigner;
  * 
  * Class to facilitate configuring new instances based on template based
  * configuration files.
+ * The files being generated are:
+ * appengine-web.xml
+ * portal-gwt.xml
+ * surveyentry.gwt.xml
+ * UploadConstants.properties
+ * survey.properties
  * 
  * To use this utility, you must supply all of following values on the command
  * line in order:
@@ -46,12 +52,9 @@ import com.gallatinsystems.instancecreator.util.S3PolicySigner;
  * awsSecretKey - secret key used when connecting to Amazon S3 (for file uploads)
  * awsIdentifier  - the Amazon account identifier for S3
  * instanceName  - the name for this flow Instance. This will be used in url formation (so if the value supplied here is "flow-myorg" then the instance url would be flow-myorg.appspot.com
- * s3bucket - the root directory on S3 for this instance
  * directories - pipe (|) delimited list of S3 sub-directories (within the s3Bucket) for which upload keys should be generated. Usually this is the string "reports|devicezip|surveys|helpcontent|bootstrap|images
  * s3policyFileTemplateName - name of the template file to use for the s3 policy file generation. Usually this is s3policy.vm
  * signingKey - the secret key used to sign submissions from the Handheld. This can be any unique string though for best results, a large prime number is prefered
- * dataUploadUrl - url used for data uploads (usually the root of the S3 account... something like http://yourorg.s3.amazonaws.com/) (INCLUDE THE TRAILING SLASH)
- * serverBase - base url for the instance (usually something like http://yourorg.appspot.com)
  * storepass - password for the keystore used to sign the applet jar
  * keypass - password for the certificate key in the keystore for signing the applet jar
  * alias - certificate alias in the keystore for signing the applet jar
@@ -62,9 +65,13 @@ import com.gallatinsystems.instancecreator.util.S3PolicySigner;
  * localLocation - directory on the local file system that will store the output of running this utililty
  * keystore - full path to a java keystore that contains a certificate that will be used to sign the applet jar
  * mapsApiKey - a Google Maps API key that has been registered for the new instance's domain
+ * restApiKey - test
  * </pre>
  * 
- * 
+ * derived quantities are: 
+ * * s3bucket - the root directory on S3 for this instance - assumed to be same as instance name
+ * * dataUploadUrl - url used for data uploads (assumed to be the root of the S3 account ... http://instanceName.s3.amazonaws.com/)
+ * * serverBase - base url for the instance (assumed to be http://instanceName.appspot.com)
  * 
  * 
  */
@@ -80,33 +87,34 @@ public class InstanceConfigurator {
 		String[] directories;
 		String s3policyFileTemplateName;
 		aws_secret_key = args[0];
+		String instanceName = args[2];
 		ic.addAttribute("awsSecretKey", args[0]);
 		ic.addAttribute("awsIdentifier", args[1]);
-		ic.addAttribute("instanceName", args[2]);
-		ic.addAttribute("s3bucket", args[3]);
-		String s3bucket = args[3];
-		directories = args[4].split("\\|");
-		s3policyFileTemplateName = args[5];
+		ic.addAttribute("instanceName", instanceName);
+		ic.addAttribute("s3bucket", instanceName);
+		String s3bucket = instanceName;
+		directories = args[3].split("\\|");
+		s3policyFileTemplateName = args[4];
 		ic.addAttribute("s3Id", args[1]);
-		ic.addAttribute("signingKey", args[6]);
-		ic.addAttribute("dataUploadUrl", args[7]);
-		ic.addAttribute("serverBase", args[8]);
-		ic.addAttribute("surveyS3Url", args[7] + "surveys");
-		ic.addAttribute("s3urldevicezip", args[7] + "devicezip");
-		ic.addAttribute("storepass", args[9]);
-		ic.addAttribute("keypass", args[10]);
-		ic.addAttribute("alias", args[11]);
-		ic.addAttribute("alais", args[11]);
-		ic.addAttribute("reportsEmailAddress", args[12]);
-		ic.addAttribute("defaultPhotoCaption", args[13]);
-		ic.addAttribute("scoreAPFlag", args[14]);
-		ic.addAttribute("organization", args[15]);
-		ic.addAttribute("s3Url", args[7]);
-		ic.addAttribute("s3url", args[7]);
-		String localLocation = args[16];
-		ic.addAttribute("keystore", args[17]);
-		ic.addAttribute("mapsApiKey", args[18]);
-		ic.addAttribute("restApiKey",args[19]);		
+		ic.addAttribute("signingKey", args[5]);
+		ic.addAttribute("dataUploadUrl", "http://" + instanceName + ".s3.amazonaws.com");
+		ic.addAttribute("serverBase", "http://" + instanceName + ".appspot.com");
+		ic.addAttribute("surveyS3Url", "http://" + instanceName + ".s3.amazonaws.com/surveys");
+		ic.addAttribute("s3urldevicezip", "http://" + instanceName + ".s3.amazonaws.com/devicezip");
+		ic.addAttribute("storepass", args[6]);
+		ic.addAttribute("keypass", args[7]);
+		ic.addAttribute("alias", args[8]);
+		ic.addAttribute("alais", args[8]);
+		ic.addAttribute("reportsEmailAddress", args[9]);
+		ic.addAttribute("defaultPhotoCaption", args[10]);
+		ic.addAttribute("scoreAPFlag", args[11]);
+		ic.addAttribute("organization", args[12]);
+		 ic.addAttribute("s3Url", "http://" + instanceName + ".s3.amazonaws.com");
+		 ic.addAttribute("s3url", "http://" + instanceName + ".s3.amazonaws.com");
+		String localLocation = args[13];
+		ic.addAttribute("keystore", args[14]);
+		ic.addAttribute("mapsApiKey", args[15]);
+		ic.addAttribute("restApiKey",args[16]);		
 
 		localLocation = ic.createLocalDeployDir(localLocation, args[2]);
 
@@ -164,7 +172,7 @@ public class InstanceConfigurator {
 	}
 
 	private static void checkUsage(String[] args) {
-		if (args.length != 20) {
+		if (args.length != 17) {
 			System.out
 					.println("Bad command line arguments. Usage:\n\t java com.gallatinsystems.instancecreator.app.InstanceConfigurator <awsSecretKey> <awsIdentifier> <instanceName> <s3Bucket> <directories> <s3policyFileTemplateName> <signingKey> <dataUploadUrl> <serverBase> <storepass> <keypass> <alias> <reportsEmailAddress> <defaultPhotoCaption> <scoreAPFlag> <organization> <localLocation> <keystore> <mapsApiKey> <restApiKey>");
 			System.exit(1);
