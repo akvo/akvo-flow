be80bb87b7f31be2d7815bffe1cd5d98be4eff5a
Daniel Karlsson <daniel@kardans.com>
2013-02-07 07:41:48 +0100

    Removed Nginx symlink and created a Puppet template

--- dev-env/puppet/modules/akvo-flow/manifests/init.pp
+++ dev-env/puppet/modules/akvo-flow/manifests/init.pp
@@ -1,9 +1,15 @@
 class akvo-flow {
+
+  file { "/etc/nginx/sites-enabled/flow":
+    ensure  => present,
+    content => template("akvo-flow/nginx_flow.erb")
+  }
   
   file { "/akvo-flow/GAE/war/WEB-INF/appengine-web.xml":
     ensure  => present,
     content => template("akvo-flow/appengine-web.xml.erb")
   }
+
   file { "/akvo-flow/GAE/build.properties":
     ensure  => present,
     content => template("akvo-flow/build.properties.erb")
