akvo/akvo-flow

commit 61d05c9a714bf71f2ab53cac50dec6170bcb0c1d

updated summary computation

GAE/src/org/waterforpeople/mapping/dao/SurveyInstanceDAO.java
@@ -241,9 +241,9 @@ public class SurveyInstanceDAO extends BaseDAO<SurveyInstance> {
 
 	}
 
-	
 	/***********************
 	 * returns raw entities
+	 * 
 	 * @param returnKeysOnly
 	 * @param beginDate
 	 * @param endDate
@@ -354,6 +354,33 @@ public class SurveyInstanceDAO extends BaseDAO<SurveyInstance> {
 			q.setRange(0, count);
 		}
 		return (List<QuestionAnswerStore>) q.execute(instanceId);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<QuestionAnswerStore> listQuestionAnswerStore(Long instanceId,
+			String type, Integer count) {
+		PersistenceManager pm = PersistenceFilter.getManager();
+		Query query = pm.newQuery(QuestionAnswerStore.class);
+
+		Map<String, Object> paramMap = null;
+
+		StringBuilder filterString = new StringBuilder();
+		StringBuilder paramString = new StringBuilder();
+		paramMap = new HashMap<String, Object>();
+
+		appendNonNullParam("surveyInstanceId", filterString, paramString,
+				"Long", instanceId, paramMap);
+
+		appendNonNullParam("type", filterString, paramString, "String", type,
+				paramMap);
+
+		query.setFilter(filterString.toString());
+		query.declareParameters(paramString.toString());
+
+		if (count != null) {
+			query.setRange(0, count);
+		}
+		return (List<QuestionAnswerStore>) query.execute(instanceId);
 	}
 
 	/**

GAE/src/org/waterforpeople/mapping/analytics/SurveyQuestionSummarizer.java
@@ -19,10 +19,13 @@ package org.waterforpeople.mapping.analytics;
 import java.util.List;
 
 import org.waterforpeople.mapping.analytics.dao.SurveyQuestionSummaryDao;
+import org.waterforpeople.mapping.app.gwt.client.survey.QuestionDto.QuestionType;
 import org.waterforpeople.mapping.dao.SurveyInstanceDAO;
 import org.waterforpeople.mapping.domain.QuestionAnswerStore;
 
 import com.gallatinsystems.framework.analytics.summarization.DataSummarizer;
+import com.gallatinsystems.survey.dao.QuestionDao;
+import com.gallatinsystems.survey.domain.Question;
 
 /**
  * This class will summarize survey information by updating counts by response
@@ -39,8 +42,11 @@ public class SurveyQuestionSummarizer implements DataSummarizer {
 		if (key != null) {
 			SurveyInstanceDAO instanceDao = new SurveyInstanceDAO();
 			List<QuestionAnswerStore> answers = instanceDao
-					.listQuestionAnswerStore(new Long(key), null);
-			if (answers != null) {
+					.listQuestionAnswerStore(new Long(key), "VALUE", null);
+			if (answers != null && answers.size() > 0) {
+				QuestionDao questionDao = new QuestionDao();
+				List<Question> qList = questionDao.listQuestionByType(answers
+						.get(0).getSurveyId(), Question.Type.OPTION);
 				int i = 0;
 				if (offset != null) {
 					i = offset;
@@ -49,7 +55,10 @@ public class SurveyQuestionSummarizer implements DataSummarizer {
 				}
 				// process BATCH_SIZE items
 				while (i < answers.size() && i < offset + BATCH_SIZE) {
-					SurveyQuestionSummaryDao.incrementCount(answers.get(i),1);
+					if (isSummarizable(answers.get(i), qList)) {
+						SurveyQuestionSummaryDao.incrementCount(answers.get(i),
+								1);
+					}
 					i++;
 				}
 				// if we still have more answers to process, return false
@@ -59,6 +68,27 @@ public class SurveyQuestionSummarizer implements DataSummarizer {
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * returns true if the question type for the answer object is an OPTION type
+	 * @param answer
+	 * @param questions
+	 * @return
+	 */
+	private boolean isSummarizable(QuestionAnswerStore answer,
+			List<Question> questions) {
+		if (questions != null && answer != null) {
+			long id = Long.parseLong(answer.getQuestionID());
+			for (Question q : questions) {
+				if (q.getKey().getId() == id) {
+					return true;
+				}
+			}
+			return false;
+		} else {
+			return false;
+		}
 	}
 
 	@Override

GAE/src/org/waterforpeople/mapping/analytics/SurveyQuestionSummaryUpdater.java
@@ -21,6 +21,8 @@ import org.waterforpeople.mapping.domain.QuestionAnswerStore;
 
 import com.gallatinsystems.framework.analytics.summarization.DataSummarizer;
 import com.gallatinsystems.framework.domain.DataChangeRecord;
+import com.gallatinsystems.survey.dao.QuestionDao;
+import com.gallatinsystems.survey.domain.Question;
 
 /**
  * handles updates to questionSummary objects
@@ -48,15 +50,19 @@ public class SurveyQuestionSummaryUpdater implements DataSummarizer {
 			Integer offset, String cursor) {
 
 		DataChangeRecord changeRecord = new DataChangeRecord(value);
-		SurveyQuestionSummaryDao.incrementCount(
-				constructQAS(changeRecord.getId(), changeRecord.getOldVal()),
-				-1);
-		if (changeRecord.getNewVal() != null
-				&& changeRecord.getNewVal().trim().length() > 0) {
+		QuestionDao qDao = new QuestionDao();
+		Question q = qDao.getByKey(new Long(changeRecord.getId()));
+		if (q != null && Question.Type.OPTION.equals(q.getType())) {
 			SurveyQuestionSummaryDao
 					.incrementCount(
 							constructQAS(changeRecord.getId(),
-									changeRecord.getNewVal()), 1);
+									changeRecord.getOldVal()), -1);
+			if (changeRecord.getNewVal() != null
+					&& changeRecord.getNewVal().trim().length() > 0) {
+				SurveyQuestionSummaryDao.incrementCount(
+						constructQAS(changeRecord.getId(),
+								changeRecord.getNewVal()), 1);
+			}
 		}
 
 		return true;

GAE/src/org/waterforpeople/mapping/app/web/DataProcessorRestServlet.java
@@ -368,6 +368,9 @@ public class DataProcessorRestServlet extends AbstractRestApiServlet {
 	private Map<String, Map<String, Long>> summarizeQuestionAnswerStore(
 			Long surveyId, Date sinceDate) {
 		QuestionAnswerStoreDao qasDao = new QuestionAnswerStoreDao();
+		QuestionDao questionDao = new QuestionDao();
+		List<Question> qList = questionDao.listQuestionByType(surveyId,
+				Question.Type.OPTION);
 		String cursor = null;
 		Map<String, Map<String, Long>> summaryMap = new HashMap<String, Map<String, Long>>();
 		List<QuestionAnswerStore> qasList = null;
@@ -377,30 +380,54 @@ public class DataProcessorRestServlet extends AbstractRestApiServlet {
 			if (qasList != null && qasList.size() > 0) {
 				cursor = QuestionAnswerStoreDao.getCursor(qasList);
 				for (QuestionAnswerStore qas : qasList) {
-					String val = qas.getValue();
-					// skip images since the summary is meaningless in those
-					// cases
-					if (val == null
-							|| val.toLowerCase().trim().endsWith(".jpg")) {
-						continue;
+					if (isSummarizable(qas, qList)) {
+						String val = qas.getValue();
+						// skip images since the summary is meaningless in those
+						// cases
+						if (val == null
+								|| val.toLowerCase().trim().endsWith(".jpg")) {
+							continue;
+						}
+						Map<String, Long> countMap = summaryMap.get(qas
+								.getQuestionID());
+						if (countMap == null) {
+							countMap = new HashMap<String, Long>();
+							summaryMap.put(qas.getQuestionID(), countMap);
+						}
+						Long count = countMap.get(val);
+						if (count == null) {
+							count = 1L;
+						} else {
+							count = count + 1;
+						}
+						countMap.put(val, count);
 					}
-					Map<String, Long> countMap = summaryMap.get(qas
-							.getQuestionID());
-					if (countMap == null) {
-						countMap = new HashMap<String, Long>();
-						summaryMap.put(qas.getQuestionID(), countMap);
-					}
-					Long count = countMap.get(val);
-					if (count == null) {
-						count = 1L;
-					} else {
-						count = count + 1;
-					}
-					countMap.put(val, count);
 				}
 			}
 		} while (qasList != null && qasList.size() > 0);
 		return summaryMap;
+	}
+
+	/**
+	 * checks if a question is summarizable by looking for an association to an
+	 * OPTION question
+	 * 
+	 * @param qas
+	 * @param qList
+	 * @return
+	 */
+	private boolean isSummarizable(QuestionAnswerStore qas, List<Question> qList) {
+		if (qList != null && qas != null) {
+			for (Question q : qList) {
+				long id = Long.parseLong(qas.getQuestionID());
+				if (q.getKey().getId() == id) {
+					return true;
+				}
+			}
+			return false;
+		} else {
+			return false;
+		}
 	}
 
 	/**

commit 24127b76965fc2373e2f064d3f3408620739ef23

Issue #136 - Remove method introduced in changeset 61d05c9

GAE/src/org/waterforpeople/mapping/dao/SurveyInstanceDAO.java
@@ -365,33 +365,6 @@ public class SurveyInstanceDAO extends BaseDAO<SurveyInstance> {
 		return (List<QuestionAnswerStore>) q.execute(instanceId);
 	}
 
-	@SuppressWarnings("unchecked")
-	public List<QuestionAnswerStore> listQuestionAnswerStore(Long instanceId,
-			String type, Integer count) {
-		PersistenceManager pm = PersistenceFilter.getManager();
-		Query query = pm.newQuery(QuestionAnswerStore.class);
-
-		Map<String, Object> paramMap = null;
-
-		StringBuilder filterString = new StringBuilder();
-		StringBuilder paramString = new StringBuilder();
-		paramMap = new HashMap<String, Object>();
-
-		appendNonNullParam("surveyInstanceId", filterString, paramString,
-				"Long", instanceId, paramMap);
-
-		appendNonNullParam("type", filterString, paramString, "String", type,
-				paramMap);
-
-		query.setFilter(filterString.toString());
-		query.declareParameters(paramString.toString());
-
-		if (count != null) {
-			query.setRange(0, count);
-		}
-		return (List<QuestionAnswerStore>) query.execute(instanceId);
-	}
-
 	/**
 	 * lists all questionAnswerStore objects for a specific question
 	 * 