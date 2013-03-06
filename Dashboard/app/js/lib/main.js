1ed3e9f7d2def17415b1b5907bfdc8fccfe05724
Iván Perdomo <ivan@akvo.org>
2013-03-06 09:21:36 +0100

    Issue #134 - Move the location of vendor libraries

--- Dashboard/app/js/lib/main.js
+++ Dashboard/app/js/lib/main.js
@@ -1,11 +1,11 @@
 
-require('akvo-flow/vendor/jquery-ui-1.8.21.custom');
-require('akvo-flow/vendor/modernizr.custom.32662');
-require('akvo-flow/vendor/d3.v2.min');
-require('akvo-flow/vendor/jquery.dataTables');
-require('akvo-flow/vendor/handlebars-1.0.rc.1');
-require('akvo-flow/vendor/ember-1.0.0.pre-2-36');
-require('akvo-flow/vendor/ember-data-rev10');
+//require('vendor/jquery-ui-1.8.21.custom');
+//require('vendor/modernizr.custom.32662');
+//require('vendor/d3.v2.min');
+//require('vendor/jquery.dataTables');
+//require('vendor/handlebars-1.0.rc.1');
+//require('vendor/ember-1.0.0.pre-2-36');
+//require('vendor/ember-data-rev10');
 
 require('akvo-flow/all_locales');
 require('akvo-flow/models/FLOWrest-adapter-v2');
