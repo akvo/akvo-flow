35496229180189a3f4fe4f7c316eb689f55113f8
Daniel Karlsson <daniel@kardans.com>
2013-03-08 14:07:23 +0100

    Public map page and dashboard dep libs issues solved
    The public map and the admin now uses the same vendor
    libs. This means that if the user visits either and then the
    other vendor libs should be in the users browser cache,
    ready for use without the need for more downloads.
    
    . #134

--- Dashboard/app/js/lib/main.js
+++ Dashboard/app/js/lib/main.js
@@ -1,21 +1,4 @@
 
-//require('vendor/jquery-ui-1.8.21.custom');
-//require('vendor/modernizr.custom.32662');
-//require('vendor/d3.v2.min');
-//require('vendor/jquery.dataTables');
-//require('vendor/handlebars-1.0.rc.1');
-//require('vendor/ember-1.0.0.pre-2-36');
-//require('vendor/ember-data-rev10');
-
-
-require('vendor/jquery-ui-1.8.21.custom');
-require('vendor/modernizr.custom.32662');
-require('vendor/d3.v2.min');
-require('vendor/jquery.dataTables');
-require('vendor/handlebars-1.0.rc.1');
-require('vendor/ember-1.0.0.pre-2-36');
-require('vendor/ember-data-rev10');
-
 require('akvo-flow/all_locales');
 require('akvo-flow/models/FLOWrest-adapter-v2');
 require('akvo-flow/models/models');
