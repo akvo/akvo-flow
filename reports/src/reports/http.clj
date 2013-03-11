c212cd33e1dbacf057c5ba774fbaf9936456deac
Iván Perdomo <ivan@akvo.org>
2013-03-11 18:07:43 +0100

    Adds simple validation to http layer

--- reports/src/reports/http.clj
+++ reports/src/reports/http.clj
@@ -6,23 +6,35 @@
             [compojure.route :as route]
             [compojure.handler :as handler]))
 
-(defn- sample-payload []
+(def generate-sample
   {"baseURL" "http://instance.akvoflow.org"
    "surveyId" "123"
    "opts" {"exportType" "RAW_DATA"
            "locale" "en"
            "imgPrefix" "http://instance.s3.amazonaws.com/images"}})
 
+(def invalidate-sample
+  {"baseURL" "http://instance.akvoflow.org"
+   "surveyIds" [123 456 789]})
+
+(defn generate-report [params]
+  (if (empty? params)
+    {:status 400 :body {"sample" generate-sample}}
+    (response (sch/generate-report params))))
+
+(defn invalidate-cache [params]
+  (if (empty? params)
+    {:status 400 :body {"sample" invalidate-sample}}
+    (response (sch/invalidate-cache params))))
 
 (defroutes main-routes
   (POST "/generate" [:as {params :json-params}]
-        (response (sch/generate-report params)))
+        (generate-report params))
 
   (POST "/invalidate" [:as {params :json-params}]
-        (response (sch/invalidate-cache params)))
+        (invalidate-cache params))
 
   (route/not-found "Page not found"))
-
 
 (def app
   (-> (handler/site main-routes)
