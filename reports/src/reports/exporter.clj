cc2e4f41127000b586cf732a3e002b2b710afdfd
Iván Perdomo <ivan@akvo.org>
2013-04-03 19:01:33 +0200

    Use java.io.tmpdir for storing the exported files

--- reports/src/reports/exporter.clj
+++ reports/src/reports/exporter.clj
@@ -9,12 +9,16 @@
     "xls"
     "xlsx"))
 
+(defn- get-path []
+  (format "%s/%s/%s" (System/getProperty "java.io.tmpdir") "akvo/flow/reports" (UUID/randomUUID)))
+
 (defn- get-file [et id]
-  (let [path (str "/var/tmp/akvo/flow/reports/" (UUID/randomUUID))]
+  (let [path (get-path)]
     (.mkdirs (io/file path))
     (io/file (format "%s/%s-%s.%s" path et id (get-file-extension et)))))
 
 (defn ^File export-report [type base id options]
   (let [exporter (.getExporter (SurveyDataImportExportFactory.) type)
         file (get-file type id)]
-    (.export exporter {"surveyId" id} file base options) file))
+    (.export exporter {"surveyId" id} file base options)
+    file))
