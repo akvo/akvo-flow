fc29ce1cb68ec9838bc2f7a96fac864c315da437
Iván Perdomo <ivan@akvo.org>
2013-04-16 15:49:17 +0200

    Bump version
    * The new version includes the upload functionality

--- reports/project.clj
+++ reports/project.clj
@@ -1,4 +1,4 @@
-(defproject reports "0.1.0-SNAPSHOT"
+(defproject reports "0.2.0-SNAPSHOT"
   :description "HTTP layer to applets functionality"
   :url "https://github.com/akvo/akvo-flow"
   :license {:name "GNU Affero General Public License"
