711fc0ba94aa2fd98cc648e1837b4585791a67e6
Iván Perdomo <ivan@akvo.org>
2013-04-18 17:13:27 +0200

    Rollback and use exec /usr/bin/java instead of /bin/sh

--- reports/config/server/opt/flow/services/services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-JAR="/opt/akvo/flow/services/current.jar"
-CONFIG_REPO="/opt/akvo/flow/repo/akvo-flow-server-config"
-PORT_NUMBER=3000
-JVM_OPTS="-Xmx1024m -d64 -server"
-
-/usr/bin/java $JVM_OPTS -jar $JAR $CONFIG_REPO $PORT
