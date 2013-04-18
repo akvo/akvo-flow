5becac9ad48a40707101486941bf89fce1db34f0
Iván Perdomo <ivan@akvo.org>
2013-04-18 14:29:19 +0200

    Change Upstart conf file and adds service.sh helper

---
+++ reports/config/server/opt/flow/services/services.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+JAR="/opt/akvo/flow/services/current.jar"
+CONFIG_REPO="/opt/akvo/flow/repo/akvo-flow-server-config"
+PORT_NUMBER=3000
+LOGS="/var/log/akvo/flow-services.log"
+JVM_OPTS="-Xmx1024m -d64 -server"
+
+/usr/bin/java $JVM_OPTS -jar $JAR $CONFIG_REPO $PORT >> $LOG 2>&1
