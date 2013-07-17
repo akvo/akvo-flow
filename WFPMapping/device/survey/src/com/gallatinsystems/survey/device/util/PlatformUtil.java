b14a4d209e2ce53784aac1740d2e6236418ac8d2
Iván Perdomo <katratxo@gmail.com>
2013-07-17 09:42:04 +0200

    Issue #280 - Include header in new Java class

--- WFPMapping/device/survey/src/com/gallatinsystems/survey/device/util/PlatformUtil.java
+++ WFPMapping/device/survey/src/com/gallatinsystems/survey/device/util/PlatformUtil.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (C) 2013 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo FLOW.
+ *
+ *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+
 package com.gallatinsystems.survey.device.util;
 
 import android.content.Context;
@@ -10,7 +26,7 @@ import android.util.Log;
  */
 public class PlatformUtil {
 	private static final String TAG = PlatformUtil.class.getSimpleName();
-	
+
 	/**
 	 * Get the version name assigned in AndroidManifest.xml
 	 * @param context
