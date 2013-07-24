df44a425ef6a87875b797cecb40c62f883e3527c
nehafish <neha@akvo.org>
2013-07-25 01:14:19 +0200

    Dropdown Survey Group Selection Refactor with jquery trigger(change) event.

--- Dashboard/tests/casperjs/survey_reports.js
+++ Dashboard/tests/casperjs/survey_reports.js
@@ -159,12 +159,9 @@ casper.then(function () {
 		this.test.assertVisible('#ember8562', 'Survey Group Drop Down Visible');
 		// this.test.assertVisible(ember_xpatch('//*[contains(text(), "Select survey group")]'), 'Select Survey Group Visible');
 		this.test.assertSelectorHasText('select.ember-select option', "Select survey group");
-		this.test.assertSelectorHasText('select.ember-select option', "Akvo QA");
+		this.test.assertSelectorHasText('select.ember-select:nth-of-type(1)', "Akvo QA");
 
-		this.evaluate(function () {
-			$("select:has(option:contains('Akvo QA'))").change();
-		});
-	});
+	});        
 });
 
 casper.then(function () {
@@ -172,14 +169,37 @@ casper.then(function () {
 		// Iterate through select options to find group 'IPE Test', execute click()
 
 		this.evaluate( function() {
+				var val = '2164002';
+				var val2 = '2165002';
+
 		 		$('select.ember-select option').each(function(index, option) {
-   	 			if ($(this).text() === "IPE Test") {
-		 			$("select:has(option:contains('Akvo group'))").click(); 
-		 			console.log("PASS IPE Test Survey Click");
+   	 			if ($(this).text() === "Akvo QA") {
+				    $('select.ember-select:nth-of-type(1)').val(val);
+		 		    $('select.ember-select:nth-of-type(1)').trigger('change');
+		 			console.log("Akvo Survey Group Selected" + " " + val);
 		 			return false;
 					}
 				});
+
+		 		$('select.ember-select option').each(function(index, option) {
+		 			if ($(this).text() === "QA Eng Survey I") {
+		 				$('select.ember-select:nth-of-type(2)').val(val2);
+		 				$('select.ember-select:nth-of-type(2)').trigger('change');
+		 				console.log("Akvo Survey Selected" + " " + val2);
+		 				return false;
+					}
+				});
 		
+
+		 		// $('select.ember-select:nth-of-type(1)').change();
+		 		// $('select.ember-select:nth-of-type(1)').val(val);
+				// $('select.ember-select:nth-of-type(1)').change();
+
+        
+				// this.test.assertSelectorHasText('select.ember-select:nth-of-type(2)', "QA Eng Survey I");
+		});
+		
+		// this.test.assertSelectorHasText('select#ember5496.ember-view.ember-select', "QA Eng Survey I");
 
  	   // Iterate through select options for 'Neha Test', execute click()	
   				// $('select.ember-select option').each(function(index, option) {
@@ -188,9 +208,6 @@ casper.then(function () {
   				   // 	console.log("PASS Neha Test Survey Selected");
   				   // 	return false;
 				  //  }
- });
-
-
 		
 		casper.capture('screenshots/DataCleaningSelect_DropDown.png');
 		this.test.assertVisible(ember_xpath('//*[contains(text()," Raw data report ")]'), 'Raw Data Report Button Visible');
