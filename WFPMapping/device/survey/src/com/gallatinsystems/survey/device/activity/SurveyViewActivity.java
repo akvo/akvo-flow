452fbb44d5d309bfc3f4528008a9ca5a1ba4353c
Inigo Lopez de Heredia <inigo@akvo.org>
2013-08-01 17:19:49 +0100

    Remove unused imports

--- WFPMapping/device/survey/src/com/gallatinsystems/survey/device/activity/SurveyViewActivity.java
+++ WFPMapping/device/survey/src/com/gallatinsystems/survey/device/activity/SurveyViewActivity.java
@@ -26,9 +26,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import android.app.AlertDialog;
 import android.app.TabActivity;
