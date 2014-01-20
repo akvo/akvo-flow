9c2f730ca6ccb79b8a7ac60c30d612e76cefa177
Iván Perdomo <ivan@perdomo.me>
2014-01-20 17:51:41 +0100

    Issue #247 - Draft implementation of navigation for SurveyInstance
    * A user wants to see the details of a particular monitored entity,
      he/she clicks the view details, and the survey instances are shown,
      then if he/she wants to see the answers to the questions, a click on
      the view details for a particular survey instance is required

--- Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
+++ Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
@@ -1,9 +1,19 @@
 FLOW.MonitoringDataTableView = FLOW.View.extend({
-	showingDetailsDialog: false,
-	showDetailsDialog: function (evt) {
-      this.toggleProperty('showingDetailsDialog');
-	},
-	closeDetailsDialog: function () {
-      this.toggleProperty('showingDetailsDialog');
-	}
+  showingDetailsDialog: false,
+  showingSurveyInstanceDialog: false,
+
+  showDetailsDialog: function (evt) {
+    this.toggleProperty('showingDetailsDialog');
+  },
+
+  closeDetailsDialog: function () {
+    this.toggleProperty('showingDetailsDialog');
+  },
+
+  showSurveyInstanceDialog: function (evt) {
+    this.toggleProperty('showingSurveyInstanceDialog');
+  },
+  hideSurveyInstanceDialog: function (evt) {
+    this.toggleProperty('showingSurveyInstanceDialog');
+  }
 });
