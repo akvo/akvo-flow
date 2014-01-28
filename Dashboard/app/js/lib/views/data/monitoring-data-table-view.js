2007ba1cf5431c12dcb41ffcff2ba59c970e2d04
Iván Perdomo <ivan@perdomo.me>
2014-01-28 17:40:29 +0100

    Issue #276 - Code improvements on SurveyInstance details
    * Remove unused code
    * Change from `Dialog` to `Details` in function name

--- Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
+++ Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
@@ -9,12 +9,9 @@ FLOW.MonitoringDataTableView = FLOW.View.extend({
     this.toggleProperty('showingDetailsDialog');
   },
 
-  showSurveyInstanceDialog: function (evt) {
+  showSurveyInstanceDetails: function (evt) {
     FLOW.questionAnswerControl.doQuestionAnswerQuery(evt.context.get('keyId'));
     $('.si_details').hide();
     $('tr[data-flow-id="si_details_' + evt.context.get('keyId') + '"]').show();
-  },
-  hideSurveyInstanceDialog: function (evt) {
-    this.toggleProperty('showingSurveyInstanceDialog');
   }
 });
