1862c687d0f53e56c68acf4fc31229cd180687f5
Mark Tiele Westra <m.t.westra@akvo.org>
2014-03-03 20:42:15 +0100

    [#363] Method to create test locales, to test clustering

--- GAE/src/org/waterforpeople/mapping/app/web/rest/ActionRestService.java
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/ActionRestService.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Random;
 
 import javax.inject.Inject;
 
@@ -33,8 +34,11 @@ import org.waterforpeople.mapping.app.web.dto.BootstrapGeneratorRequest;
 import org.waterforpeople.mapping.app.web.dto.DataProcessorRequest;
 import org.waterforpeople.mapping.app.web.rest.dto.RestStatusDto;
 import org.waterforpeople.mapping.dao.DeviceApplicationDao;
+import org.waterforpeople.mapping.dao.QuestionAnswerStoreDao;
 import org.waterforpeople.mapping.dao.SurveyInstanceDAO;
 import org.waterforpeople.mapping.domain.DeviceApplication;
+import org.waterforpeople.mapping.domain.QuestionAnswerStore;
+import org.waterforpeople.mapping.domain.SurveyInstance;
 import org.waterforpeople.mapping.app.gwt.server.survey.SurveyServiceImpl;
 
 import com.gallatinsystems.common.Constants;
@@ -95,12 +99,59 @@ public class ActionRestService {
 			}
 		} else if ("populateGeocellsForLocale".equals(action)){
 			status = computeGeocellsForLocales();
+		} else if ("createTestLocales".equals(action)){
+			status = createTestLocales();
 		}
-
 		statusDto.setStatus(status);
 		response.put("actions", "[]");
 		response.put("meta", statusDto);
 		return response;
+	}
+
+	/**
+	*  Used to create test locales. The only field populated is surveyId,
+	* which is set to 1.
+	* To be used only to test clustering during development
+	* in order to speed this up, it is advisable to 
+	* comment out the code in SurveyalRestServlet which computes
+	* the geoplace while running this method.
+	**/ 
+	private String createTestLocales(){
+		double latc;
+		double lonc;
+		double lat;
+		double lon;
+
+		SurveyInstanceDAO sDao = new SurveyInstanceDAO();
+		QuestionAnswerStoreDao qaDao = new QuestionAnswerStoreDao();
+		Random generator = new Random();
+		// create random points, in clusters.
+		for (int i = 0 ; i < 1 ; i++){
+			latc = generator.nextDouble() * 120 - 60;
+			lonc = generator.nextDouble() * 360 - 180;
+			for (int j = 0; j < 10000; j++){
+				SurveyInstance newSI = new SurveyInstance();
+				newSI.setSurveyId(1L);
+				newSI = sDao.save(newSI);
+				QuestionAnswerStore newQAS = new QuestionAnswerStore();
+				newQAS.setSurveyInstanceId(newSI.getKey().getId());
+				newQAS.setType("GEO");
+				lat = latc + generator.nextDouble() * 10 - 5;
+				lon = lonc + generator.nextDouble() * 10 - 5;
+				String geoloc = lat + "|" + lon + "|" + 0 + "|" + "aaaaaa";
+				newQAS.setValue(geoloc);
+				newQAS = sDao.save(newQAS);
+
+				Queue queue = QueueFactory.getDefaultQueue();
+				queue.add(TaskOptions.Builder
+					.withUrl("/app_worker/surveyalservlet")
+					.param(SurveyalRestRequest.ACTION_PARAM,
+						SurveyalRestRequest.INGEST_INSTANCE_ACTION)
+					.param(SurveyalRestRequest.SURVEY_INSTANCE_PARAM,
+							newSI.getKey().getId() + ""));
+			}
+		}
+		return "ok";
 	}
 
 	/**
