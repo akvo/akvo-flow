ce9b2f4f135d537157c9e4ff0e2df015fd64ceed
Iván Perdomo <ivan@akvo.org>
2014-05-28 10:22:58 +0200

    [#276] Fix deployment after merge
    * gwt-servlet is required because we still have some references to GWT
    * Added missing import java.util.UUID

--- GAE/src/com/gallatinsystems/surveyal/app/web/SurveyalRestServlet.java
+++ GAE/src/com/gallatinsystems/surveyal/app/web/SurveyalRestServlet.java
@@ -22,6 +22,7 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
