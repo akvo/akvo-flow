37d7691a6bbcd8afe5606f1b6d0530f19b191cf3
Jonas Enlund <jonas.enlund@gmail.com>
2014-08-18 08:49:49 +0300

    [#717] Add UserRestService endpoints to create and delete apikeys
    * POST /users/{id}/apikeys -- Create new apikeys for user
    * DELETE /users/{id}/apikeys -- Delete apikeys for user

---
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package org.waterforpeople.mapping.app.web.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = -7254884856692314966L;
+}
