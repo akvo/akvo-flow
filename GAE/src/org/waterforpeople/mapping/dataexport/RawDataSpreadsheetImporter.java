f666274c77aa78a6014f15e4e3a80118978b51bd
Jonas Enlund <jonas.enlund@gmail.com>
2014-08-27 11:04:52 +0300

    [#650] Remove unused import

--- GAE/src/org/waterforpeople/mapping/dataexport/RawDataSpreadsheetImporter.java
+++ GAE/src/org/waterforpeople/mapping/dataexport/RawDataSpreadsheetImporter.java
@@ -30,7 +30,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
