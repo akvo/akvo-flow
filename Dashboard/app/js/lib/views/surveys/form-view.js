5f017f98addf0f7318cab505c27c82d135bbe26e
Jonas Enlund <jonas.enlund@gmail.com>
2014-10-27 08:38:22 +0200

    [#805] Show/collapse form basics

--- Dashboard/app/js/lib/views/surveys/form-view.js
+++ Dashboard/app/js/lib/views/surveys/form-view.js
@@ -1,3 +1,9 @@
 FLOW.FormView = Ember.View.extend({
-	templateName: 'navSurveys/form'
+	templateName: 'navSurveys/form',
+	showFormBasics: false,
+
+	toggleShowFormBasics: function () {
+		this.set('showFormBasics', !this.get('showFormBasics'));
+	}
+
 })
