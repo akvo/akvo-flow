3087a80a31bb51e98752e226e4356d88673ba7fe
Iván Perdomo <ivan@akvo.org>
2014-11-14 19:12:50 +0100

    [#818] WIP - Enable AffirmativeBased voting for securing requests

---
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/security/CustomVoter.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (C) 2014 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo FLOW.
+ *
+ *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+
+package org.waterforpeople.mapping.app.web.rest.security;
+
+import java.util.Collection;
+import java.util.logging.Logger;
+
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.FilterInvocation;
+
+public class CustomVoter implements AccessDecisionVoter<Object> {
+
+    private static final Logger log = Logger.getLogger(CustomVoter.class.getName());
+
+    @Override
+    public boolean supports(ConfigAttribute attribute) {
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return FilterInvocation.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public int vote(Authentication authentication, Object object,
+            Collection<ConfigAttribute> attributes) {
+
+        log.info(object.toString() + " - CustomVoter - voting: "
+                + AccessDecisionVoter.ACCESS_ABSTAIN);
+
+        return AccessDecisionVoter.ACCESS_ABSTAIN;
+    }
+}
