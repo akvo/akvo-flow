a511c130c95496734278d093aa25995d02df5224
Emmanuel Mulo <emmanuel@akvo.org>
2014-12-16 18:50:55 +0100

    [#1020] Fix variable definition

--- Dashboard/app/js/lib/controllers/survey-controllers.js
+++ Dashboard/app/js/lib/controllers/survey-controllers.js
@@ -273,8 +273,9 @@ FLOW.projectControl = Ember.ArrayController.create({
       var currentProjectAncestors = this.get('breadCrumbs').slice();
       currentProjectAncestors.reverse();// reversed to start matching from the most specific path.
       var i;
+      var path;
       for(i = 0; i < currentProjectAncestors.length; i++) {
-          var path = currentProjectAncestors[i].get('path');
+          path = currentProjectAncestors[i].get('path');
           if(path in FLOW.currentUser.pathPermissions){
               return FLOW.currentUser.pathPermissions[path];
           }
