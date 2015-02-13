f32337d4c11e6c1fca4cf39cc18a9db5d8b9363d
Jonas Enlund <jonas.enlund@gmail.com>
2015-02-13 18:04:43 +0200

    [#1147] Updates based on review from @iperdomo, @mtwestra and @muloem

--- scripts/data/src/org/akvo/gae/remoteapi/EnsureHouseholdPrivacy.java
+++ scripts/data/src/org/akvo/gae/remoteapi/EnsureHouseholdPrivacy.java
@@ -2,7 +2,9 @@ package org.akvo.gae.remoteapi;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;;
@@ -18,28 +20,22 @@ public class EnsureHouseholdPrivacy implements Process {
             System.exit(1);
         }
 
-        Query surveyQuery = new Query("SurveyGroup");
+	    Filter projectFilter = new FilterPredicate("projectType", FilterOperator.EQUAL, "PROJECT");
+	    Filter publicFilter = new FilterPredicate("privacyLevel", FilterOperator.EQUAL, "PUBLIC");
+	    Query surveyQuery = new Query("SurveyGroup").setFilter(CompositeFilterOperator.and(projectFilter, publicFilter));
 
         for (Entity survey : ds.prepare(surveyQuery).asIterable()) {
-            Object projectType = survey.getProperty("projectType");
-            if ("PROJECT".equals(projectType)) {
-                long id = survey.getKey().getId();
-                Filter filter = new FilterPredicate("surveyGroupId", FilterOperator.EQUAL, id);
-                Query formQuery = new Query("Survey").setFilter(filter);
-                boolean containsHouseholdData = false;
-                for (Entity form : ds.prepare(formQuery).asIterable()) {
-                    Object pointType = form.getProperty("pointType");
-                    if ("Household".equals(pointType)) {
-                        containsHouseholdData = true;
-                    }
-                }
-                Object privacyLevel = survey.getProperty("privacyLevel");
-                if (containsHouseholdData && !"PRIVATE".equals(privacyLevel)) {
-                    survey.setProperty("privacyType", "PRIVATE");
-                    ds.put(survey);
-                    System.out.println(String.format("Survey #%s (%s) privacyLevel changed to PRIVATE",
-                            id, survey.getProperty("name")));
-                }
+            long id = survey.getKey().getId();
+            Filter surveyIdFilter = new FilterPredicate("surveyGroupId", FilterOperator.EQUAL, id);
+            Filter householdFilter = new FilterPredicate("pointType", FilterOperator.EQUAL, "Household");
+            Query formQuery = new Query("Survey").setFilter(CompositeFilterOperator.and(surveyIdFilter, householdFilter));
+            boolean containsHouseholdData = !ds.prepare(formQuery).asList(FetchOptions.Builder.withLimit(1)).isEmpty();
+            if (containsHouseholdData) {
+                survey.setProperty("privacyLevel", "PRIVATE");
+                ds.put(survey);
+                System.out.println(String.format("Survey #%s (%s) privacyLevel changed to PRIVATE",
+                        id, survey.getProperty("name")));
+                
             }
         }
     }
