09b2371e7d92147640087b65d555a79b6077426c
Emmanuel Mulo <emmanuel@akvo.org>
2015-02-17 16:39:39 +0100

    [#1153] Add all jars in lib to dependencies
    * Review comments from @iperdomo

--- scripts/data/correct_folder_survey_path.sh
+++ scripts/data/correct_folder_survey_path.sh
@@ -4,4 +4,4 @@
 # and the sources have been compiled in a bin directory also located
 # in the /path/to/scripts/data directory
 
-java -cp bin:lib/appengine-api-1.0-sdk-1.8.8.jar:lib/appengine-remote-api.jar org.akvo.gae.remoteapi.RemoteAPI CorrectFolderSurveyPath $1 "$2" "$3"
+java -cp bin:"lib/*" org.akvo.gae.remoteapi.RemoteAPI CorrectFolderSurveyPath $1 "$2" "$3"
