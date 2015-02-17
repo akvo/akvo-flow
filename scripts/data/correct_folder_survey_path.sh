441f15af59145cbaa5d0e599824e7dd1d9a6527c
Emmanuel Mulo <emmanuel@akvo.org>
2015-02-17 14:45:36 +0100

    [#1153] Add short documentation + remove unnecessary dependencies

--- scripts/data/correct_folder_survey_path.sh
+++ scripts/data/correct_folder_survey_path.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
-java -cp bin:lib/appengine-api-1.0-sdk-1.8.8.jar:lib/appengine-api-labs-1.8.8.jar:lib/appengine-jsr107cache-1.8.8.jar:lib/appengine-remote-api.jar:lib/jsr107cache-1.1.jar  org.akvo.gae.remoteapi.RemoteAPI CorrectFolderSurveyPath $1 "$2" "$3"
+# Assumes the script is executed in the /path/to/scripts/data/ directory
+# and the sources have been compiled in a bin directory also located
+# in the /path/to/scripts/data directory
+
+java -cp bin:lib/appengine-api-1.0-sdk-1.8.8.jar:lib/appengine-remote-api.jar org.akvo.gae.remoteapi.RemoteAPI CorrectFolderSurveyPath $1 "$2" "$3"
