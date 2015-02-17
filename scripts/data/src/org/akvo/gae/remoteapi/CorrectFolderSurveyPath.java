6321211b81aeddfa1a347384ef3e156ed9b40546
Emmanuel Mulo <emmanuel@akvo.org>
2015-02-17 14:36:24 +0100

    [#1153] Data script to correct folder/survey/form path

---
+++ scripts/data/src/org/akvo/gae/remoteapi/CorrectFolderSurveyPath.java
@@ -0,0 +1,81 @@
+/*
+ *  Copyright (C) 2015 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo FLOW.
+ *
+ *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+
+package org.akvo.gae.remoteapi;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+
+public class CorrectFolderSurveyPath implements Process {
+
+    private final String SURVEY_GROUP = "SurveyGroup";
+    private final String SURVEY = "Survey";
+
+    @Override
+    public void execute(DatastoreService ds, String[] args) throws Exception {
+
+        System.out.println("Fetching survey groups and surveys.");
+
+        final List<Entity> surveyGroups = ds.prepare(new Query(SURVEY_GROUP)).asList(
+                FetchOptions.Builder.withDefaults());
+        final List<Entity> surveys = ds.prepare(new Query(SURVEY)).asList(
+                FetchOptions.Builder.withDefaults());
+
+        // combine entities to one list
+        LinkedList<Entity> completeList = new LinkedList<Entity>();
+        completeList.addAll(surveyGroups);
+        completeList.addAll(surveys);
+
+        for (Entity rootLevelEntity : completeList) {
+            if (rootLevelEntity.getKind().equals(SURVEY_GROUP)
+                    && rootLevelEntity.getProperty("parentId") == null) {
+                updateFolderSurveyPaths(rootLevelEntity, "", completeList);
+            }
+        }
+
+        System.out.println("Saving " + surveyGroups.size() + " survey groups; " + surveys.size()
+                + " surveys.");
+        ds.put(completeList);
+    }
+
+    private void updateFolderSurveyPaths(Entity entity, String pathPrefix, List<Entity> entities) {
+        // set folder/survey path
+        String entityPath = String.format("%s/%s", pathPrefix, entity.getProperty("name"));
+        entity.setProperty("path", entityPath);
+        System.out.println("Setting path for: " + entity.getKey() + " = " + entityPath);
+
+        // set all child folders/surveys paths
+        for (Entity childEntity : entities) {
+            Long parentId = getParentId(childEntity);
+            if (parentId != null && parentId.equals(entity.getKey().getId())) {
+                updateFolderSurveyPaths(childEntity, entityPath, entities);
+            }
+        }
+    }
+
+    private Long getParentId(Entity childEntity) {
+        if (childEntity.getKind().equals(SURVEY_GROUP)) {
+            return (Long) childEntity.getProperty("parentId");
+        } else {
+            return (Long) childEntity.getProperty("surveyGroupId");
+        }
+    }
+}
