cafc2330941fd1f4d4d1278dde374db0a171f21a
Jonas Enlund <jonas.enlund@gmail.com>
2015-02-26 16:12:09 +0200

    [#1117] Update deps to latest

--- Dashboard/app/cljs/project.clj
+++ Dashboard/app/cljs/project.clj
@@ -3,13 +3,13 @@
   :url "http://akvo.org/products/akvoflow/"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/clojurescript "0.0-2913"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [om "0.8.0-alpha2"]
-                 [cljs-ajax "0.3.3"]
-                 [sablono "0.2.22"]]
+                 [org.omcljs/om "0.8.8"]
+                 [cljs-ajax "0.3.10"]
+                 [sablono "0.3.4"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.5"]]
 
   :source-paths ["src"]
 
