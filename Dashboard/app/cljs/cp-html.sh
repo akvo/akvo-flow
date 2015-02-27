7d5c62d7681206fe08892d1c2ce43589a11ddbe1
Jonas Enlund <jonas.enlund@gmail.com>
2015-02-27 15:14:01 +0200

    [#1117] Tweaks based on @iperdomo's review

--- Dashboard/app/cljs/cp-html.sh
+++ Dashboard/app/cljs/cp-html.sh
@@ -5,7 +5,7 @@ OUT_ROOT=../../../GAE/war/admin/frames/
 mkdir -p ${OUT_ROOT}
 
 if [[ $* == *--production* ]]; then
-    cat users.html | sed 's/users\.css/users\.min\.css/' > ${OUT_ROOT}users.html
+    sed 's/users\.css/users\.min\.css/' users.html > ${OUT_ROOT}users.html
 else
     cp users.html ${OUT_ROOT}users.html
 fi
