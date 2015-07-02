bcb5fe16c8301d3e0305ceaf2e4eeb93c8dbb817
Jonas Enlund <jonas.enlund@gmail.com>
2015-07-02 13:56:03 +0300

    [#1280] Add named_maps endpoints

--- GAE/src/org/waterforpeople/mapping/app/web/rest/CartodbRestService.java
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/CartodbRestService.java
@@ -2,9 +2,14 @@
 package org.waterforpeople.mapping.app.web.rest;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,10 +17,12 @@ import java.util.Map;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.waterforpeople.mapping.app.web.rest.dto.NamedMapPayload;
 
 import com.gallatinsystems.common.util.PropertyUtil;
 
@@ -23,8 +30,13 @@ import com.gallatinsystems.common.util.PropertyUtil;
 @RequestMapping("/cartodb")
 public class CartodbRestService {
 
-    private static final String API_KEY = PropertyUtil.getProperty("cartodbApiKey");
+    private static final String CDB_API_KEY = PropertyUtil.getProperty("cartodbApiKey");
     private static final String SQL_API = PropertyUtil.getProperty("cartodbSqlApi");
+
+    private static final String CDB_ACCOUNT_NAME = PropertyUtil.getProperty("cartodbAccountName");
+    private static final String CDB_HOST = PropertyUtil.getProperty("cartodbHost");
+    private static final String CDB_TILER_PORT = PropertyUtil.getProperty("cartodbTilerPort");
+
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @RequestMapping(method = RequestMethod.GET, value = "answers")
@@ -82,12 +94,14 @@ public class CartodbRestService {
 
     @RequestMapping(method = RequestMethod.POST, value = "questions")
     @ResponseBody
-    public Map<String, Object> getQuestions(@RequestParam(value = "form_id", required = true) Long formId) {
+    public Map<String, Object> getQuestions(
+            @RequestParam(value = "form_id", required = true) Long formId) {
 
         Map<String, Object> response = new HashMap<>();
         response.put("questions", null);
         try {
-            response.put("questions",
+            response.put(
+                    "questions",
                     queryCartodb(String.format("SELECT * FROM question WHERE form_id = %d", formId)));
             return response;
         } catch (IOException e) {
@@ -123,17 +137,84 @@ public class CartodbRestService {
     }
 
     @SuppressWarnings("unchecked")
+    @RequestMapping(method = RequestMethod.GET, value = "named_maps")
+    @ResponseBody
+    public Map<String, Object> getNamedMaps() throws IOException {
+        HttpURLConnection connection = getConnection("GET", mapsApiURL());
+        return objectMapper.readValue(connection.getInputStream(), Map.class);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "named_maps")
+    @ResponseBody
+    public Map<String, Object> createNamedMaps(
+            @RequestBody NamedMapPayload payload)
+            throws IOException {
+
+        HttpURLConnection connection = getConnection("POST", mapsApiURL());
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        objectMapper.writeValue(writer, buildNamedMap(payload));
+        writer.close();
+
+        InputStream result = connection.getInputStream();
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> resultMap = objectMapper.readValue(result, Map.class);
+
+        return resultMap;
+    }
+
+    @SuppressWarnings("unchecked")
     private static List<Map<String, Object>> queryCartodb(String query) throws IOException {
-        String urlString = String.format(SQL_API + "?q=%s&api_key=%s",
-                URLEncoder.encode(query, "UTF-8"), API_KEY);
-        URL url = new URL(urlString);
+        HttpURLConnection connection = getConnection("GET", sqlApiURL(query));
+        JsonNode jsonNode = objectMapper.readTree(connection.getInputStream());
+        return objectMapper.convertValue(jsonNode.get("rows"), List.class);
+    }
+
+    private static HttpURLConnection getConnection(String method, URL url)
+            throws IOException {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setDoOutput(true);
-        connection.setRequestMethod("GET");
+        connection.setRequestMethod(method);
         connection.setRequestProperty("Content-Type", "application/json");
+        return connection;
 
-        JsonNode jsonNode = objectMapper.readTree(connection.getInputStream());
+    }
 
-        return objectMapper.convertValue(jsonNode.get("rows"), List.class);
+    private static final URL mapsApiURL() throws MalformedURLException {
+        return new URL(String.format("http://%s.%s:%s/api/v1/map/named?api_key=%s",
+                CDB_ACCOUNT_NAME, CDB_HOST, CDB_TILER_PORT, CDB_API_KEY));
+    }
+
+    private static final URL sqlApiURL(String query) throws MalformedURLException,
+            UnsupportedEncodingException {
+        // TODO: Build URL properly without SQL_API
+        String urlString = String.format(SQL_API + "?q=%s&api_key=%s",
+                URLEncoder.encode(query, "UTF-8"), CDB_API_KEY);
+        return new URL(urlString);
+    }
+
+    private static final Map<String, Object> buildNamedMap(NamedMapPayload namedMapPayload) {
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("name", namedMapPayload.getName());
+        result.put("version", "0.0.1");
+        Map<String, String> authMap = new HashMap<>();
+        authMap.put("method", "open");
+        result.put("auth", authMap);
+        Map<String, Object> layerGroupMap = new HashMap<>();
+        Map<String, Object> optionsMap = new HashMap<>();
+        optionsMap.put("cartocss_version", "2.1.1");
+        optionsMap.put("cartocss", namedMapPayload.getCartocss());
+        optionsMap.put("sql", namedMapPayload.getSql());
+        optionsMap.put("interactivity", namedMapPayload.getInteractivity());
+        Map<String, Object> layerMap = new HashMap<>();
+        layerMap.put("type", "cartodb");
+        layerMap.put("options", optionsMap);
+        List<Object> layersList = new ArrayList<>();
+        layersList.add(layerMap);
+        layerGroupMap.put("layers", layersList);
+        result.put("layergroup", layerGroupMap);
+
+        return result;
     }
 }
