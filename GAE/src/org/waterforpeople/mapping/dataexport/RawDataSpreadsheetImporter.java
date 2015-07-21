afc445917164e9e6e88961af1c341a5edb14484c
Jonas Enlund <jonas.enlund@gmail.com>
2015-07-21 10:11:22 +0300

    [#1204] Adjust first question offset index

--- GAE/src/org/waterforpeople/mapping/dataexport/RawDataSpreadsheetImporter.java
+++ GAE/src/org/waterforpeople/mapping/dataexport/RawDataSpreadsheetImporter.java
@@ -192,11 +192,9 @@ public class RawDataSpreadsheetImporter implements DataImporter {
                 String durationSeconds = null;
                 StringBuilder sb = new StringBuilder();
 
-                // Monitoring headers
-                // [identifier, displayName, instanceId, date, submitter, duration, questions...]
-
-                // Non-monitoring headers
-                // [instanceId, date, submitter, duration, questions...]
+                // Headers
+                // [identifier, displayName, deviceIdentifier, instanceId, date, submitter,
+                // duration, questions...]
 
                 int instanceIdx = firstQuestionCol - 4;
                 int dateIdx = firstQuestionCol - 3;
@@ -213,7 +211,7 @@ public class RawDataSpreadsheetImporter implements DataImporter {
                     if (cell.getColumnIndex() == instanceIdx) {
                         if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
                             instanceId = new Double(cell.getNumericCellValue())
-                            .intValue() + "";
+                                    .intValue() + "";
                         } else if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
                             instanceId = cell.getStringCellValue();
 
@@ -250,13 +248,13 @@ public class RawDataSpreadsheetImporter implements DataImporter {
                     if (cell.getColumnIndex() == durationIdx) {
                         if (hasDurationCol) {
                             switch (cell.getCellType()) {
-                                // if the cell type is string, we expect hh:mm:ss format
+                            // if the cell type is string, we expect hh:mm:ss format
                                 case Cell.CELL_TYPE_STRING:
                                     duration = cell.getStringCellValue();
                                     durationSeconds = String.valueOf(durationToSeconds(duration));
                                     digest.update(duration.getBytes());
                                     break;
-                                    // if the cell type if numeric, we expect a single seconds value
+                                // if the cell type if numeric, we expect a single seconds value
                                 case Cell.CELL_TYPE_NUMERIC:
                                     durationSeconds = String.valueOf(cell.getNumericCellValue());
                                     digest.update(durationSeconds.getBytes());
@@ -336,9 +334,9 @@ public class RawDataSpreadsheetImporter implements DataImporter {
                                     "questionId="
                                             + questionIDColMap.get(cell
                                                     .getColumnIndex())
-                                                    + "|value=").append(
-                                                            cellVal != null ? URLEncoder.encode(
-                                                                    cellVal, "UTF-8") : "");
+                                            + "|value=").append(
+                                    cellVal != null ? URLEncoder.encode(
+                                            cellVal, "UTF-8") : "");
                         } else {
                             hasValue = true;
                             sb.append("questionId="
@@ -401,8 +399,8 @@ public class RawDataSpreadsheetImporter implements DataImporter {
                             instanceId == null ? null
                                     : getResetUrlString(instanceId, dateString, submitter,
                                             durationSeconds),
-                                            sb.toString(),
-                                            criteria.get(KEY_PARAM));
+                            sb.toString(),
+                            criteria.get(KEY_PARAM));
 
                 } else {
                     // if we didn't need to upload, then just increment our
@@ -559,7 +557,7 @@ public class RawDataSpreadsheetImporter implements DataImporter {
                     if (!firstQuestionFound && cellValue.matches("[0-9]+\\|.+")) {
                         firstQuestionFound = true;
                         int idx = cell.getColumnIndex();
-                        if (!(idx == 4 || idx == 6)) {
+                        if (idx != 7) {
                             errorMap.put(idx, "Found the first question at the wrong column index");
                             break;
                         }
