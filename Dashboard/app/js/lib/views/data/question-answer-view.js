4e8ff9b9e90c7e1358f1acbeaaa3c50d886f6f5d
gabemart <g4bemart@gmail.com>
2015-08-20 13:07:44 +0100

    Revert "[#864] Add checkbox menu to edit multiple-option data"
    This reverts commit 9e2ab82fb2fce14fa434ec848cf0df19ea236f5e.

--- Dashboard/app/js/lib/views/data/question-answer-view.js
+++ Dashboard/app/js/lib/views/data/question-answer-view.js
@@ -10,7 +10,6 @@ function formatDate(value) {
 FLOW.QuestionAnswerView = Ember.View.extend({
   isTextType: false,
   isOptionType: false,
-  isMultipleOptionType: false,
   isNumberType: false,
   isBarcodeType: false,
   isGeoshapeType: false,
@@ -23,7 +22,6 @@ FLOW.QuestionAnswerView = Ember.View.extend({
   inEditMode: false,
   isNotEditable: false,
   value: null,
-  displayValue: null,
   numberValue: null,
   date: null,
   photoUrl: null,
@@ -41,10 +39,8 @@ FLOW.QuestionAnswerView = Ember.View.extend({
     questionId = this.content.get('questionID');
     q = FLOW.store.find(FLOW.Question, questionId);
     type = q.get('type');
-    multipleFlag = q.get('allowMultipleFlag');
     this.set('isTextType', type == 'FREE_TEXT' || type === 'CASCADE');
     this.set('isOptionType', type == 'OPTION');
-    this.set('isMultipleOptionType', multipleFlag === true);
     this.set('isNumberType', type == 'NUMBER');
     this.set('isBarcodeType', type == 'BARCODE');
     this.set('isPhotoType', type == 'PHOTO');
@@ -94,47 +90,13 @@ FLOW.QuestionAnswerView = Ember.View.extend({
       this.set('optionsList', tempList);
 
       // set answer
-
-      if (!this.get('isMultipleOptionType')) {
-        qaValue = this.content.get('value');
-        this.get('optionsList').forEach(function (item) {
-          if (item.get('value') == qaValue) {
-            choice = item;
-          }
-        });
-        this.set('optionChoice', choice);
-      } else {
-
-        /*
-        Values for multiple-option questions are stored as pipe-separated strings.
-        Add spaces on either side of the pipes to the display value to make it
-        easier to read.
-        */
-
-        this.content.set('displayValue', this.content.get('value').replace(/\|/g, ' | '));         
-
-        /* 
-        Set the initial value for multiple-option questions.
-        Iterate through the optionsList array and mark each
-        option we have stored.
-        */
-        tempList = [];
-        qaValue = [];
-        qaValue = this.content.get('value').split('|');
-        this.get('optionsList').forEach(function (item) {
-          var isSelected = false;
-          qaValue.forEach(function (qaItem) {
-            if (item.get('value') === qaItem.toString()) {
-              isSelected = true;
-            }
-          });
-          tempList.push(Ember.Object.create({
-            isSelected: isSelected,
-            value: item.get('value'),
-          }));         
-        });  
-        this.set('optionsList', tempList);
-      }
+      qaValue = this.content.get('value');
+      this.get('optionsList').forEach(function (item) {
+        if (item.get('value') == qaValue) {
+          choice = item;
+        }
+      });
+      this.set('optionChoice', choice);
     }
     if ((this.get('isPhotoType') || this.get('isVideoType')) && !Ember.empty(this.content.get('value'))) {
       // Since photos have a leading path from devices that we need to trim
@@ -153,7 +115,7 @@ FLOW.QuestionAnswerView = Ember.View.extend({
   },
 
   doSave: function () {
-    var value, valueArr, tempDate = null;
+    var tempDate = null;
     if (this.get('isDateType')) {
       if (Ember.empty(this.get('date'))) {
         this.content.set('value', null);
@@ -166,19 +128,7 @@ FLOW.QuestionAnswerView = Ember.View.extend({
         }
       }
     } else if (this.get('isOptionType')) {
-      if (this.get('isMultipleOptionType')) {
-        valueArr = [];
-        this.get('optionsList').forEach( function(item) {
-          if (item.get('isSelected')) {
-            valueArr.push(item.get('value'));
-          }
-        });
-        value = valueArr.join('|');
-        this.content.set('value', value);
-        this.content.set('displayValue', value.replace(/\|/g, ' | '));
-      } else {
-        this.content.set('value', this.optionChoice.get('value'));
-      }
+      this.content.set('value', this.optionChoice.get('value'));
     } else if (this.get('isNumberType')) {
       this.content.set('value', this.get('numberValue'));
     } else {
