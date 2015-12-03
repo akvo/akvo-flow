43a45c2924773dc5343313b14b4443a369437d64
Inigo Lopez de Heredia <inigo@akvo.org>
2015-12-03 13:17:54 +0100

    [#828] Add missing XML annotation

--- GAE/src/com/gallatinsystems/survey/domain/xml/Option.java
+++ GAE/src/com/gallatinsystems/survey/domain/xml/Option.java
@@ -58,6 +58,7 @@ public class Option {
     @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
     @XmlSchemaType(name = "NMTOKEN")
     protected String value;
+    @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
     @XmlSchemaType(name = "NMTOKEN")
     protected String code;
 
