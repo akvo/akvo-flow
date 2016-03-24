17a1f3df8eda91f779ccdb32aaa9808c61f9177b
Emmanuel Mulo <emmanuel@akvo.org>
2016-03-24 14:04:23 +0100

    [#1602] Remove old locales template file
    * Remove ununsed js dependency

--- Dashboard/app/js/lib/main.js
+++ Dashboard/app/js/lib/main.js
@@ -1,6 +1,5 @@
 require('akvo-flow/models/FLOWrest-adapter-v2-common');
 require('akvo-flow/models/models');
-//require('akvo-flow/models/fixtures');
 require('akvo-flow/flowenv');
 require('akvo-flow/controllers/controllers');
 require('akvo-flow/views/views');
