3408571adc99307b0cfea510321753acc9bd9887
rumca <ruarcc@gmail.com>
2016-05-06 09:03:36 +0100

    [#1566] adding bahasa translations

--- Dashboard/app/js/lib/controllers/general-controllers-common.js
+++ Dashboard/app/js/lib/controllers/general-controllers-common.js
@@ -4,7 +4,8 @@ FLOW.dashboardLanguageControl = Ember.Object.create({
   content: [{ label: "English (Default)", value: "en"},
             { label: "Español", value: "es" },
             { label: "Français", value: "fr" },
-            { label: "Português", value: "pt" }],
+            { label: "Português", value: "pt" }
+            { label: "Bahasa", value: "id"}],
 
   languageChanged: function () {
     var localeUrl = '/ui-strings.js?locale=' + this.dashboardLanguage;
