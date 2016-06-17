36f6b369fb56b6a11b0dc75f1a87ebe7cb157215
Emmanuel Mulo <emmanuel@akvo.org>
2016-06-17 16:37:56 +0200

    [#1659] Add data approval domain classes

---
+++ GAE/src/com/gallatinsystems/survey/domain/ApprovalStep.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2016 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo Flow.
+ *
+ *  Akvo Flow is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+
+package com.gallatinsystems.survey.domain;
+
+import javax.jdo.annotations.PersistenceCapable;
+
+import com.gallatinsystems.framework.domain.BaseDomain;
+
+/**
+ * An ApprovalStep entity stores information about the steps in set of data approvals. These steps
+ * may or may not have to be executed in an ordered sequence.
+ */
+@PersistenceCapable
+public class ApprovalStep extends BaseDomain {
+
+    private static final long serialVersionUID = -5473782085204354921L;
+
+    private String title;
+
+    private int order;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+}
