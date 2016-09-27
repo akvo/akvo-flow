55a6aa8d069535a4cfb2d461e0af8963105307aa
Emmanuel Mulo <emmanuel@akvo.org>
2016-09-27 08:53:13 +0200

    [#1664] Adding pending status for approvals

--- Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
+++ Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
@@ -119,5 +119,5 @@ FLOW.DataPointApprovalStatusView = FLOW.View.extend({
  * The data approval block for each data point
  */
 FLOW.DataPointApprovalView = FLOW.View.extend({
-    approvalStatus: Ember.A([{ label: Ember.String.loc('_approved') },{ label: Ember.String.loc('_rejected')}])
+    approvalStatus: Ember.A([{label: Ember.String.loc('_pending')}, { label: Ember.String.loc('_approved') },{ label: Ember.String.loc('_rejected')}])
 });
