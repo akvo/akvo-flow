184125e723251cc31c6294bcf040f8bb4086dcc1
Mark Tiele Westra <m.t.westra@akvo.org>
2016-10-19 15:45:10 +0200

    [#1577] whitespace formatting only
    * no code changes

--- GAE/src/org/waterforpeople/mapping/domain/CaddisflyResult.java
+++ GAE/src/org/waterforpeople/mapping/domain/CaddisflyResult.java
@@ -18,42 +18,42 @@ package org.waterforpeople.mapping.domain;
 import java.io.Serializable;
 
 public class CaddisflyResult implements Serializable, Comparable {
-	private static final long serialVersionUID = 1L;
-	private String name;
-	private String unit;
-	private int id;
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String unit;
+    private int id;
 
-	public CaddisflyResult(String name, String unit, int id) {
-		this.setName(name);
-		this.setUnit(unit);
-		this.setId(id);
-	}
+    public CaddisflyResult(String name, String unit, int id) {
+        this.setName(name);
+        this.setUnit(unit);
+        this.setId(id);
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getUnit() {
-		return unit;
-	}
+    public String getUnit() {
+        return unit;
+    }
 
-	public void setUnit(String unit) {
-		this.unit = unit;
-	}
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public int compareTo(Object o) {
-		return (id - ((CaddisflyResult) o).getId());
-	}
+    public int compareTo(Object o) {
+        return (id - ((CaddisflyResult) o).getId());
+    }
 }
