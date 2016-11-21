caea4172741bb21d367a8618563dcbc0df50a640
Emmanuel Mulo <emmanuel@akvo.org>
2016-11-21 10:41:15 +0300

    [#1786] Load data point approvals after retrieving all data points

--- Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
+++ Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
@@ -49,7 +49,17 @@ FLOW.MonitoringDataTableView = FLOW.View.extend({
 	  if (this.get('cursorStart')) {
 		criteria.since = this.get('cursorStart');
 	  }
-      FLOW.router.surveyedLocaleController.populate(criteria);
+      var surveyedLocaleController = FLOW.router.get('surveyedLocaleController');
+      surveyedLocaleController.populate(criteria);
+
+      surveyedLocaleController.get('content').on('didLoad', function () {
+          var surveyedLocales = this;
+          var surveyedLocaleIds = Ember.A();
+          surveyedLocales.forEach(function (item) {
+              surveyedLocaleIds.addObject(item.get('keyId'));
+          })
+          FLOW.router.dataPointApprovalController.loadBySurveyedLocaleId(surveyedLocaleIds);
+      })
   },
 
   doNextPage: function () {
