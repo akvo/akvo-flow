1ebea5d25132eb91b4d039bc6443dc22dcf9e210
Stellan Lagerström <stellanl@akvo.org>
2017-03-21 16:29:57 +0100

    [#1203]Update the other caller.

--- GAE/src/org/waterforpeople/mapping/app/web/rest/QuestionRestService.java
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/QuestionRestService.java
@@ -392,7 +392,7 @@ public class QuestionRestService {
             return null;
         }
         return SurveyUtils.copyQuestion(source, dto.getQuestionGroupId(), dto.getOrder(),
-                source.getSurveyId());
+                source.getSurveyId(), null);
     }
 
     private Question newQuestion(QuestionDto dto) {
