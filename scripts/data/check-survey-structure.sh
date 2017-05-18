5f0ecbe3408bbeba4afa104f2caa09adc95cf9b1
stellanl <stellanl@akvo.org>
2017-05-18 16:03:42 +0200

    [#2098]Depersonalise shellscript.

--- scripts/data/check-survey-structure.sh
+++ scripts/data/check-survey-structure.sh
@@ -4,7 +4,7 @@
 
 APP_ID=$1
 SERVICE_ACCOUNT="sa-$APP_ID@$APP_ID.iam.gserviceaccount.com"
-P12_FILE_PATH="/home/stellan/dev/akvo-flow-server-config/$1/$1.p12"
+P12_FILE_PATH="/path/to/akvo-repositories/akvo-flow-server-config/$1/$1.p12"
 
 java -cp bin:"lib/*" \
      org.akvo.gae.remoteapi.RemoteAPI \
