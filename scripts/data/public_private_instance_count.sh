7c273f47f5b16073827d632b3a92c82b1773c004
stellanl <stellanl@akvo.org>
2017-07-13 01:33:40 +0200

    [#2160]Undo accidental change.

--- scripts/data/public_private_instance_count.sh
+++ scripts/data/public_private_instance_count.sh
@@ -5,7 +5,7 @@
 
 APP_ID=$1
 SERVICE_ACCOUNT="sa-$APP_ID@$APP_ID.iam.gserviceaccount.com"
-P12_FILE_PATH="/home/stellan/dev/akvo-flow-server-config/$1/$1.p12"
+P12_FILE_PATH="/path/to/akvo-flow-server-config/$1/$1.p12"
 CHUNK_SIZE="300"
 
 java -cp bin:"lib/*" \
