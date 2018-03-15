e2e3bab003ddc7fc5dcf2c8b70dab1b2cad1562c
bogere goldsoft <goldsoft25@gmail.com>
2018-03-15 12:10:24 +0300

    [#2531]-Clear the data point records when user navigates away from the monitoring tab

--- Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
+++ Dashboard/app/js/lib/views/data/monitoring-data-table-view.js
@@ -98,6 +98,10 @@ FLOW.MonitoringDataTableView = FLOW.View.extend({
   hasPrevPage: function () {
     return FLOW.router.surveyedLocaleController.get('pageNumber');
   }.property('FLOW.router.surveyedLocaleController.pageNumber'),
+  
+  willDestroyElement: function () {
+    FLOW.router.surveyedLocaleController.set('currentContents', null)
+  }
 });
 
 /**
