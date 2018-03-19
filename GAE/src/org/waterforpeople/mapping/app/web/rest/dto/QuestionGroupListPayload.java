a27b9706f48ea56a731ba254e9120e76529ef6ba
stellanl <stellanl@akvo.org>
2018-03-19 15:38:09 +0100

    [#2523]Add new payload class.

---
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/dto/QuestionGroupListPayload.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (C) 2018 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo FLOW.
+ *
+ *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
+
+package org.waterforpeople.mapping.app.web.rest.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.waterforpeople.mapping.app.gwt.client.survey.QuestionGroupDto;
+
+public class QuestionGroupListPayload implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    List<QuestionGroupDto> questiongroups = null;
+
+    public List<QuestionGroupDto> getQuestionGroups() {
+        return questiongroups;
+    }
+
+    public void setQuestionGroups(List<QuestionGroupDto> questiongroups) {
+        this.questiongroups = questiongroups;
+    }
+}
