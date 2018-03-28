04ceba4822faff691bb5e9cc03e432ae4dbc82f7
Iván Perdomo <ivan@perdomo.me>
2018-03-28 15:05:44 +0200

    [#2562] Initial changes to deploy to UAT1 when merging to develop

---
+++ ci/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function log {
+   echo "$(date +"%T") - INFO - $*"
+}
+
+#if [[ "${TRAVIS_BRANCH}" != "develop" ]]; then
+#    exit 0
+#fi
+
+#if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+#    exit 0
+#fi
+
+log Authentication with gcloud
+docker run -ti google/cloud-sdk:latest gcloud version
+
+#openssl aes-256-cbc -K $encrypted_13abf95e958f_key -iv $encrypted_13abf95e958f_iv \
+#	-in ci/gcloud-service-account.json.enc -out ci/gcloud-service-account.json -d
+
+#gcloud auth activate-service-account --key-file ci/gcloud-service-account.json
+#gcloud config set project akvoflow-uat1
+#gcloud config set container/cluster europe-west1-d
+#gcloud config set compute/zone europe-west1-d
