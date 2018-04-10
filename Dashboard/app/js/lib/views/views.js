575e7e43b9afb1fa1c352cdb7ee236fd532583e6
kymni <charles@akvo.org>
2018-04-10 16:16:56 +0300

    [#2578] improve custom implementation of Ember.RadioButton

--- Dashboard/app/js/lib/views/views.js
+++ Dashboard/app/js/lib/views/views.js
@@ -905,29 +905,15 @@ FLOW.ColumnView = Ember.View.extend({
 var set = Ember.set,
   get = Ember.get;
 Ember.RadioButton = Ember.View.extend({
-  title: null,
-  checked: false,
-  group: "radio_button",
-  disabled: false,
-
-  classNames: ['ember-radio-button'],
-
-  defaultTemplate: Ember.Handlebars.compile('<label><input type="radio" {{ bindAttr disabled="view.disabled" name="view.group" value="view.option" checked="view.checked"}} />{{view.title}}</label>'),
-
-  bindingChanged: function () {
-    if (this.get("option") == get(this, 'value')) {
-      this.set("checked", true);
-    }
-  }.observes("value"),
-
-  change: function () {
-    Ember.run.once(this, this._updateElementValue);
-  },
-
-  _updateElementValue: function () {
-    var input = this.$('input:radio');
-    set(this, 'value', input.attr('value'));
-  }
+    tagName : 'input',
+    type : 'radio',
+    attributeBindings : ['name', 'type', 'value', 'checked:checked:'],
+    click : function() {
+        this.set('selection', this.$().val());
+    },
+    checked : function() {
+        return this.get('value') === this.get('selection');
+    }.property()
 });
 
 FLOW.SelectFolder = Ember.Select.extend({
