874c5711fa9ac2a1e2b58d0762f0ba8235c80be0
Maseline <mmusimire@gmail.com>
2018-04-12 13:08:48 +0300

    [#2363]Removed white space

--- Dashboard/app/js/lib/views/reports/export-reports-views.js
+++ Dashboard/app/js/lib/views/reports/export-reports-views.js
@@ -149,13 +149,13 @@ FLOW.ExportReportsAppletView = FLOW.View.extend({
   reportToDate: undefined,
   
   setMinDate: function () {
-    if (this.get('reportFromDate') !== "") {
+    if (this.get('reportFromDate')) {
       this.$(".to_date").datepicker("option", "minDate", this.get("reportFromDate"))
     }
   }.observes('this.reportFromDate'),
 
   setMaxDate: function () {
-    if (this.get('reportToDate') !== "") { 
+    if (this.get('reportToDate')) {
      this.$(".from_date").datepicker("option", "maxDate", this.get("reportToDate"))
     }
   }.observes('this.reportToDate'),
