20eea338bddf04d474ca8ef0a16b3dd4927da251
Iván Perdomo <ivan@perdomo.me>
2018-04-17 22:29:26 +0200

    [#2562] Clone repo as part of deployment, verify operation
    * To avoid developer local changes we clone the config repository
      as part of the deployment
    * To avoid issues with ssh keys we clone the repo using https, but
      requires the developer with 2FA enabled, to have a personal token
      to be used as password
    * After POST to a known instance (akvoflow-dev2) we attempt to verify
      that the operation was done

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+yellow='\033[1;33m'
+green='\033[0;32m'
+nc='\033[0m'
 version="${1}"
 config_repo="${CONFIG_REPO:=/akvo-flow-server-config}"
 deploy_bucket_name="${deploy_bucket_name:=akvoflowsandbox-deployment}"
@@ -18,7 +21,7 @@ echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/source
      unzip=6.0-16+deb8u3
 
 # Force login
-gcloud auth login --activate --force
+gcloud auth login --brief --activate --force
 
 akvo_org_account=$( (gcloud auth list --filter="active" 2>&1 | grep -E '^\*.*akvo\.org$') || echo "")
 
@@ -27,15 +30,16 @@ if [[ -z "${akvo_org_account}" ]]; then
     exit 1
 fi
 
-echo "Updating akvo-flow-server-config to latest changes..."
-(
-    cd "${config_repo}"
-    git checkout -- .
-    git checkout master
-)
+echo "Cloning akvo-flow-server-config..."
+echo -e "${yellow}WARNING:${nc} If you have 2FA enabled in GitHub, make sure you have a
+         ${green}personal access token${nc} available and use it as password"
+echo "Visit for more info:
+https://blog.github.com/2013-09-03-two-factor-authentication/#how-does-it-work-for-command-line-git"
 
-echo "Deploying to akvoflowsandbox..."
+git clone --depth=50 --branch=master \
+    https://github.com/akvo/akvo-flow-server-config.git "${config_repo}"
 
+echo "Deploying to akvoflowsandbox using gcloud..."
 mkdir -p "${tmp}"
 gsutil cp "gs://${deploy_bucket_name}/${version}.war" "${tmp}"
 unzip "${tmp}/${version}.war" -d "${target_dir}"
@@ -59,11 +63,11 @@ echo "Retrieving version definitions..."
 
 access_token=$(gcloud auth print-access-token)
 
-curl -H "Authorization: Bearer ${access_token}" \
+curl -s -H "Authorization: Bearer ${access_token}" \
      "${api_root}/apps/akvoflowsandbox/services/default/versions/1?view=FULL" \
      > "${tmp}/1.json"
 
-curl -H "Authorization: Bearer ${access_token}" \
+curl -s -H "Authorization: Bearer ${access_token}" \
      "${api_root}/apps/akvoflowsandbox/services/default/versions/dataprocessor?view=FULL" \
      > "${tmp}/dataprocessor.json"
 
@@ -72,7 +76,7 @@ find "${config_repo}" -name 'appengine-web.xml' -exec sha1sum {} + > "${tmp}/sha
 (
     cd "${tmp}"
 
-    instance_id="akvoflow-uat2"
+    instance_id="akvoflow-dev2"
     instance_file="${instance_id}.json"
 
     # select required keys
@@ -93,7 +97,36 @@ find "${config_repo}" -name 'appengine-web.xml' -exec sha1sum {} + > "${tmp}/sha
     sed -i -e "s|${sandbox_sha1_sum}|${instance_sha1_sum}|g" "${instance_file}"
     gsutil cp -J "${config_repo}/${instance_id}/appengine-web.xml" "gs://${deploy_bucket_name}/${instance_sha1_sum}"
 
-    curl -X POST -T "${instance_file}" -H "Content-Type: application/json" \
+    echo "Deploying akvoflow-dev2 using GAE Admin API..."
+    curl -s -X POST -T "${instance_file}" -H "Content-Type: application/json" \
 	 -H "Authorization: Bearer ${access_token}" \
-	 "${api_root}/apps/${instance_id}/services/default/versions"
+	 "${api_root}/apps/${instance_id}/services/default/versions" > \
+         "${instance_id}_operation.json"
+
+    instance_operation_path=$(jq -r .name "${instance_id}_operation.json")
+    for i in {1..20}
+    do
+      sleep 5
+      echo "Checking deployment status - Attempt ${i}"
+      done=$( (curl -s \
+                    -H "Content-Type: application/json" \
+                    -H "Authorization: Bearer ${access_token}" \
+                    "${api_root}/${instance_operation_path}" | jq .done) || "")
+      if [[ "${done}" == "true" ]]; then
+        break
+      fi
+    done
+
+    echo "Deployment to akvoflow-dev2 done"
+    echo "Basic check for akvoflow-dev2"
+
+    available=$(curl -s -o /dev/null -w "%{http_code}" http://akvoflow-dev2.appspot.com/devicetimerest || "")
+
+    if [[ "${available}" != "200" ]]; then
+	echo >&2 "http://akvoflow-dev2.appspot.com/devicetimerest is not available"
+	echo >&2 "Aborting"
+	exit 1
+    fi
+
+    echo "Done"
 )
