cd9c6070f4f05ca1efc5eb8209701400dc2fcd4f
Iván Perdomo <ivan@perdomo.me>
2018-05-08 21:53:57 +0200

    [#2628] Use mvn appengine:deploy for deployment
    - Temporary disable check on branch `develop`

---
+++ ci/mvn-deploy.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function log {
+   echo "$(date +"%T") - INFO - $*"
+}
+
+gcloud auth activate-service-account "${SERVICE_ACCOUNT_ID}" --key-file=/app/src/ci/akvoflow-uat1.p12
+gcloud config set project "${PROJECT_ID}"
+gcloud config set compute/zone europe-west1-d
+
+log Requesting "${PROJECT_ID}" config
+
+curl -s -o GAE/target/akvo-flow/WEB-INF/appengine-web.xml \
+     "https://$GH_USER:$GH_TOKEN@raw.githubusercontent.com/akvo/$CONFIG_REPO/master/${PROJECT_ID}/appengine-web.xml"
+
+log Update __VERSION__
+
+version=$(git describe)
+
+sed -i "s/__VERSION__/${version}/" GAE/target/akvo-flow/admin/js/app.js
+
+cd /app/src/GAE
+
+log Updating version 1
+
+mvn appengine:deploy -Dapp.deploy.project="${PROJECT_ID}" -Dapp.deploy.version=1
+
+log Version 1 updated
+
+#gsutil cp GAE/target/akvo-flow.war "gs://akvoflowsandbox-deployment/${version}.war"
+
+#log Updating default service version 1
+
+#gcloud app deploy GAE/target/akvo-flow/WEB-INF/appengine-web.xml --promote --version=1
+
+#log Updating default service version dataprocessor
+
+#gcloud app deploy GAE/target/akvo-flow/WEB-INF/appengine-web.xml --no-promote --version=dataprocessor
