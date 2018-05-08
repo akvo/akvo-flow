03c44508c9fda0a59eedf7eeb5293df5822ec416
Iván Perdomo <ivan@perdomo.me>
2018-05-08 22:00:41 +0200

    [#2628] Fix path when saving appengine-web.xml

--- ci/mvn-deploy.sh
+++ ci/mvn-deploy.sh
@@ -14,14 +14,14 @@ gcloud config set compute/zone europe-west1-d
 
 log Requesting "${PROJECT_ID}" config
 
-curl -s -o GAE/target/akvo-flow/WEB-INF/appengine-web.xml \
+curl -s -o ./target/akvo-flow/WEB-INF/appengine-web.xml \
      "https://$GH_USER:$GH_TOKEN@raw.githubusercontent.com/akvo/$CONFIG_REPO/master/${PROJECT_ID}/appengine-web.xml"
 
 log Update __VERSION__
 
 version=$(git describe)
 
-sed -i "s/__VERSION__/${version}/" target/akvo-flow/admin/js/app.js
+sed -i "s/__VERSION__/${version}/" ./target/akvo-flow/admin/js/app.js
 
 log Updating version 1
 
