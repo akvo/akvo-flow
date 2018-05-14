6336b1f780e82a640fbb73a59d41e845dfde28b9
Iván Perdomo <ivan@perdomo.me>
2018-05-14 12:25:14 +0200

    [#2628] Deploy index|cron|queue as part of dev-deploy

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -10,3 +10,7 @@ instance_id=$(sed -n 's/\(.*\)<application>\(.*\)<\/application>\(.*\)/\2/p' tar
 
 mvn appengine:deploy -Dapp.deploy.project="${instance_id}" -Dapp.deploy.version=1
 mvn appengine:deploy -Dapp.deploy.project="${instance_id}" -Dapp.deploy.version=dataprocessor -Dapp.deploy.promote=false
+
+gcloud app deploy target/appengine-staging/WEB-INF/appengine-generated/cron.yaml --project="${instance_id}" --quiet
+gcloud app deploy target/appengine-staging/WEB-INF/appengine-generated/index.yaml --project="${instance_id}" --quiet
+gcloud app deploy target/appengine-staging/WEB-INF/appengine-generated/queue.yaml --project="${instance_id}" --quiet
