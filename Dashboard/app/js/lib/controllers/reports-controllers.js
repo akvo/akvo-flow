7084b2428de38c8dc19ff0d51d443c4753a9f5ef
kymni <charles@akvo.org>
2018-07-18 14:58:57 +0300

    [#2755] check if reports have finished generating

--- Dashboard/app/js/lib/controllers/reports-controllers.js
+++ Dashboard/app/js/lib/controllers/reports-controllers.js
@@ -37,17 +37,43 @@ FLOW.ReportsController = Ember.ArrayController.extend({
   sortAscending: false,
   content: null,
   reportsAvailable: false,
+  reportsCheckScheduled: false,
 
   populate: function () {
     this.set('content', FLOW.store.find(FLOW.Report));
   },
 
   reportsObserver: function () {
-    var reports = this.get('content');
+    var reports = this.get('content'), reportsQuery = null, self = this;
     if (reports && !reports.isUpdating) {
       this.set('reportsAvailable', reports.content.length > 0);
 
-      //TODO refresh reports list after checing if processing has completed
+      if (!this.get('reportsCheckScheduled')) {
+        var generatingReports = reports.filter(function(report) {
+          return report.get('state') === "IN_PROGRESS" || report.get('state') === "QUEUED";
+        });
+        if (generatingReports.length > 0) {
+          this.set('reportsCheckScheduled', true);
+          reportsQuery = setInterval(function(){//if reports are still generating, wait 5s and then recheck
+            var stillGeneratingReports = self.get('content').filter(function(report){
+              return report.get('state') === "IN_PROGRESS" || report.get('state') === "QUEUED";
+            });
+
+            if (stillGeneratingReports.length > 0) {
+              stillGeneratingReports.forEach(function(report){
+                FLOW.store.findQuery(FLOW.Report, {keyId: report.get('keyId')});
+              });
+            } else {
+              self.set('reportsCheckScheduled', false);
+              clearInterval(reportsQuery);
+            }
+          }, 10000);
+        } else {
+          if (reportsQuery) {
+            clearInterval(reportsQuery);
+          }
+        }
+      }
     }
   }.observes('content', 'content.isUpdating')
 });
