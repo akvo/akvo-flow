b693d0c52ffaa3c348b02f18fc5f1b7371edbfeb
kymni <charles@akvo.org>
2018-08-23 12:51:09 +0300

    [#2821] create surveyGroupId variable and check for at least one ancestor

--- Dashboard/app/js/lib/models/models.js
+++ Dashboard/app/js/lib/models/models.js
@@ -129,9 +129,9 @@ FLOW.SurveyGroup = FLOW.BaseModel.extend({
 FLOW.Survey = FLOW.BaseModel.extend({
   didLoad: function () {
     // set the survey group name
-    var ancestorIds = this.get('ancestorIds');
-    if (this.get('surveyGroupId') && ancestorIds.length > 1) {
-      var sg = FLOW.store.find(FLOW.SurveyGroup, this.get('surveyGroupId'));
+    var ancestorIds = this.get('ancestorIds'), sgId = this.get('surveyGroupId');
+    if (sgId && ancestorIds.length > 0) {
+      var sg = FLOW.store.find(FLOW.SurveyGroup, sgId);
       if (!Ember.empty(sg)) {
         this.set('surveyGroupName', sg.get('code'));
       }
