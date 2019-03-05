3ad33e139d1017df6946badaebdd7197c4d8571a
stellanl <stellanl@akvo.org>
2019-03-05 21:43:10 +0100

    [#2944]Ingest files on single-instance queue. Replace tabs w spaces in XML.

--- GAE/src/org/waterforpeople/mapping/app/web/ProcessorServlet.java
+++ GAE/src/org/waterforpeople/mapping/app/web/ProcessorServlet.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010,2012-2016,2018 Stichting Akvo (Akvo Foundation)
+ *  Copyright (C) 2010,2012-2016,2018-2019 Stichting Akvo (Akvo Foundation)
  *
  *  This file is part of Akvo FLOW.
  *
@@ -40,35 +40,42 @@ import java.util.List;
 import java.util.logging.Logger;
 
 /**
- * Servlet used by app to trigger processing of new survey data TODO: move parameter name strings
- * into constants
+ * Servlet used by app to trigger processing of new survey data
  */
 public class ProcessorServlet extends HttpServlet {
 
     private static final long serialVersionUID = -7062679258542909086L;
-    private static final Logger log = Logger.getLogger(ProcessorServlet.class
-            .getName());
+    private static final Logger log = Logger.getLogger(ProcessorServlet.class.getName());
+    
+    private static final String ACTION_PARAM = "action";
+    private static final String SUBMIT_ACTION = "submit";
+    private static final String IMAGE_ACTION = "image";
+    private static final String CASCADE_ACTION = "cascade";
+    private static final String CHECKSUM_PARAM = "checksum";
+    private static final String IMEI_PARAM = "imei";
+    private static final String ANDROID_ID_PARAM = "androidId";
+    private static final String PHONE_NUMBER_PARAM = "phoneNumber";
+    private static final String FILE_NAME_PARAM = "fileName";
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        if (req.getParameter("action") == null) {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        
+        String action = StringUtils.trim(req.getParameter(ACTION_PARAM));
+        if (action == null) {
             log.info("No action specified for processor");
             return;
         }
+        log.info("  ProcessorServlet->action->" + action);
 
-        String action = StringUtils.trim(req.getParameter("action"));
-        String fileName = StringUtils.trim(req.getParameter("fileName"));
-        String phoneNumber = StringUtils.trim(req.getParameter("phoneNumber"));
-        String androidId = StringUtils.trim(req.getParameter("androidId"));
-        String imei = StringUtils.trim(req.getParameter("imei"));
-        String checksum = StringUtils.trim(req.getParameter("checksum"));
+        String fileName = StringUtils.trim(req.getParameter(FILE_NAME_PARAM));
+        String phoneNumber = StringUtils.trim(req.getParameter(PHONE_NUMBER_PARAM));
+        String androidId = StringUtils.trim(req.getParameter(ANDROID_ID_PARAM));
+        String imei = StringUtils.trim(req.getParameter(IMEI_PARAM));
+        String checksum = StringUtils.trim(req.getParameter(CHECKSUM_PARAM));
 
-        log.info("	ProcessorServlet->action->" + action);
-
-        if (action.equals("submit")) {
+        if (SUBMIT_ACTION.equals(action)) {
             if (fileName == null) {
-                log.info("Request for processing without filename: phoneNumber=(" + phoneNumber
+                log.warning("Request for processing without filename: phoneNumber=(" + phoneNumber
                         + "); IMEI=(" + imei + ")");
                 resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 return;
@@ -86,9 +93,9 @@ public class ProcessorServlet extends HttpServlet {
             }
 
             log.info("  ProcessorServlet->filename->" + fileName);
-            log.info("about to submit task for fileName: " + fileName);
             // Submit the fileName for processing
-            Queue queue = QueueFactory.getDefaultQueue();
+            // on a single-instance queue to prevent race conditions
+            Queue queue = QueueFactory.getQueue("processDeviceFile");
             TaskOptions options = TaskOptions.Builder.withUrl("/app_worker/task")
                     .param(TaskRequest.ACTION_PARAM, TaskRequest.PROCESS_FILE_ACTION)
                     .param(TaskRequest.FILE_NAME_PARAM, fileName);
@@ -96,15 +103,12 @@ public class ProcessorServlet extends HttpServlet {
             if (androidId != null) {
                 options.param(TaskRequest.ANDROID_ID, androidId);
             }
-
             if (phoneNumber != null) {
                 options.param(TaskRequest.PHONE_NUM_PARAM, phoneNumber);
             }
-
             if (imei != null) {
                 options.param(TaskRequest.IMEI_PARAM, imei);
             }
-
             if (checksum != null) {
                 options.param(TaskRequest.CHECKSUM_PARAM, checksum);
             }
@@ -113,7 +117,7 @@ public class ProcessorServlet extends HttpServlet {
 
             log.info("submiting task for fileName: " + fileName);
 
-        } else if (action.equals("image")) {
+        } else if (IMAGE_ACTION.equals(action)) {
             Device d = null;
             DeviceDAO dao = new DeviceDAO();
 
@@ -126,13 +130,13 @@ public class ProcessorServlet extends HttpServlet {
             }
 
             DeviceFileJobQueueDAO dfDao = new DeviceFileJobQueueDAO();
-            List<DeviceFileJobQueue> missing = dfDao.listByDeviceAndFile(d
-                    .getKey().getId(), fileName);
+            List<DeviceFileJobQueue> missing = dfDao.listByDeviceAndFile(
+                    d.getKey().getId(), fileName);
             log.info(String.format(
                     "Deleting %s entities matching the fileName %s",
                     missing.size(), fileName));
             dfDao.delete(missing);
-        } else if (action.equals("cascade")) {
+        } else if (CASCADE_ACTION.equals(action)) {
             Long crId = null;
             final String status = StringUtils.trim(req.getParameter("status"));
             final String message = StringUtils.trim(req.getParameter("message"));
