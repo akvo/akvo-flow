a44ec30c660bb77b2a72cf85734d7d6dcb7e58d4
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2019-03-11 17:06:57 +0200

    #1502 webpack prod config

---
+++ Dashboard/scripts/buildDev.js
@@ -0,0 +1,40 @@
+// More info on Webpack's Node API here: https://webpack.js.org/api/node/
+// Allowing console calls below since this is a build file.
+/* eslint-disable no-console */
+import webpack from 'webpack';
+import config from '../webpack.config.prod';
+import { chalkError, chalkSuccess, chalkWarning, chalkProcessing } from './chalkConfig';
+
+process.env.NODE_ENV = 'development'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
+console.log(chalkProcessing('Generating bundle. This will take a moment...'));
+
+const compiler = webpack(config);
+
+compiler.watch({
+  // Example watchOptions
+  aggregateTimeout: 300,
+  poll: undefined,
+}, (error, stats) => {
+  if (error) { // so a fatal error occurred. Stop here.
+    console.log(chalkError(error));
+    return 1;
+  }
+
+  const jsonStats = stats.toJson();
+
+  if (jsonStats.hasErrors) {
+    return jsonStats.errors.map(error2 => console.log(chalkError(error2)));
+  }
+
+  if (jsonStats.hasWarnings) {
+    console.log(chalkWarning('Webpack generated the following warnings: '));
+    jsonStats.warnings.map(warning => console.log(chalkWarning(warning)));
+  }
+
+  console.log(`Webpack stats: ${stats}`);
+
+  // if we got this far, the build succeeded.
+  console.log(chalkSuccess('Your app is compiled and ready to roll!'));
+
+  return 0;
+});
