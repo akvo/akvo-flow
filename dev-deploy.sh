4622a30c1f0463bd14bc5a3132b263f957fe6feb
Emmanuel Mulo <emmanuel@akvo.org>
2019-03-18 03:33:01 +0100

    [#2694] Remove backends deployment code

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -14,7 +14,3 @@ fi
 java -cp /google-cloud-sdk/platform/google_appengine/google/appengine/tools/java/lib/appengine-tools-api.jar \
      com.google.appengine.tools.admin.AppCfg \
      update ./target/appengine-staging
-
-java -cp /google-cloud-sdk/platform/google_appengine/google/appengine/tools/java/lib/appengine-tools-api.jar \
-     com.google.appengine.tools.admin.AppCfg \
-     backends update ./target/appengine-staging
