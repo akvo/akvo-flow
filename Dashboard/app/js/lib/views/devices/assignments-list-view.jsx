341899e5c61cb99a1801f1085dfeb14922a59f89
kymni <charles@akvo.org>
2019-04-04 14:18:06 +0300

    [#2796] switching views test

--- Dashboard/app/js/lib/views/devices/assignments-list-view.jsx
+++ Dashboard/app/js/lib/views/devices/assignments-list-view.jsx
@@ -9,21 +9,38 @@ FLOW.AssignmentsListView = FLOW.ReactComponentView.extend({
     //tmp solution because observe not working
     let render = setInterval(() => {
       if (FLOW.surveyAssignmentControl.content.isLoaded) {
-        self.renderView();
+        self.assignmentsList();
         clearInterval(render);
       }
     }, 500);
   },
 
-  renderView () {
-    let assignments = FLOW.surveyAssignmentControl.get('content');
+  assignmentsList () {
+    const assignments = FLOW.surveyAssignmentControl.get('content');
     this.reactRender(
+      <div><div className="deviceControls">
+        <a className="btnOutline" onClick={() => this.editAssignment("new")}>Create new assignment</a>
+      </div>
+      <div id="devicesListTable_length" className="dataTables_length"></div>
       <table className="dataTable"><tbody>{assignments.map(assignment => (
         <tr key={assignment.get('keyId')}>
           <td className="name">{assignment.get('name')}</td>
+          <td className="action"><a style={{cursor: 'pointer'}} onClick={() => this.editAssignment("edit", assignment)}>Edit</a></td>
         </tr>
-      ))}</tbody></table> //move styling to css
+      ))}</tbody></table></div> //move styling to css
       //<div style={{ visibility: 'hidden' }}>React {moment().seconds()}</div>
     );
+  },
+
+  editAssignment (action, assignment) {
+    var self = this;
+    this.reactRender(
+      <div><div className="deviceControls">
+        <a className="btnOutline" onClick={() => self.assignmentsList()}>back</a>
+      </div>
+      <div id="devicesListTable_length" className="dataTables_length"></div>
+      <div>{action == "edit" ? assignment.get('name') : "new ass"}</div></div>
+    );
+    //alert(action+" "+assignment.get("name"));
   }
 });
