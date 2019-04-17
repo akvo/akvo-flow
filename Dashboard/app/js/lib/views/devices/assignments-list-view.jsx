e478a768c9288772769c1a33301b7953a73d6f74
kymni <charles@akvo.org>
2019-04-17 15:36:07 +0300

    [#2796] navigation between views

--- Dashboard/app/js/lib/views/devices/assignments-list-view.jsx
+++ Dashboard/app/js/lib/views/devices/assignments-list-view.jsx
@@ -20,15 +20,19 @@ FLOW.AssignmentsListView = FLOW.ReactComponentView.extend({
     const assignments = FLOW.surveyAssignmentControl.get('content');
     this.reactRender(
       <div><div className="deviceControls">
-        <a className="btnOutline" onClick={() => this.editAssignment("new")}>Create new assignment</a>
+        <a className="btnOutline" onClick={() => this.editAssignment("new")} style={{float: 'right'}}>Create new assignment</a>
       </div>
       <div id="devicesListTable_length" className="dataTables_length"></div>
-      <table className="dataTable"><tbody>{assignments.map(assignment => (
-        <tr key={assignment.get('keyId')}>
-          <td className="name">{assignment.get('name')}</td>
-          <td className="action"><a style={{cursor: 'pointer'}} onClick={() => this.editAssignment("edit", assignment)}>Edit</a></td>
-        </tr>
-      ))}</tbody></table></div> //move styling to css
+      {!assignments.get('length') && (<div style={{marginTop: '40px'}}>No assignments</div>)}
+      {assignments.get('length') > 0 && (
+        <table className="dataTable"><tbody>{assignments.map(assignment => (
+          <tr key={assignment.get('keyId')}>
+            <td className="name">{assignment.get('name')}</td>
+            <td className="action"><a style={{cursor: 'pointer'}} onClick={() => this.editAssignment("edit", assignment)}>Edit</a></td>
+          </tr>
+        ))}</tbody></table>
+      )}
+      </div> //move styling to css
       //<div style={{ visibility: 'hidden' }}>React {moment().seconds()}</div>
     );
   },
@@ -36,11 +40,21 @@ FLOW.AssignmentsListView = FLOW.ReactComponentView.extend({
   editAssignment (action, assignment) {
     var self = this;
     this.reactRender(
-      <div><div className="deviceControls">
-        <a className="btnOutline" onClick={() => self.assignmentsList()}>back</a>
+      <div>
+        <div className="deviceControls">
+          <a className="btnOutline" onClick={() => self.assignmentsList()} style={{float: 'left'}}>back</a>{/* TODO: button should reload assignments*/}
+          <label style={{float: 'left'}}>{action == "edit" ? assignment.get('name') : "new ass"}</label>
+          <a className="btnOutline" style={{float: 'right'}}>save</a>
+        </div>
+        <div id="devicesListTable_length" className="dataTables_length"></div>
+        <div style={{marginTop: '40px'}}>
+          <div>duration</div>
+          <div>{/*should cascade*/}
+            folder/survey selectors
+          </div>
+          <div>forms list</div>
+        </div>
       </div>
-      <div id="devicesListTable_length" className="dataTables_length"></div>
-      <div>{action == "edit" ? assignment.get('name') : "new ass"}</div></div>
     );
     //alert(action+" "+assignment.get("name"));
   }
