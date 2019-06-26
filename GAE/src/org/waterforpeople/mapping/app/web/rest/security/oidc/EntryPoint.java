103b1c0051d9503d04ec7c2470daebcb1abdb57d
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-06-26 16:17:26 +0200

    [#3125] Auth0 EntryPoint

---
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/security/oidc/EntryPoint.java
@@ -0,0 +1,43 @@
+package org.waterforpeople.mapping.app.web.rest.security.oidc;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.auth0.AuthenticationController;
+
+public class EntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = Logger.getLogger(EntryPoint.class.getName());
+
+    private final AppConfig appConfig;
+    private final AuthenticationController controller;
+
+    public EntryPoint(AppConfig appConfig) {
+        this.appConfig = appConfig;
+        this.controller = this.appConfig.authenticationController();
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        logger.fine("Performing login");
+        String redirectUri = request.getScheme() + "://" + request.getServerName();
+        if ((request.getScheme().equals("http") && request.getServerPort() != 80) || (request.getScheme().equals("https") && request.getServerPort() != 443)) {
+            redirectUri += ":" + request.getServerPort();
+        }
+        redirectUri += "/callback";
+        String authorizeUrl = controller.buildAuthorizeUrl(request, redirectUri)
+                .withAudience(String.format("https://%s/userinfo", appConfig.getDomain()))
+                .withScope("openid profile email")
+                .withParameter("prompt", "select_account")
+                .build();
+        response.sendRedirect(response.encodeRedirectURL(authorizeUrl));
+
+    }
+}
