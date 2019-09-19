e008af2041806cc76467ac2837502a3b8c49a284
Juan A. Ruz <juanantonioruz@gmail.com>
2019-09-19 11:45:29 +0200

    [#3190] Add i18n support

--- GAE/src/org/akvo/flow/rest/security/oidc/EntryPoint.java
+++ GAE/src/org/akvo/flow/rest/security/oidc/EntryPoint.java
@@ -18,6 +18,9 @@ package org.akvo.flow.rest.security.oidc;
 
 import java.io.IOException;
 import java.util.logging.Logger;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,25 +37,40 @@ public class EntryPoint implements AuthenticationEntryPoint {
     private final AppConfig appConfig;
     private final AuthenticationController controller;
 
+    final Set<String> enabledLocales = new HashSet<String>(
+            Arrays.asList("en", "es", "fr", "pt", "id", "vi"));
+
     public EntryPoint(AppConfig appConfig) {
         this.appConfig = appConfig;
         this.controller = this.appConfig.authenticationController();
     }
+    private boolean isAllowedLocale(String locale) {
+        return enabledLocales.contains(locale);
+    }
+
+    private String getLocale(String reqPararameter){
+	if(isAllowedLocale(reqPararameter)){
+	    return reqPararameter;
+	} else {
+	    return "en";
+	}
+    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-
-        logger.fine("Performing login");
+	String locale = getLocale(request.getParameter("locale"));
+        logger.fine("Performing login. Locale: "+ locale);
         String redirectUri = request.getScheme() + "://" + request.getServerName();
         if ((request.getScheme().equals("http") && request.getServerPort() != 80) || (request.getScheme().equals("https") && request.getServerPort() != 443)) {
             redirectUri += ":" + request.getServerPort();
         }
         redirectUri += "/callback";
         String authorizeUrl = controller.buildAuthorizeUrl(request, redirectUri)
-                .withAudience(String.format("https://%s/userinfo", appConfig.getDomain()))
-                .withScope("openid profile email")
-                .withParameter("prompt", "select_account")
-                .build();
+	    .withAudience(String.format("https://%s/userinfo", appConfig.getDomain()))
+	    .withScope("openid profile email")
+	    .withParameter("prompt", "select_account")
+	    .withParameter("ui_locales", locale)
+	    .build();
         response.sendRedirect(response.encodeRedirectURL(authorizeUrl));
 
     }
