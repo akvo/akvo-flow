36a727ee65cb9df007e90e05bf622442e82fb487
Marvin Kome <marvin@akvo.org>
2019-11-25 15:06:16 +0100

    [#3297]  -  remove unused comments and files

--- Dashboard/app/js/lib/components/devices/AssignmentsEditView/DevicesSection.jsx
+++ Dashboard/app/js/lib/components/devices/AssignmentsEditView/DevicesSection.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-// import DeviceGroupSelectorView from 'akvo-flow/components/selectors/DeviceSelector';
 
 export default class DevicesSection extends React.Component {
   render() {
@@ -10,25 +9,6 @@ export default class DevicesSection extends React.Component {
           <i className="fa fa-mobile" />
           <p>No devices added to assignment yet</p>
         </div>
-
-        {/* <div className="heading">
-          <h3>{strings.devices}</h3>
-          <span className="info">
-            {data.numberOfDevices}{' '}
-            {data.numberOfDevices === 1 ? strings.device : strings.devices}{' '}
-            {strings.selected}
-          </span>
-        </div> */}
-
-        {/* <div className="assignment-device-selector">
-          <DeviceGroupSelectorView
-            deviceGroupNames={data.deviceGroupNames}
-            deviceGroups={data.deviceGroups}
-            activeDeviceGroups={data.activeDeviceGroups}
-            handleDeviceCheck={actions.handleDeviceCheck}
-            onSelectAll={actions.handleSelectAllDevice}
-          />
-        </div> */}
       </div>
     );
   }
