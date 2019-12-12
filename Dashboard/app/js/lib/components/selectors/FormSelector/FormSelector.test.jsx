4eeead77000c880867d5f940ed6b1584c1a9039a
Marvin Kome <marvin@akvo.org>
2019-12-12 13:54:59 +0100

    [#3341] - update form selector snapshots

--- Dashboard/app/js/lib/components/selectors/FormSelector/FormSelector.test.jsx
+++ Dashboard/app/js/lib/components/selectors/FormSelector/FormSelector.test.jsx
@@ -18,16 +18,17 @@ afterAll(() => {
   console.error = originalError;
 });
 
-
 // Main Tests
 describe('FormSelector Tests', () => {
   afterEach(cleanup);
 
   it('+++ renders <snapshot> correctly', () => {
-    const wrapper = render(<FormSelector
-      forms={{ 145492013: { name: 'Handpump', checked: false } }}
-      onCheck={jest.fn()}
-    />);
+    const wrapper = render(
+      <FormSelector
+        forms={{ 145492013: { name: 'Handpump', checked: false } }}
+        onCheck={jest.fn()}
+      />
+    );
     expect(wrapper.container).toMatchSnapshot();
   });
 });
