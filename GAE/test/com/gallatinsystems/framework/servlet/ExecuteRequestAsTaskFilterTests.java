9f7318a50967e92d0c50b86394cd83ce666e3662
Emmanuel Mulo <emmanuel@akvo.org>
2020-03-12 13:07:15 +0100

    [#3451] Remove unused dependencies

--- GAE/test/com/gallatinsystems/framework/servlet/ExecuteRequestAsTaskFilterTests.java
+++ GAE/test/com/gallatinsystems/framework/servlet/ExecuteRequestAsTaskFilterTests.java
@@ -38,9 +38,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class ExecuteRequestAsTaskFilterTests {
 
