cb0cc243decb6fe65c43ed54bbdbd689f59f57d7
Emmanuel Mulo <emmanuel@akvo.org>
2020-03-12 11:11:49 +0100

    [#3451] Add test to map request to task without stripping parameters

--- GAE/test/com/gallatinsystems/framework/servlet/ExecuteRequestAsTaskFilterTests.java
+++ GAE/test/com/gallatinsystems/framework/servlet/ExecuteRequestAsTaskFilterTests.java
@@ -34,7 +34,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import java.io.IOException;
-import java.util.SortedMap;
+import java.util.Map;
 
 import static com.gallatinsystems.framework.servlet.ExecuteRequestAsTaskFilter.REST_PRIVATE_KEY_PROP;
 import static org.junit.jupiter.api.Assertions.*;
@@ -58,41 +58,37 @@ class ExecuteRequestAsTaskFilterTests {
         httpRequest.setMethod("POST");
         httpRequest.setContentType("application/x-www-form-urlencoded");
         httpRequest.addParameter("action", "saveSurveyInstance");
-        httpRequest.addParameter("runAsTask", "1");
         httpRequest.addParameter("collectionDate", "30-03-2017+10%3A05%3A57+CEST");
         httpRequest.addParameter("duration", "41");
         httpRequest.addParameter("questionId", "149382277%7C0%3D44.3845%257C7.5845%257C1%7Ctype%3DGEO");
         httpRequest.addParameter("questionId", "151492308%7C0%3Dhttp%253A%252F%252Fwaterforpeople.s3.amazonaws.com%252Fimages%252Fdb0756e0-b2ec-49ed-bbdb-26d4478af5c3.jpg%7Ctype%3DIMAGE");
         httpRequest.addParameter("questionId", "152332013%7C0%3Dfiunsc%7Ctype%3DVALUE");
         httpRequest.addParameter("questionId", "149292013%7C0%3D3%7Ctype%3DVALUE");
+        httpRequest.addParameter("runAsTask", "1");
         httpRequest.addParameter("submitter", "tony");
         httpRequest.addParameter("surveyId", "15316201398");
         httpRequest.addParameter("surveyInstanceId", "15337202187");
         httpRequest.addParameter("ts", "2020/03/03 15:05:14");
+        httpRequest.addParameter("h", "CmuehPsWW6//5Q4i8O1P5AjS/8Y=");
 
         helper.setUp();
     }
 
     @Test
-    void testExecuteRequestTask() {
+    void testRequestToTaskMapping() {
         ExecuteRequestAsTaskFilter filter = new ExecuteRequestAsTaskFilter();
         ExecuteRequestAsTaskFilter.RequestToTaskMapper requestToTaskMapper = filter.new RequestToTaskMapper(this.httpRequest, testPrivateKey);
 
         assertTrue(filter.isTaskRequest(this.httpRequest), "The *original* request MUST have a runAsTask parameter whose value is == 1");
 
-        SortedMap<String, String[]> sortedTaskParams = requestToTaskMapper.getTaskRequestParams();
-        assertEquals(9, sortedTaskParams.size(), "Unexpected number of params for the request");
+        Map<String, String[]> taskRequestParams = requestToTaskMapper.getTaskRequestParams();
+        assertEquals(13, taskRequestParams.size(), "Unexpected number of params for the request");
         assertEquals("/testservletapi", requestToTaskMapper.getUrl());
-        assertEquals("action", sortedTaskParams.firstKey());
-        assertEquals("ts", sortedTaskParams.lastKey());
-        assertEquals("30-03-2017+10%3A05%3A57+CEST", sortedTaskParams.get("collectionDate")[0]);
-        assertEquals("41", sortedTaskParams.get("duration")[0]);
-        assertEquals("149382277%7C0%3D44.3845%257C7.5845%257C1%7Ctype%3DGEO", sortedTaskParams.get("questionId")[0]);
-        assertEquals("151492308%7C0%3Dhttp%253A%252F%252Fwaterforpeople.s3.amazonaws.com%252Fimages%252Fdb0756e0-b2ec-49ed-bbdb-26d4478af5c3.jpg%7Ctype%3DIMAGE", sortedTaskParams.get("questionId")[1]);
-        assertEquals("152332013%7C0%3Dfiunsc%7Ctype%3DVALUE", sortedTaskParams.get("questionId")[2]);
-        assertEquals("149292013%7C0%3D3%7Ctype%3DVALUE", sortedTaskParams.get("questionId")[3]);
-        assertEquals("2020/03/03 15:05:14", sortedTaskParams.get(RestRequest.TIMESTAMP_PARAM)[0]);
-        assertEquals("CmuehPsWW6//5Q4i8O1P5AjS/8Y=", sortedTaskParams.get(RestRequest.HASH_PARAM)[0]);
+        assertEquals("saveSurveyInstance", taskRequestParams.get("action")[0]);
+        assertEquals("2020/03/03 15:05:14", taskRequestParams.get("ts")[0]);
+        assertEquals("30-03-2017+10%3A05%3A57+CEST", taskRequestParams.get("collectionDate")[0]);
+        assertEquals("41", taskRequestParams.get("duration")[0]);
+        assertEquals("CmuehPsWW6//5Q4i8O1P5AjS/8Y=", taskRequestParams.get("h")[0]);
     }
 
     @Test
