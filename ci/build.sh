993a8d1792db1fb124a71f732b33c6cecef945aa
Iván Perdomo <ivan@perdomo.me>
2020-04-02 06:10:28 +0200

    [#3479] Move Clojars deploy to Promotions

--- ci/build.sh
+++ ci/build.sh
@@ -34,20 +34,3 @@ lein build
 cd "${SRC_DIR}/GAE"
 
 mvn package
-
-# Temporary disable publishing to clojars
-#if [[ "${CI_BRANCH}" != "master" ]] && [[ -z "$CI_TAG" ]]; then
-if [[ -z "$CI_TAG" ]]; then
-  exit 0
-fi
-
-echo "Setting project version to $FLOW_GIT_VERSION"
-mvn versions:set -DnewVersion="${FLOW_GIT_VERSION}"
-
-mvn deploy:deploy-file -s "${SRC_DIR}/maven-ci-settings.xml" \
-                       -Durl="https://clojars.org/repo" \
-                       -DrepositoryId=clojars \
-                       -Dfile=target/akvo-flow-classes.jar \
-                       -DpomFile=pom.xml \
-                       -Dpackaging=jar \
-                       -Dclassifier=classes
