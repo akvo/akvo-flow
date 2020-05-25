fcef8309cc2fa7beb662356ae9fc86bcc8124adf
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-25 12:48:21 +0200

    [#3571] Lastest version of flip/deploy scripts

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200525.102101.e972f9b \
+  -it akvo/akvo-devops:20200525.103249.2b55a8b \
   flow-promote-test-to-prod.sh
