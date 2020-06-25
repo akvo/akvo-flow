65160f7d07b301f2a92379a07b019aab8f4a92e8
Iván Perdomo <ivan@perdomo.me>
2020-06-25 08:29:17 +0200

    [#3571] Update to latest version of the scripts

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200525.103249.2b55a8b \
+  -it akvo/akvo-devops:20200625.062832.94a7a9c \
   flow-promote-test-to-prod.sh
