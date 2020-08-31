bde8424173ec8b79aa4f53a6c4ac113589916a61
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-08-31 13:18:27 +0200

    Fix Slack notifications in deployment scripts

--- ci/flip-production-traffic.sh
+++ ci/flip-production-traffic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200525.103249.2b55a8b \
+  -it akvo/akvo-devops:20200831.111310.9e071e8 \
   flow-flip-production-traffic.sh
--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200625.062832.94a7a9c \
+  -it akvo/akvo-devops:20200831.111310.9e071e8 \
   flow-promote-test-to-prod.sh
