99378e3c6258ff946d4b684a6a2d5b5db1372ffa
Emmanuel Mulo <emmanuel@akvo.org>
2020-10-07 06:18:22 +0200

    [#3654] Update copyright notice

--- GAE/src/org/waterforpeople/mapping/app/gwt/client/surveyinstance/SurveyInstanceDto.java
+++ GAE/src/org/waterforpeople/mapping/app/gwt/client/surveyinstance/SurveyInstanceDto.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010-2014, 2019 Stichting Akvo (Akvo Foundation)
+ *  Copyright (C) 2010-2014, 2019,2020 Stichting Akvo (Akvo Foundation)
  *
  *  This file is part of Akvo FLOW.
  *
