2c42c0267dec684eedca838edd1bbd13b455ba66
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-10-23 12:36:37 +0200

    [#3660] Update deploy script to send Zulip notifications

--- ci/flip-production-traffic.sh
+++ ci/flip-production-traffic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200831.111310.9e071e8 \
+docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
+  -it akvo/akvo-devops:20201023.101935.7dacd92 \
   flow-flip-production-traffic.sh
