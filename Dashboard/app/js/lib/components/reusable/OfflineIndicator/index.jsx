ea90934183c19c66223fa3c8589dcbe326df8af1
Marvin Kome <marvin@akvo.org>
2020-11-18 06:01:08 +0100

    [#3624] - ping gogle every 5 seconds for internet status.

--- Dashboard/app/js/lib/components/reusable/OfflineIndicator/index.jsx
+++ Dashboard/app/js/lib/components/reusable/OfflineIndicator/index.jsx
@@ -6,18 +6,24 @@ export default class OfflineIndicator extends React.Component {
   };
 
   componentDidMount() {
-    window.addEventListener('online', this.onlineStatus);
-    window.addEventListener('offline', this.onlineStatus);
+    this.timer = setInterval(() => {
+      fetch('https://www.google.com/generate_204', { mode: 'no-cors' })
+        .then(res => {
+          // with no-cors, res.status is 0 because the javascript ignores all response.
+          // we don't need the response, all we need to know is if the request went through or not
+          if (res.status !== 204) {
+            this.setState({ isOffline: false });
+          }
+        })
+        .catch(() => {
+          this.setState({ isOffline: true });
+        });
+    }, 5000);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('online', this.onlineStatus);
-    window.removeEventListener('offline', this.onlineStatus);
+    clearInterval(this.timer);
   }
-
-  onlineStatus = () => {
-    this.setState({ isOffline: !navigator.onLine });
-  };
 
   render() {
     return this.state.isOffline && <p>You are offline. Changes {"won't"} be saved</p>;
