31fbea4df07c6e8a2c25b6da1eb6091121653c4e
Iván Perdomo <ivan@perdomo.me>
2020-12-03 18:53:16 +0100

    [#3689] Fix dependency for questions created >= 2020-10-01

--- scripts/data/src/org/akvo/gae/remoteapi/FixCopyDependency.java
+++ scripts/data/src/org/akvo/gae/remoteapi/FixCopyDependency.java
@@ -21,119 +21,187 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 
-import java.sql.Date;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class FixCopyDependency implements Process {
 
+    private static final Map<Long, Entity> questionCache = new HashMap<>();
+    private static final Map<String, Entity> dependencyCache = new HashMap<>();
+
+
     @Override
     public void execute(DatastoreService ds, String[] args) throws Exception {
-        fixDependencyForSurvey(ds);
-        // System.out.println(surveysWithMissingDependencies(ds));
+        //brokenSurveysWithData(ds);
+        fixDependencyForSurveys(ds);
     }
 
-    private Set<Long> surveysWithMissingDependencies(DatastoreService ds) {
-        Set<Long> brokenSurveys = new HashSet<>();
+    private void brokenSurveysWithData(DatastoreService ds) {
+        for (Long surveyId : surveysWithMissingDependencies(ds)) {
+            if (getDataForSurvey(ds, surveyId).size() > 0) {
+                System.out.println("Survey id " + surveyId + " has data");
+            }
+        }
+    }
 
-        Instant t = Instant.parse("2020-11-01T00:00:00Z");
+    private List<Long> surveysWithMissingDependencies(DatastoreService ds) {
+        final Set<Long> brokenSurveys = new HashSet<>();
+        final Date t = Date.from(Instant.parse("2020-10-01T00:00:00Z"));
 
-        Query q = new Query("Question");
-        Filter f1 = new FilterPredicate("createdDateTime", FilterOperator.GREATER_THAN_OR_EQUAL, Date.from(t));
-        q.setFilter(f1);
+        final Query q = new Query("Question")
+                .setFilter(new FilterPredicate("createdDateTime", FilterOperator.GREATER_THAN_OR_EQUAL, t))
+                .addSort("createdDateTime", Query.SortDirection.ASCENDING);
 
-        PreparedQuery pq = ds.prepare(q);
         int n = 0;
-        for (Entity question : pq.asIterable(FetchOptions.Builder.withChunkSize(500))) {
-            if (Boolean.TRUE.equals(question.getProperty("dependentFlag")) && question.getProperty("dependentQuestionId") == null) {
-                System.out.println("Question id " + question.getKey().getId());
+        for (Entity question : ds.prepare(q).asIterable(FetchOptions.Builder.withChunkSize(500))) {
+            if (question.getProperty("sourceQuestionId") != null &&
+                    Boolean.TRUE.equals(question.getProperty("dependentFlag")) &&
+                    question.getProperty("dependentQuestionId") == null) {
                 n++;
                 brokenSurveys.add((Long) question.getProperty("surveyId"));
             }
         }
-        System.out.println("Number of issue " + n);
-        return brokenSurveys;
+        System.out.println("Broken questions: " + n);
+        System.out.println("Broken surveys: " + brokenSurveys.size());
+        return new ArrayList<>(brokenSurveys);
     }
 
-    private void fixDependencyForSurvey(DatastoreService ds) {
+    private List<Entity> getDataForSurvey(DatastoreService ds, Long surveyId) {
+        final Query q = new Query("SurveyInstance")
+                .setFilter(new FilterPredicate("surveyId", FilterOperator.EQUAL, surveyId))
+                .setKeysOnly();
+        return ds.prepare(q).asList(FetchOptions.Builder.withChunkSize(500));
+    }
 
-        final Query q = new Query("Question");
-        Instant t = Instant.parse("2020-11-01T00:00:00Z");
-        final Filter theFilter = new FilterPredicate("createdDateTime", FilterOperator.GREATER_THAN_OR_EQUAL, Date.from(t));
-        q.setFilter(theFilter);
-        q.addSort("createdDateTime", Query.SortDirection.ASCENDING);
+    private void fixDependencyForSurveys(DatastoreService ds) {
 
-        final PreparedQuery pq = ds.prepare(q);
+        List<Entity> copiedQuestions = getBrokenQuestions(ds);
 
-        Map<Long, Entity> questionCache = new HashMap<>();
-        Map<Long, Long> dependencyCache = new HashMap<>();
-        List<Entity> entitiesToSave = new ArrayList<>();
+        for (Entity question : copiedQuestions) {
 
-        for (Entity brokenQuestion : pq.asIterable(FetchOptions.Builder.withChunkSize(500))) {
+            Long sourceQuestionId = (Long) question.getProperty("sourceQuestionId");
+            Entity sourceQuestion = getSourceQuestion(ds, sourceQuestionId);
 
-            if (brokenQuestion.getProperty("sourceQuestionId") == null) {
-                continue;
-            }
-            if (Boolean.FALSE.equals(brokenQuestion.getProperty("dependentFlag")) ||
-                    (Boolean.TRUE.equals(brokenQuestion.getProperty("dependentFlag")) &&
-                            brokenQuestion.getProperty("dependentQuestionId") != null)) {
+            if (sourceQuestion == null) {
+                System.out.println("Broken sourceQuestionId for question: " + question.getKey().getId());
                 continue;
             }
 
-            Long sourceQuestionId = (Long) brokenQuestion.getProperty("sourceQuestionId");
-            Entity sourceQuestion = null;
+            Long sourceDependentQuestionId = (Long) sourceQuestion.getProperty("dependentQuestionId");
 
-            if (!questionCache.containsKey(sourceQuestionId)) {
-                try {
-                    sourceQuestion = ds.get(KeyFactory.createKey("Question", sourceQuestionId));
-                } catch (EntityNotFoundException e) {
-                    System.out.println("Source question with id " + sourceQuestionId + " not found, for: " + brokenQuestion.getKey().getId() + " Please check!");
-                    continue;
-                }
-                questionCache.put(sourceQuestionId, sourceQuestion);
-            } else {
-                sourceQuestion = questionCache.get(sourceQuestionId);
-            }
-
-            Long oldDependentId = (Long) sourceQuestion.getProperty("dependentQuestionId");
-            if (oldDependentId == null) {
-                System.out.println("OldDependantId null for question: " + sourceQuestion.getKey().getId());
+            if (sourceDependentQuestionId == null) {
+                System.out.println("Source dependentQuestionId is null for question: "
+                        + sourceQuestion.getKey().getId()
+                        + " broken question: " + question.getKey().getId());
                 continue;
             }
 
-            if (!dependencyCache.containsKey(oldDependentId)) {
+            Long surveyId = (Long) question.getProperty("surveyId");
+            Entity newDependent = getNewDependentQuestion(ds, surveyId, sourceDependentQuestionId);
 
-                Entity newDependentQuestion = null;
-                try {
-                    newDependentQuestion = ds.get(KeyFactory.createKey("Question", oldDependentId));
-                } catch (EntityNotFoundException e) {
-                    System.out.println("Question with id " + oldDependentId + " not found, for: " + sourceQuestion.getKey().getId() + " Please check!");
-                    continue;
-                }
-                dependencyCache.put(oldDependentId, newDependentQuestion.getKey().getId());
+            if (newDependent == null) {
+                System.out.println("New dependent question for source id " + sourceDependentQuestionId + " not found");
+                continue;
             }
 
-            Long newDependentId = dependencyCache.get(oldDependentId);
+            setNewDependency(question, newDependent);
 
-            System.out.println(String.format("Setting %s for %s", newDependentId, brokenQuestion.getKey().getId()));
-
-            brokenQuestion.setProperty("dependentQuestionId", newDependentId);
             // entitiesToSave.add(brokenQuestion);
         }
 
         // ds.put(entitiesToSave);
+    }
+
+    private List<Entity> getBrokenQuestions(DatastoreService ds) {
+        Date t = Date.from(Instant.parse("2020-10-01T00:00:00Z"));
+
+        Query qg = new Query("QuestionGroup")
+                .setFilter(new FilterPredicate("createdDateTime", FilterOperator.GREATER_THAN_OR_EQUAL, t))
+                .addSort("createdDateTime", Query.SortDirection.ASCENDING);
+
+        Map<Long, Integer> groupOrder = new HashMap<>();
+
+        for (Entity group : ds.prepare(qg).asList(FetchOptions.Builder.withChunkSize(500))) {
+            groupOrder.put(group.getKey().getId(), ((Long) group.getProperty("order")).intValue());
+        }
+
+        Query q = new Query("Question")
+                .setFilter(new FilterPredicate("createdDateTime", FilterOperator.GREATER_THAN_OR_EQUAL, t))
+                .addSort("createdDateTime", Query.SortDirection.ASCENDING);
+
+        List<Entity> candidateQuestions = ds.prepare(q).asList(FetchOptions.Builder.withChunkSize(500));
+        List<Entity> copiedQuestions = candidateQuestions
+                .stream()
+                .filter(entity -> entity.getProperty("sourceQuestionId") != null
+                        && Boolean.TRUE.equals(entity.getProperty("dependentFlag")) &&
+                        entity.getProperty("dependentQuestionId") == null)
+                .collect(Collectors.toList());
+
+        copiedQuestions.sort((entity1, entity2) -> {
+            Integer order1 = ((Long) entity1.getProperty("order")).intValue();
+            Integer order2 = ((Long) entity2.getProperty("order")).intValue();
+
+            Long group1 = (Long) entity1.getProperty("questionGroupId");
+            Long group2 = (Long) entity2.getProperty("questionGroupId");
+
+            if (group1.equals(group2)) {
+                return order1.compareTo(order2);
+            }
+
+            Integer orderWithGroup1 = groupOrder.get(group1) * order1;
+            Integer orderWithGroup2 = groupOrder.get(group2) * order2;
+
+            return orderWithGroup1.compareTo(orderWithGroup2);
+        });
+
+        return copiedQuestions;
+    }
+
+    private Entity getSourceQuestion(DatastoreService ds, Long sourceQuestionId) {
+        if (questionCache.containsKey(sourceQuestionId)) {
+            return questionCache.get(sourceQuestionId);
+        }
+        Entity q = null;
+        try {
+            q = ds.get(KeyFactory.createKey("Question", sourceQuestionId));
+        } catch (EntityNotFoundException e) {
+            System.out.println("Source question with id " + sourceQuestionId + " not found");
+        }
+        questionCache.put(sourceQuestionId, q);
+        return q;
+    }
+
+    private Entity getNewDependentQuestion(DatastoreService ds, Long surveyId, Long sourceDependentQuestionId) {
+        String key = surveyId + "-" + sourceDependentQuestionId;
+
+        if (dependencyCache.containsKey(key)) {
+            return dependencyCache.get(key);
+        }
+
+        Entity q = null;
+
+        Query query = new Query("Question");
+        Filter sourceFilter = new FilterPredicate("sourceQuestionId", FilterOperator.EQUAL, sourceDependentQuestionId);
+        Filter surveyFilter = new FilterPredicate("surveyId", FilterOperator.EQUAL, surveyId);
+        Filter f = new Query.CompositeFilter(Query.CompositeFilterOperator.AND, Arrays.asList(surveyFilter, sourceFilter));
+        query.setFilter(f);
+
+        q = ds.prepare(query).asSingleEntity();
+
+        dependencyCache.put(key, q);
+        return q;
+    }
+
+    private void setNewDependency(Entity question, Entity newDependent) {
+        System.out.println(String.format("Setting %s for %s", newDependent.getKey().getId(), question.getKey().getId()));
+        question.setProperty("dependentQuestionId", newDependent.getKey().getId());
+        questionCache.put(question.getKey().getId(), question);
     }
 }
