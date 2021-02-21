c8cd124c845f4f81f962b91b1fdac370f7442736
Daniel Lebrero Berna <dlebrero@gmail.com>
2021-02-21 09:29:28 +0100

    [#3747] Zulip notifications to correct stream

--- ci/flip-production-traffic.sh
+++ ci/flip-production-traffic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20201023.101935.7dacd92 \
+  -it akvo/akvo-devops:20210211.183324.b615f87 \
   flow-flip-production-traffic.sh
