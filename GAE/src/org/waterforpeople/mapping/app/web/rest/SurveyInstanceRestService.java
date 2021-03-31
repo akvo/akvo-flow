f99c88342fc055fb9e914b5541ae7e0285ce1e48
Emmanuel Mulo <emmanuel@akvo.org>
2021-03-31 13:04:55 +0200

    [#3755] Update copyright

--- GAE/src/org/waterforpeople/mapping/app/web/rest/SurveyInstanceRestService.java
+++ GAE/src/org/waterforpeople/mapping/app/web/rest/SurveyInstanceRestService.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2012-2015,2017,2020 Stichting Akvo (Akvo Foundation)
+ *  Copyright (C) 2012-2015,2017,2020,2021 Stichting Akvo (Akvo Foundation)
  *
  *  This file is part of Akvo FLOW.
  *
