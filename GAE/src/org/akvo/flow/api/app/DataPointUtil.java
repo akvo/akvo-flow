b0b646d9c5f475e45b68036808c7d3f6a4b15d19
Emmanuel Mulo <emmanuel@akvo.org>
2021-05-03 13:04:00 +0200

    [#3597] Update copyright

--- GAE/src/org/akvo/flow/api/app/DataPointUtil.java
+++ GAE/src/org/akvo/flow/api/app/DataPointUtil.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2019 Stichting Akvo (Akvo Foundation)
+ *  Copyright (C) 2019, 2021 Stichting Akvo (Akvo Foundation)
  *
  *  This file is part of Akvo FLOW.
  *
--- GAE/src/org/waterforpeople/mapping/dao/SurveyInstanceDAO.java
+++ GAE/src/org/waterforpeople/mapping/dao/SurveyInstanceDAO.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010-2015, 2017-2020 Stichting Akvo (Akvo Foundation)
+ *  Copyright (C) 2010-2015, 2017-2021 Stichting Akvo (Akvo Foundation)
  *
  *  This file is part of Akvo FLOW.
  *
