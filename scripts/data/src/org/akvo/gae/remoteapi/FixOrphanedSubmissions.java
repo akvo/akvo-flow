d7b2162c08ffb307c77bc79c4621b8cc21cb3324
valeria <valeria@akvo.org>
2021-05-04 12:41:24 +0200

    [#3597] removed unused imports

--- scripts/data/src/org/akvo/gae/remoteapi/FixOrphanedSubmissions.java
+++ scripts/data/src/org/akvo/gae/remoteapi/FixOrphanedSubmissions.java
@@ -14,11 +14,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import static org.akvo.gae.remoteapi.DataUtils.batchDelete;
 
 public class FixOrphanedSubmissions implements Process {
