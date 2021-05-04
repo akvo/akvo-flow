3102755c71a67416881ea832e5c664e2cc73ad65
valeria <valeria@akvo.org>
2021-05-04 09:19:01 +0200

    [#3597] WIP optimizing methods to retrieve data

--- scripts/data/src/org/akvo/gae/remoteapi/FixOrphanedSubmissions.java
+++ scripts/data/src/org/akvo/gae/remoteapi/FixOrphanedSubmissions.java
@@ -12,7 +12,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FixOrphanedSubmissions implements Process {
 
@@ -45,14 +47,30 @@ public class FixOrphanedSubmissions implements Process {
         List<Entity> dataPoints = getDataPoints(ds, surveyGroupIds);
         System.out.printf("Found %d monitoring datapoints\n", dataPoints.size());
 
+    /*    List<Entity> filteredDataPoints = dataPoints
+                .stream()
+                .filter(entity -> entity.getProperty("creationSurveyId") != null)
+                .collect(Collectors.toList());
+        List<Long> datapointsIds = filteredDataPoints
+                .stream()
+                .map(entity -> entity.getKey().getId())
+                .collect(Collectors.toList());
+
+        List<Long> dataPointIdsWithSurveyInstance = getRegistrationSurveyInstances(ds, filteredDataPoints);
+
+        datapointsIds.removeAll(dataPointIdsWithSurveyInstance);
+
+        List<Entity> instances = getSurveyInstances(ds, datapointsIds);*/
+
         List<Long> dataPointIdsToCleanup = dataPoints
                 .stream()
                 .filter(entity -> entity.getProperty("creationSurveyId") != null)
-                .map(entity -> getDataPointIdsToDelete(ds, entity))
+                .map(entity -> getDataPointIdToDelete(ds, entity))
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
 
         List<Entity> instances = getSurveyInstances(ds, dataPointIdsToCleanup);
+
         final StringBuilder sb = new StringBuilder();
         if (instances != null) {
             for (Entity instance : instances) {
@@ -73,7 +91,56 @@ public class FixOrphanedSubmissions implements Process {
         }
     }
 
-    private Long getDataPointIdsToDelete(DatastoreService ds, Entity entity) {
+/*    private List<Long> getRegistrationSurveyInstances(DatastoreService ds, List<Entity> datapoints) {
+        List<Long> dataPointsThatAreOk = new ArrayList<>();
+        Query instanceQuery = new Query("SurveyInstance");
+        List<Query.CompositeFilter> filters = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            Entity entity = datapoints.get(i);
+            Query.Filter f2 = new Query.FilterPredicate("surveyedLocaleId", Query.FilterOperator.EQUAL, entity.getKey().getId());
+            Query.Filter f = new Query.FilterPredicate("surveyId", Query.FilterOperator.EQUAL, (Long) entity.getProperty("creationSurveyId"));
+            Query.CompositeFilter compositeFilter = Query.CompositeFilterOperator.and(f2, f);
+           filters.add(compositeFilter);
+        }
+        System.out.println("Filters: " + filters.size());
+        if (!filters.isEmpty()) {
+            instanceQuery.setFilter(Query.CompositeFilterOperator.or(filters.toArray(new Query.CompositeFilter[0])));
+            instanceQuery.setKeysOnly();
+            List<Entity> entities = ds.prepare(instanceQuery).asList(FetchOptions.Builder.withChunkSize(1000));
+            if (entities != null) {
+                for (Entity e: entities) {
+                    dataPointsThatAreOk.add(e.getKey().getId());
+                }
+            }
+        }
+        return dataPointsThatAreOk;
+    }*/
+
+    private List<Long> getRegistrationSurveyInstances(DatastoreService ds, List<Entity> datapoints) {
+        List<Long> dataPointsThatAreOk = new ArrayList<>();
+        Query instanceQuery = new Query("SurveyInstance");
+        List<Long> dataPointIds = new ArrayList<>();
+        List<Long> surveyIds = new ArrayList<>();
+        for (Entity entity : datapoints) {
+            dataPointIds.add(entity.getKey().getId());
+            surveyIds.add((Long) entity.getProperty("creationSurveyId"));
+        }
+
+        Query.Filter f2 = new Query.FilterPredicate("surveyedLocaleId", Query.FilterOperator.IN, dataPointIds);
+        Query.Filter f = new Query.FilterPredicate("surveyId", Query.FilterOperator.IN, surveyIds);
+        Query.CompositeFilter compositeFilter = Query.CompositeFilterOperator.and(f2, f);
+        instanceQuery.setFilter(compositeFilter);
+        instanceQuery.setKeysOnly();
+        List<Entity> entities = ds.prepare(instanceQuery).asList(FetchOptions.Builder.withChunkSize(1000));
+        if (entities != null) {
+            for (Entity e : entities) {
+                dataPointsThatAreOk.add(e.getKey().getId());
+            }
+        }
+        return dataPointsThatAreOk;
+    }
+
+    private Long getDataPointIdToDelete(DatastoreService ds, Entity entity) {
         long dataPointId = entity.getKey().getId();
         Entity registrationFormInstance = getRegistrationSurveyInstance(ds, (Long) entity.getProperty("creationSurveyId"), dataPointId);
         if (registrationFormInstance == null) {
@@ -116,9 +183,13 @@ public class FixOrphanedSubmissions implements Process {
     }
 
     private List<Entity> getDataPoints(DatastoreService ds, List<Long> ids) {
+        if (ids == null || ids.isEmpty()) {
+            return Collections.EMPTY_LIST;
+        }
         Query.Filter f1 = new Query.FilterPredicate("surveyGroupId", Query.FilterOperator.IN, ids);
+        Query.Filter f2 = new Query.FilterPredicate("creationSurveyId", Query.FilterOperator.NOT_EQUAL, null);
         Query q = new Query("SurveyedLocale");
-        q.setFilter(f1);
+        q.setFilter(Query.CompositeFilterOperator.and(f1, f2));
         PreparedQuery pq = ds.prepare(q);
         return pq.asList(FetchOptions.Builder.withChunkSize(1000));
     }
