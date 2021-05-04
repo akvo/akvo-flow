e0d59e9c8cf5298fd0c867c3e555f0d5f0b1838c
valeria <valeria@akvo.org>
2021-05-04 16:16:02 +0200

    [#3597] fixed script path

--- scripts/data/remove_orphaned_survey_instances.sh
+++ scripts/data/remove_orphaned_survey_instances.sh
@@ -4,7 +4,7 @@
 
 APP_ID=$1
 SERVICE_ACCOUNT="sa-$APP_ID@$APP_ID.iam.gserviceaccount.com"
-P12_FILE_PATH="/home/valeria/src/akvo-flow-server-config/$1/$1.p12"
+P12_FILE_PATH="$FLOW_SERVER_CONFIG/$1/$1.p12"
 
 java -cp bin:"lib/*" \
      org.akvo.gae.remoteapi.RemoteAPI \
