87e6fda165bdd6f12b1ff44085cd948b88532b3a
Valeria Rogatchevskikh <valeria@akvo.org>
2021-05-04 16:17:02 +0200

    Update remove_orphaned_instances_all_instances.sh

--- scripts/data/remove_orphaned_instances_all_instances.sh
+++ scripts/data/remove_orphaned_instances_all_instances.sh
@@ -3,7 +3,7 @@
 set -eu
 # USAGE: ./remove_orphaned_survey_instances.sh
 
-#find $FLOW_SERVER_CONFIG/ -name "appengine-web.xml" -exec sed -n "s/\(.*\)<application>\(.*\)<\/application>\(.*\)/\2/p" {} \; | sort > instances.txt
+find $FLOW_SERVER_CONFIG/ -name "appengine-web.xml" -exec sed -n "s/\(.*\)<application>\(.*\)<\/application>\(.*\)/\2/p" {} \; | sort > instances.txt
 
 for i in $(cat instances.txt); do
   ./remove_orphaned_survey_instances.sh $i
