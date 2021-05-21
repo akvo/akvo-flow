0b79e836513b3d10598445df8f9fe66f14df73b7
valeria <valeria@akvo.org>
2021-05-21 13:47:50 +0200

    [#3597] show ok button but disabled

--- Dashboard/app/js/lib/controllers/permissions.js
+++ Dashboard/app/js/lib/controllers/permissions.js
@@ -112,6 +112,7 @@ FLOW.dialogControl = Ember.Object.create({
   activeAction: null,
   showOK: true,
   showCANCEL: true,
+  showOKDisabled: false,
 
   confirm(event) {
     this.set('activeView', event.view);
@@ -170,11 +171,13 @@ FLOW.dialogControl = Ember.Object.create({
 
         if (this.isRegistrationFormInstance(event.contexts[1])) {
           this.set('showOK', false);
+          this.set('showOKDisabled', true);
           const self = this;
           const instanceDeleted = event.contexts[1];
           const submissions = FLOW.SurveyInstance.find({surveyedLocaleId: instanceDeleted.get('surveyedLocaleId')});
           submissions.on('didLoad', () => {
             self.set('showOK', true);
+            this.set('showOKDisabled', false);
             const numberOfSubmissions = submissions.get('content').length;
             const numberMonitoringSubmissions = numberOfSubmissions - 1;
             if (numberMonitoringSubmissions > 0) {
