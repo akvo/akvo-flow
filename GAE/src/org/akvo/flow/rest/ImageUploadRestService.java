08fc21ce014b4f2ac4f7f545d042ac2b98b83340
valeria <valeria@akvo.org>
2021-05-25 12:32:16 +0200

    [#3791] upload images to s3

--- GAE/src/org/akvo/flow/rest/ImageUploadRestService.java
+++ GAE/src/org/akvo/flow/rest/ImageUploadRestService.java
@@ -1,11 +1,17 @@
 package org.akvo.flow.rest;
 
+import com.gallatinsystems.common.util.S3Util;
 import com.gallatinsystems.survey.dao.QuestionDao;
 import com.gallatinsystems.survey.dao.SurveyDAO;
 import com.gallatinsystems.survey.domain.Question;
 import com.gallatinsystems.survey.domain.Survey;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.util.Pair;
+import javax.annotation.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +26,9 @@ import org.waterforpeople.mapping.domain.SurveyInstance;
 @Controller
 @RequestMapping("/image_upload")
 public class ImageUploadRestService {
+
+    private static final Logger log = Logger
+            .getLogger(ImageUploadRestService.class.getName());
 
     @RequestMapping(method = RequestMethod.POST, value = "")
     @ResponseBody
@@ -50,14 +59,42 @@ public class ImageUploadRestService {
         if (file == null || file.isEmpty()) {
             return new Pair<>(400, "File is not valid");
         }
-        String fileName = generateFileName();
-        uploadImageToS3(file, fileName);
-        saveQuestionAnswer(question, formInstance, fileName);
+        String fileExtension = getFileType(file);
+        if (fileExtension == null) {
+            return new Pair<>(400, "File type is not valid: only jpg and png are accepted"); //TODO: shall we accept other formats?
+        }
+        String filename = uploadImageToS3(file, generateFileName(fileExtension));
+        if (filename == null) {
+            return new Pair<>(400, "Upload to s3 failed for: " + filename);
+        }
+        saveQuestionAnswer(question, formInstance, filename);
         return new Pair<>(responseCode, errorMessage);
     }
 
-    private void uploadImageToS3(MultipartFile file, String fileName) {
-        //TODO:
+    @Nullable
+    private String getFileType(MultipartFile file) {
+        String contentType = file.getContentType() != null? file.getContentType(): "";
+        switch (contentType) {
+            case "image/jpeg":
+                return ".jpg";
+            case "image/png":
+                return ".png";
+        }
+        return null;
+    }
+
+    @Nullable
+    private String uploadImageToS3(MultipartFile file, String fileName) {
+        try {
+            Properties props = System.getProperties();
+            String bucketName = props.getProperty("s3bucket");
+            String directory = props.getProperty("images");
+            S3Util.put(bucketName, directory + "/" + fileName, file.getBytes(), file.getContentType(), true);
+            return fileName;
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Error uploading image", e);
+            return null;
+        }
     }
 
     private void saveQuestionAnswer(Question question, SurveyInstance formInstance, String fileName) {
@@ -79,11 +116,11 @@ public class ImageUploadRestService {
         new QuestionAnswerStoreDao().save(store);
     }
 
-    private String generateFileName() {
+    private String generateFileName(String fileExtension) {
         //in the app this is how we create the filename
         //TODO: what if image is not JPG but PNG
         //maybe pass this as param?
-        return UUID.randomUUID().toString()+".jpg";
+        return UUID.randomUUID().toString() + fileExtension;
     }
 
     private SurveyInstance getFormInstance(String formInstanceId) {
