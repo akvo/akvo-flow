e5ec6d03abcb9ad2d59fc535171102201b31454b
Emmanuel Mulo <emmanuel@akvo.org>
2021-08-18 23:24:18 +0200

    [#3838] Add test for scenario where the imported instance has no errors

--- GAE/test/org/waterforpeople/mapping/app/web/RawDataRestServletTest.java
+++ GAE/test/org/waterforpeople/mapping/app/web/RawDataRestServletTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RawDataRestServletTest {
     private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
@@ -47,5 +48,22 @@ public class RawDataRestServletTest {
         RawDataRestServlet servlet = new RawDataRestServlet();
         Map<Long, String> validateMissingDataPoint = servlet.validateImportRequest(importRequest);
         assertEquals("Associated datapoint is missing [ datapoint id = " + instance.getSurveyedLocaleId() + "]", validateMissingDataPoint.get(instance.getKey().getId()));
+    }
+
+    @Test
+    void testFormInstanceValidationOk() {
+        Long surveyId = dataStoreTestUtil.randomId();
+
+        List<SurveyedLocale> dataPoints = dataStoreTestUtil.createDataPoints(surveyId, 1);
+        List<SurveyInstance> formInstances = dataStoreTestUtil.createFormInstances(dataPoints, 1);
+
+        SurveyInstance instance = formInstances.get(0);
+
+        RawDataImportRequest importRequest = new RawDataImportRequest();
+        importRequest.setSurveyInstanceId(instance.getKey().getId());
+
+        RawDataRestServlet servlet = new RawDataRestServlet();
+        Map<Long, String> validateImportErrors = servlet.validateImportRequest(importRequest);
+        assertTrue(validateImportErrors.isEmpty());
     }
 }
