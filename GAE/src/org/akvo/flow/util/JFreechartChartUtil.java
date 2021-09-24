7c6fff9be8b6975839ab379f379dc1665c6ea6e3
Emmanuel Mulo <emmanuel@akvo.org>
2021-09-24 19:00:29 +0200

    [#3877] Remove unused classes and methods

--- GAE/src/org/akvo/flow/util/JFreechartChartUtil.java
+++ GAE/src/org/akvo/flow/util/JFreechartChartUtil.java
@@ -45,37 +45,7 @@ public class JFreechartChartUtil {
 
     private static final Logger log = Logger
             .getLogger(JFreechartChartUtil.class.getName());
-    /**
-     * generates a pie chart with the set of labels and values passed in (the labels and values
-     * arrays must both be non-null and contain the same number of elements). The chart is returned
-     * as a byte array representing the image.
-     * 
-     * @param labels
-     * @param values
-     * @param title
-     * @param width
-     * @param height
-     * @return - byte array containing the image, null if there is an error.
-     */
-    public static byte[] getPieChart(List<String> labels, List<String> values,
-            String title, int width, int height) {
-        DefaultPieDataset pieDataset = new DefaultPieDataset();
 
-        for (int i = 0; i < labels.size(); i++) {
-            pieDataset.setValue(labels.get(i) + " (" + values.get(i) + ")",
-                    Double.parseDouble(values.get(i)));
-        }
-        JFreeChart chart = ChartFactory.createPieChart(title, pieDataset,
-                false, false, false);
-        try {
-            return ChartUtilities.encodeAsPNG(chart.createBufferedImage(width, height));
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-            return null;
-        }
-
-    }
-    
     /**
      * generates a bar chart with the set of labels and values passed in (the labels and values
      * arrays must both be non-null and contain the same number of elements). The chart is returned
