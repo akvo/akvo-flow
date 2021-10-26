0444f4596484ca09c9918951987f138e4a5d1be1
voromahery <daniel.her@gmail.org>
2021-10-26 09:00:23 +0300

    [#3887] Add ability to toggle the edit button and to edit the existing group name

--- Dashboard/app/js/lib/views/devices/devices-views.jsx
+++ Dashboard/app/js/lib/views/devices/devices-views.jsx
@@ -35,6 +35,9 @@ FLOW.CurrentDevicesTabView = FLOW.ReactComponentView.extend(
         onSortDevices: this.devicesSort,
         onSortGroup: this.GroupSort,
         onDeleteGroup: this.deleteGroupConfirm,
+        toggleEditButton: this.toggleEditButton,
+        selectedEditGroupId: this.selectedEditGroupId,
+        renameGroup: this.renameGroup,
         sortProperties: {
           column: this.selectedColumn,
           ascending: this.sortAscending,
