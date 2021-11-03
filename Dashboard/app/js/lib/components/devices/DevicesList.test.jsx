0c46fe502e5e58f5876bdb0bd92a56fc4b1f5610
voromahery <daniel.her@gmail.org>
2021-11-03 09:57:08 +0300

    [#3887] Adds test for addToGroup and removeFromGroup button

--- Dashboard/app/js/lib/components/devices/DevicesList.test.jsx
+++ Dashboard/app/js/lib/components/devices/DevicesList.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import DevicesTabContext from './devices-context';
 import Deviceslist from './DevicesList';
 
@@ -34,25 +34,62 @@ describe('Deviceslist Tests', () => {
     version: 'version',
     navText: {
       manageDevices: 'manage device',
+      addToDeviceGroup: 'Add to device group',
+      removeFromDeviceGroup: 'Remove from deviceGroup',
     },
   };
 
   const tableHeaderClass = () => 'sorting_asc';
 
-  const selectedDeviceIds = [];
-
   const sortProperties = {
     column: 'code',
   };
 
+  let selectedDeviceIds = [];
+
+  const selectDevice = () => {
+    if (selectedDeviceIds.some(deviceId => deviceId === 261243005)) {
+      const filterDevice = selectedDeviceIds.filter(deviceId => deviceId !== 261243005);
+      selectedDeviceIds = [...filterDevice];
+    } else {
+      selectedDeviceIds = [...selectedDeviceIds, 261243005];
+    }
+    return selectedDeviceIds;
+  };
+
+  const { showAddToGroupDialog, showRemoveFromGroupDialog } = () => jest.fn();
+
   it('+++ renders <snapshot>', () => {
     const wrapper = render(
       <DevicesTabContext.Provider
-        value={{ strings, devices, tableHeaderClass, selectedDeviceIds, sortProperties }}
+        value={{
+          strings,
+          devices,
+          tableHeaderClass,
+          selectedDeviceIds,
+          sortProperties,
+          selectDevice,
+          showAddToGroupDialog,
+          showRemoveFromGroupDialog,
+        }}
       >
         <Deviceslist />
       </DevicesTabContext.Provider>
     );
+
+    const showAddToGroupButton = wrapper.getByText('Add to device group');
+    fireEvent.click(showAddToGroupButton);
+
+    const showRemoveFromGroupButton = wrapper.getByText('Remove from deviceGroup');
+    fireEvent.click(showRemoveFromGroupButton);
+
+    const selectDeviceCheckbox = document.querySelector('input');
+
+    expect(selectedDeviceIds.length).toBe(0);
+    fireEvent.click(selectDeviceCheckbox);
+    expect(selectedDeviceIds.length).toBe(1);
+    fireEvent.click(selectDeviceCheckbox);
+    expect(selectedDeviceIds.length).toBe(0);
 
     expect(wrapper.container).toMatchSnapshot();
   });
