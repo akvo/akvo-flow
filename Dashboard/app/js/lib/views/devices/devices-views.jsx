6786bee7cdcc15d912ff1a74ae8ce75434347016
voromahery <daniel.her@gmail.org>
2021-11-04 17:29:56 +0300

    [#3887] Moves sorting in the DevicesTab

--- Dashboard/app/js/lib/views/devices/devices-views.jsx
+++ Dashboard/app/js/lib/views/devices/devices-views.jsx
@@ -34,17 +34,6 @@ FLOW.CurrentDevicesTabView = FLOW.ReactComponentView.extend(
         cancelRemoveFromGroup: this.cancelRemoveFromGroup,
         onSortDevices: this.devicesSort,
         onSortGroup: this.GroupSort,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        onDeleteGroup: this.deleteGroupConfirm,
->>>>>>> 71bba63eb... [#3887] Clean code and refactor
-=======
-        // onDeleteGroup: this.deleteGroupConfirm,
->>>>>>> a257340f3... [#3887] Connect delete dialog to DevicesTab
-=======
->>>>>>> e67c8e9c0... [#3887] Cleans code
         sortProperties: {
           column: this.selectedColumn,
           ascending: this.sortAscending,
