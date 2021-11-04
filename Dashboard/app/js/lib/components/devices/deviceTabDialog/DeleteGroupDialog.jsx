35f1f9edc36c3b03b1fe53f15f07092ba1ad3611
voromahery <daniel.her@gmail.org>
2021-11-04 11:44:19 +0300

    [#3887] Fixes dialog

--- Dashboard/app/js/lib/components/devices/deviceTabDialog/DeleteGroupDialog.jsx
+++ Dashboard/app/js/lib/components/devices/deviceTabDialog/DeleteGroupDialog.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import DevicesTabContext from '../devices-context';
 
 export default function DeleteGroup() {
-  return <div>Hi</div>;
+  const { isShowDeleteDialog, cancelDeletingGroup, deleteGroupConfirm, strings } = useContext(
+    DevicesTabContext
+  );
+
+  return (
+    <div className={isShowDeleteDialog ? 'overlay display' : 'overlay'}>
+      <div className="blanket" />
+      <div className="dialogWrap">
+        <div className="confirmDialog dialog">
+          <h2>{Ember.String.loc('_device_group_delete_header')}</h2>
+          <p className="dialogMsg">{Ember.String.loc('_this_cant_be_undo')}</p>
+          <div className="buttons menuCentre">
+            <ul>
+              <li>
+                <button type="button" className="ok smallBtn" onClick={deleteGroupConfirm}>
+                  {strings.dialogText.save}
+                </button>
+              </li>
+              <li>
+                <button type="button" className="cancel" onClick={cancelDeletingGroup}>
+                  {strings.dialogText.cancel}
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
