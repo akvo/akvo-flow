bd4e65bd08fa8b1be9437bac74a5ef403a295e1d
voromahery <daniel.her@gmail.org>
2021-11-04 11:40:39 +0300

    [#3887] Test dialog

--- Dashboard/app/js/lib/components/devices/deviceTabDialog/DeleteGroupDialog.jsx
+++ Dashboard/app/js/lib/components/devices/deviceTabDialog/DeleteGroupDialog.jsx
@@ -1,34 +1,5 @@
-import React, { useContext } from 'react';
-import DevicesTabContext from '../devices-context';
+import React from 'react';
 
 export default function DeleteGroup() {
-  const { isShowDeleteDialog, cancelDeletingGroup, deleteGroupConfirm, strings } = useContext(
-    DevicesTabContext
-  );
-
-  return (
-    <div className={isShowDeleteDialog ? 'overlay display' : 'overlay'}>
-      <div className="blanket" />
-      <div className="dialogWrap">
-        <div className="confirmDialog dialog">
-          <h2>device group delete header</h2>
-          <p className="dialogMsg">this cant be undo</p>
-          <div className="buttons menuCentre">
-            <ul>
-              <li>
-                <button type="button" className="ok smallBtn" onClick={deleteGroupConfirm}>
-                  {strings.dialogText.save}
-                </button>
-              </li>
-              <li>
-                <button type="button" className="cancel" onClick={cancelDeletingGroup}>
-                  {strings.dialogText.cancel}
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Hi</div>;
 }
