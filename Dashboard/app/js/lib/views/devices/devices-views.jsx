c2c0444dd77776ce7b13622683fdadadd6a3e72e
voromahery <daniel.her@gmail.org>
2021-11-09 14:42:46 +0300

    [#3889] Fixes dialog text

--- Dashboard/app/js/lib/views/devices/devices-views.jsx
+++ Dashboard/app/js/lib/views/devices/devices-views.jsx
@@ -55,8 +55,6 @@ FLOW.CurrentDevicesTabView = FLOW.ReactComponentView.extend(
             selectGroupText: Ember.String.loc('_select_existing_device_group'),
             chooseGroup: Ember.String.loc('_choose_an_existing_device_group_from_the_list'),
             addDeviceToGroup: Ember.String.loc('_add_devices_to_device_group'),
-            dialogHeader: Ember.String.loc('_device_group_delete_header'),
-            dialogMessage: Ember.String.loc('_this_cant_be_undo'),
             save: Ember.String.loc('_ok'),
             cancel: Ember.String.loc('_cancel'),
           },
