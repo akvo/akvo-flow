7e7580a8120d76bfd34df02c3cdac1e7cebe3214
Emmanuel Mulo <emmanuel@akvo.org>
2022-05-13 12:17:38 +0200

    [#3636] Add repo config for all instances script

--- scripts/data/remove_user_all_instances.sh
+++ scripts/data/remove_user_all_instances.sh
@@ -3,6 +3,9 @@
 set -eu
 # USAGE: ./remove_user_all_instances.sh userEmail
 
+REPOS_HOME="$(cd $(dirname "$0")/../../.. && pwd)"
+FLOW_SERVER_CONFIG="$REPOS_HOME/akvo-flow-server-config"
+
 find $FLOW_SERVER_CONFIG/ -name "appengine-web.xml" -exec sed -n "s/\(.*\)<application>\(.*\)<\/application>\(.*\)/\2/p" {} \; | sort > instances.txt
 
 for i in $(cat instances.txt); do
