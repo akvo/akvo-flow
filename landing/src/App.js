1e64a02e67fdca3f12691b0dd0c966c6cfdb8fcb
Developer <daniel.her@onja.org>
2022-09-26 16:13:18 +0300

    [#3929] Configurate and add all necessary assets

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import KeyFeaturesPage from "./page/key-features/index";
+import Contact from "./page/contact/index";
+import Home from "./page/home";
+import Signup from "./page/signup";
+import PricingTab from "./components/pricing/pricing-tab/index";
+import PricingTable from "./components/pricing/pricing-table";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/key-features">
+          <KeyFeaturesPage />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/pricing">
+          <PricingTable />
+          <PricingTab />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
